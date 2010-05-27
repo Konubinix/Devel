b322df8529a8738520fd08107099b77fac9438e2
Samuel <sy3@3ds.com>
2010-05-27 13:40:21 +0200

    Ajoutd 'un advice qui push une mark avant de faire un find-tag (pour revenir où on était)

--- config/elfiles/config/advices_KONIX.el
+++ config/elfiles/config/advices_KONIX.el
@@ -39,8 +39,13 @@
   "Wait an entire day that the user says he has seen the appt"
   (message "Please press anything to say you have seen your appointment")
   (sit-for 86400)
-  )
+)
 (ad-activate 'appt-delete-window)
+
+(defadvice find-tag (before push-mark ())
+  (push-mark)
+  )
+(ad-activate 'find-tag)
 
 (defadvice find-tag (before push-mark ())
   (push-mark)

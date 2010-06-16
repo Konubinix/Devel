152354853dada33bbedc6e5e2d7d7100cb863d40
Samuel <sy3@3ds.com>
2010-06-16 22:00:59 +0200

    Ajout d'un hook qui permet de faire un "make run" après une compilation réussie (et aussi une fonction qui met en place ce hook)

--- config/elfiles/config/functions_KONIX.el
+++ config/elfiles/config/functions_KONIX.el
@@ -299,6 +299,27 @@ mieux voir."
   (interactive "fMakefile : ")
   (shell-command-to-string (concat "make -f " makefile " "param"&"))
   )
+(defun konix/set-compilation-success-run-hook ()
+  (setq konix/compilation-success-hook
+		'((lambda()
+			(konix/make-shell konix/proj-makefile "run")
+			(setq konix/compilation-success-hook nil)
+			)))
+  )
+(defvar konix/compilation-success-hook nil)
+;; Close the compilation window if there was no error at all.
+(setq compilation-exit-message-function
+	  (lambda (status code msg)
+		;; If M-x compile exists with a 0
+		(when (and (eq status 'exit) (zerop code))
+		  ;; then bury the *compilation* buffer, so that C-x b doesn't go there
+										;		  (bury-buffer "*compilation*")
+		  ;; and return to whatever were looking at before
+										;		  (replace-buffer-in-windows "*compilation*")
+		  (run-hooks 'konix/compilation-success-hook)
+		  )
+		;; Always return the anticipated result of compilation-exit-message-function
+		(cons msg code)))
 ;; ************************************************************
 ;; Git
 ;; ************************************************************

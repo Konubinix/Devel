f7e7de86cea511ee53ab504d396d0e28c8781d38
Samuel <sy3@3ds.com>
2010-06-24 23:14:59 +0200

    Puisque fluxox permet de gérer des raccourcis clavier sur plusieurs touches, je n'ai plus d'intérêt à avoir un script de confirmation de poweroff, Mod4 b p -> poweroff Mod4 b r -> reboot C'est assez facilement évitable comme combinaison (alors que Ctrl+Alt+Suppr, ça m'arrive de la faire pour killer X...) pour ne plus demander de confirmation

--- bin/poweroff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "poweroff de sur ? (o/n)"
-read arg
-if [[ $arg == "o" ]]
-then
-    poweroff
-fi
--- bin/reboot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "reboot de sur ? (o/n)"
-read arg
-if [[ $arg == "o" ]]
-then
-    reboot
-fi

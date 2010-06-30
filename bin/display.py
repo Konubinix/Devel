66f19fef7c91c5149b7719b6acf53f800d4678d8
Samuel <sy3@3ds.com>
2010-07-01 00:22:37 +0200

    Utilisation de mon notify daemon, de libnotify, ou de OSD dans display

--- bin/display.py
+++ bin/display.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python
+import sys
 
-import pyosd
-import sys, os
-import time
-if len(sys.argv) <= 1:
-    exit(1)
+def by_konubinix_notificator(message,unique=False):
+	import dbus
+	session = dbus.SessionBus()
+	if unique:
+		path = "/UniqueNotification"
+	else:
+		path = "/Notification"
+	notification = session.get_object('konubinix.notificator',
+									  path)
+	notification.Notify(message)
 
-if os.path.isfile('/tmp/display'):
-    fd = open('/tmp/display','r')
-    pid = fd.readline()
-    fd.close()
-    os.system("kill -9 "+str(pid))
+def by_pynotify(message):
+	import pynotify
+	if not pynotify.init("Message"):
+		sys.exit(1)
+	n = pynotify.Notification("Message", message)
+	n.set_timeout(3000)
+	n.show()
 
-pid = os.getpid()
+def by_pyosd(message):
+	import pyosd
+	p = pyosd.osd("-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-1", colour="green", pos=pyosd.POS_BOT,offset=40, align=pyosd.ALIGN_CENTER)
+	p.display(message)
 
-p = pyosd.osd("-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-1", colour="green", pos=pyosd.POS_BOT,offset=40, align=pyosd.ALIGN_CENTER)
-p.display(' '.join(sys.argv[1:]))
+def main(message,unique=False):
+	try:
+		by_konubinix_notificator(message,unique)
+	except:
+		try:
+			by_pynotify(message)
+		except:
+			by_pyosd(message)
 
-os.system('echo '+str(pid)+' > /tmp/display')
-time.sleep(1)
-os.remove('/tmp/display')
+if __name__ == "__main__":
+	if len(sys.argv) <= 1:
+		exit(1)
+	message = ' '.join(sys.argv[1:])
+	main(message)

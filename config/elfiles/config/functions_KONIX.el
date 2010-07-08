ede434f69bd1e88c5bc36c9bcec9b35abe2dd89d
Samuel <sy3@3ds.com>
2010-07-08 18:17:28 +0200

    Meilleure git command qui fait un cdup au lieu d'aller dans le rep de .git

--- config/elfiles/config/functions_KONIX.el
+++ config/elfiles/config/functions_KONIX.el
@@ -730,7 +730,7 @@ lieu de find-file."
 (defun konix/git/command (command)
   "Lance une commande git."
   (interactive "sCommande : ")
-  (shell-command (concat "cd \"$(git rev-parse --git-dir)\" && git " command"&"))
+  (shell-command (concat "cd \"./$(git rev-parse --show-cdup)\" && git " command"&"))
   )
 
 (defun konix/git/command-to-string (command)

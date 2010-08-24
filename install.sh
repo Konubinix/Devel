72ad1329ff9259679a1cc913a6b5d64cee7569b4
Samuel <sy3@3ds.com>
2010-08-24 20:28:26 +0200

    install variables are into " for space filename to work

--- install.sh
+++ install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-CONFIG=$(pwd)/config
+CONFIG="$(pwd)/config"
 
 function remplace(){
-    rm -vrf $1
-    ln -sv $2 $1
+    rm -vrf "$1"
+    ln -sv "$2" "$1"
 }
 
-remplace $HOME/.gitk             $CONFIG/gitk
-remplace $HOME/.gitconfig        $CONFIG/gitconfig
-remplace $HOME/.emacs            $CONFIG/emacs
-remplace $HOME/.emacs-custom.el  $CONFIG/emacs-custom.el
-remplace $HOME/.bash_aliases     $CONFIG/bash_aliases
-remplace $HOME/.bashrc           $CONFIG/bashrc
-remplace $HOME/.fluxbox          $CONFIG/fluxbox
-remplace $HOME/.pyrc.py          $CONFIG/pyrc.py
-remplace $HOME/.elfiles          $CONFIG/elfiles
-remplace $HOME/.conky.d          $CONFIG/conky
-remplace $HOME/.B.blend          $CONFIG/B.blend
-remplace $HOME/.ideskrc          $CONFIG/ideskrc
-remplace $HOME/.idesktop         $CONFIG/idesktop
-remplace $HOME/.zshrc            $CONFIG/zshrc
-remplace $HOME/.zsh_aliases      $CONFIG/zsh_aliases
-remplace $HOME/.shrc             $CONFIG/shrc
-remplace $HOME/.Xdefaults        $CONFIG/Xdefaults
-remplace $HOME/Makefile          $CONFIG/Makefile
-remplace $HOME/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml        $CONFIG/config/gnome-terminal-conf.xml
+remplace "$HOME/.gitk"             "$CONFIG/gitk"
+remplace "$HOME/.gitconfig"        "$CONFIG/gitconfig"
+remplace "$HOME/.emacs"            "$CONFIG/emacs"
+remplace "$HOME/.emacs-custom.el"  "$CONFIG/emacs-custom.el"
+remplace "$HOME/.bash_aliases"     "$CONFIG/bash_aliases"
+remplace "$HOME/.bashrc"           "$CONFIG/bashrc"
+remplace "$HOME/.fluxbox"          "$CONFIG/fluxbox"
+remplace "$HOME/.pyrc.py"          "$CONFIG/pyrc.py"
+remplace "$HOME/.elfiles"          "$CONFIG/elfiles"
+remplace "$HOME/.conky.d"          "$CONFIG/conky"
+remplace "$HOME/.B.blend"          "$CONFIG/B.blend"
+remplace "$HOME/.ideskrc"          "$CONFIG/ideskrc"
+remplace "$HOME/.idesktop"         "$CONFIG/idesktop"
+remplace "$HOME/.zshrc"            "$CONFIG/zshrc"
+remplace "$HOME/.zsh_aliases"      "$CONFIG/zsh_aliases"
+remplace "$HOME/.shrc"             "$CONFIG/shrc"
+remplace "$HOME/.Xdefaults"        "$CONFIG/Xdefaults"
+remplace "$HOME/Makefile"          "$CONFIG/Makefile"
+remplace "$HOME/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml"        "$CONFIG/config/gnome-terminal-conf.xml"
 
-remplace $HOME/bin               $(pwd)/bin
-remplace $HOME/lib               $(pwd)/lib
+remplace "$HOME/bin"               "$(pwd)/bin"
+remplace "$HOME/lib"               "$(pwd)/lib"
 
-chmod +x -Rv $HOME/bin $HOME/.fluxbox/startup $HOME/.fluxbox/bin
+chmod +x -Rv "$HOME/bin" "$HOME/.fluxbox/startup" "$HOME/.fluxbox/bin"

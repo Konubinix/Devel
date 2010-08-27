41e433d14eb233ce843c70595cfd6361fa470753
Samuel <sy3@3ds.com>
2010-08-27 20:53:25 +0200

    install.sh set up an icon theme and a crux like theme

--- install.sh
+++ install.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
 CONFIG="$(pwd)/config"
+TUNNING="$(pwd)/tunning"
 
 function remplace(){
     rm -vrf "$1"
     ln -sv "$2" "$1"
+}
+
+function install_icon_theme(){
+	archive="$1"
+	icons_dir="$HOME/.icons"
+	echo "install $archive icon theme"
+	mkdir "$icons_dir" 2>/dev/null
+	tmp_dir="$(mktemp -d)"
+	tar -C "$tmp_dir" -xf "$archive"
+	theme_name=$(ls --color=never "$tmp_dir")
+	rm -rf "$icons_dir/$theme_name"
+	mv "$tmp_dir/$theme_name" "$icons_dir/"
+	gconftool -s /desktop/gnome/interface/icon_theme -t string "$theme_name"
+	rm -rf "$tmp_dir"
+}
+
+function install_gtk_theme(){
+	archive="$1"
+	themes_dir="$HOME/.themes"
+	echo "install $archive theme"
+	mkdir "$themes_dir" 2>/dev/null
+	tmp_dir="$(mktemp -d)"
+	tar -C "$tmp_dir" -xf "$archive"
+	theme_name=$(ls --color=never "$tmp_dir")
+	rm -rf "$themes_dir/$theme_name"
+	mv "$tmp_dir/$theme_name" "$themes_dir/"
+	gconftool -s /desktop/gnome/interface/gtk_theme -t string "$theme_name"
+	rm -rf "$tmp_dir"
 }
 
 remplace "$HOME/.gitk"             "$CONFIG/gitk"
@@ -31,5 +60,11 @@ remplace "$HOME/.xsession"         "$HOME/.xinitrc"
 
 remplace "$HOME/bin"               "$(pwd)/bin"
 remplace "$HOME/lib"               "$(pwd)/lib"
+
+# ####################################################################################################
+# Gnome conf
+# ####################################################################################################
+install_icon_theme "$TUNNING/Delta_Gnome_Icons.tar.gz"
+install_gtk_theme "$TUNNING/Theme.tar.gz"
 
 chmod +x -Rv "$HOME/bin" "$HOME/.fluxbox/startup" "$HOME/.fluxbox/bin"

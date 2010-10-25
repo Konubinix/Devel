89ef82dad9ec8e4795f8e47c26c4d73569b15cec
Samuel <sy3@3ds.com>
2010-10-25 13:43:17 +0200

    change the name of some files for better customization

--- install_shell.sh
+++ install_shell.sh
@@ -7,24 +7,37 @@
 ######################################################################
 source ./install_var.sh
 
-cat <<EOF  > "$HOME/.custo.sh"
-DEVEL_DIR="${DEVEL_DIR}"
-CONFIG_DIR="${CONFIG_DIR}"
-PERSO_DIR="${PERSO_DIR}"
+cat <<EOF  > "$HOME/.shrc_var"
+#!/bin/bash
+export DEVEL_DIR="${DEVEL_DIR}"
+export CONFIG_DIR="${CONFIG_DIR}"
+export PERSO_DIR="${PERSO_DIR}"
+export CUSTOM_FILE="$HOME/.shrc_custo"
 EOF
 
+if test ! -e "$HOME/.shrc_custo"
+then
+    cat <<EOF > "$HOME/.shrc_custo"
+#!/bin/bash
+export USER=$USERNAME
+EOF
+fi
+
 cat <<EOF > "$HOME/.bashrc"
-source "$HOME/.custo.sh"
+#!/bin/bash
+source "$HOME/.shrc_var"
 source "${CONFIG_DIR}/bashrc"
 EOF
 
 cat <<EOF > "$HOME/.shrc"
-source "$HOME/.custo.sh"
+#!/bin/bash
+source "$HOME/.shrc_var"
 source "${CONFIG_DIR}/shrc"
 EOF
 
 cat <<EOF > "$HOME/.zshrc"
-source "$HOME/.custo.sh"
+#!/bin/bash
+source "$HOME/.shrc_var"
 source "${CONFIG_DIR}/zshrc"
 EOF
 

ee86d083dc5efa1da22ceb8a92a335e225cae771
Samuel Loury <konubinixweb@gmail.com>
2011-11-19 18:41:09 +0100

    Addition of a fancy_print fallback function that can be used when one don't want fancy_print to be used by client code

--- lib/konix_fancy_print.py
+++ lib/konix_fancy_print.py
@@ -2,6 +2,11 @@
 # -*- coding:utf-8 -*-
 
 import logging
+import sys
+
+def fancy_print_fallback(fg_color, bg_color, string):
+    sys.stdout.write(string)
+
 try:
     import colorconsole.terminal
     colors = colorconsole.terminal.colors
@@ -12,21 +17,20 @@ try:
 except:
     logging.warning("Fancy print will not produce colorized output because the python library colorconsole was not found")
     # add colors for using scripts not to fail
-    colors= { "BLACK"   : 0,
-              "BLUE"    : 1,
-              "GREEN"   : 2,
-              "CYAN"    : 3,
-              "RED"     : 4,
-              "PURPLE"  : 5,
-              "BROWN"   : 6,
-              "LGREY"   : 7,
-              "DGRAY"   : 8,
-              "LBLUE"   : 9,
-              "LGREEN"  : 10,
-              "LCYAN"   : 11,
-              "LRED"    : 12,
-              "LPURPLE" : 13,
-              "YELLOW"  : 14,
-              "WHITE"   : 15  }
-    def fancy_print(fg_color, bg_color, string):
-        print string
+    colors = { "BLACK"   : 0,
+               "BLUE"    : 1,
+               "GREEN"   : 2,
+               "CYAN"    : 3,
+               "RED"     : 4,
+               "PURPLE"  : 5,
+               "BROWN"   : 6,
+               "LGREY"   : 7,
+               "DGRAY"   : 8,
+               "LBLUE"   : 9,
+               "LGREEN"  : 10,
+               "LCYAN"   : 11,
+               "LRED"    : 12,
+               "LPURPLE" : 13,
+               "YELLOW"  : 14,
+               "WHITE"   : 15  }
+    fancy_print = fancy_print_fallback

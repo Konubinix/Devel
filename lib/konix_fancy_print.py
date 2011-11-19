966dcda2b4c5e373a18466b5471694e01c3f8a73
Samuel Loury <konubinixweb@gmail.com>
2011-11-19 18:10:24 +0100

    Add the colors in konix_fancy_print

--- lib/konix_fancy_print.py
+++ lib/konix_fancy_print.py
@@ -2,6 +2,22 @@
 # -*- coding:utf-8 -*-
 
 import logging
+colors= { "BLACK"   : 0,
+          "BLUE"    : 1,
+          "GREEN"   : 2,
+          "CYAN"    : 3,
+          "RED"     : 4,
+          "PURPLE"  : 5,
+          "BROWN"   : 6,
+          "LGREY"   : 7,
+          "DGRAY"   : 8,
+          "LBLUE"   : 9,
+          "LGREEN"  : 10,
+          "LCYAN"   : 11,
+          "LRED"    : 12,
+          "LPURPLE" : 13,
+          "YELLOW"  : 14,
+          "WHITE"   : 15  }
 try:
     import colorconsole.terminal
     t = colorconsole.terminal.get_terminal()

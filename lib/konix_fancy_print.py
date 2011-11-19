cd0d4645e63b6c417abe74539adce7c7f8eb9737
Samuel Loury <konubinixweb@gmail.com>
2011-11-19 18:36:56 +0100

    konix_fancy_script now uses the colorconsole.terminal.colors dict

--- lib/konix_fancy_print.py
+++ lib/konix_fancy_print.py
@@ -2,27 +2,31 @@
 # -*- coding:utf-8 -*-
 
 import logging
-colors= { "BLACK"   : 0,
-          "BLUE"    : 1,
-          "GREEN"   : 2,
-          "CYAN"    : 3,
-          "RED"     : 4,
-          "PURPLE"  : 5,
-          "BROWN"   : 6,
-          "LGREY"   : 7,
-          "DGRAY"   : 8,
-          "LBLUE"   : 9,
-          "LGREEN"  : 10,
-          "LCYAN"   : 11,
-          "LRED"    : 12,
-          "LPURPLE" : 13,
-          "YELLOW"  : 14,
-          "WHITE"   : 15  }
 try:
     import colorconsole.terminal
+    colors = colorconsole.terminal.colors
     t = colorconsole.terminal.get_terminal()
-    fancy_print = t.cprint
+    def fancy_print(foreground, background, string):
+        t.cprint(foreground, background, string)
+        t.reset()
 except:
     logging.warning("Fancy print will not produce colorized output because the python library colorconsole was not found")
+    # add colors for using scripts not to fail
+    colors= { "BLACK"   : 0,
+              "BLUE"    : 1,
+              "GREEN"   : 2,
+              "CYAN"    : 3,
+              "RED"     : 4,
+              "PURPLE"  : 5,
+              "BROWN"   : 6,
+              "LGREY"   : 7,
+              "DGRAY"   : 8,
+              "LBLUE"   : 9,
+              "LGREEN"  : 10,
+              "LCYAN"   : 11,
+              "LRED"    : 12,
+              "LPURPLE" : 13,
+              "YELLOW"  : 14,
+              "WHITE"   : 15  }
     def fancy_print(fg_color, bg_color, string):
         print string

f9a86120a4f893087751c476225a48f3623aca71
Samuel Loury <konubinixweb@gmail.com>
2011-11-28 14:34:09 +0100

    Addition of a simple implementation of pushd/popd taken from http://yagodnoe.ifmo.ru/~ad/Documentation/Bash_Shell/bash3-CHP-4-SECT-5.html It is useful when dealing when some bash implementation that do not contains it (cf cygwin bash)

--- lib/lib_bash.sh
+++ lib/lib_bash.sh
@@ -295,3 +295,16 @@ konix_string_to_color_index () {
 	done
 	RES=$FUNCT_RES
 }
+
+pushd ( ) {
+    local dirname="$*"
+    DIR_STACK="$dirname;${DIR_STACK:-$PWD;}"
+    cd "${dirname:?\"missing directory name.\"}"
+    echo "$DIR_STACK"
+}
+
+popd ( ) {
+    DIR_STACK="${DIR_STACK#*;}"
+    cd "${DIR_STACK%%;*}"
+    echo "$PWD"
+}

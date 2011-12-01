8dbf030b7e968c0c7b191bf2e9eff6fb2d94471b
Samuel Loury <konubinixweb@gmail.com>
2011-12-01 12:02:47 +0100

    More visible syncing message to be seen faster in the bunch of debug messages from gpg-agent

--- bin/_konix_sync_do_recurse.sh
+++ bin/_konix_sync_do_recurse.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 IFS=$'\n'
-echo "SYNCING with $1" >&2
+cat<<EOF >&2
+##################################################
+# SYNCING with $1
+##################################################
+EOF
 
 source _konix_sync_variables.sh
-LIST_OF_SYNC_FILE="$(mktemp -t list_of_sync_file.XXXX)"
-find ./ -name "$KONIX_SYNC_MAIN_DIR_FILE" > "$LIST_OF_SYNC_FILE"
-LIST_OF_SYNC="$(< "$LIST_OF_SYNC_FILE")"
-rm "$LIST_OF_SYNC_FILE"
+# LIST_OF_SYNC_FILE="$(mktemp -t list_of_sync_file.XXXX)"
+# find ./ -name "$KONIX_SYNC_MAIN_DIR_FILE" > "$LIST_OF_SYNC_FILE"
+LIST_OF_SYNC="${KONIX_SYNC_MAIN_DIR_FILE}"
+# rm "$LIST_OF_SYNC_FILE"
 for to_sync_dir_file in $LIST_OF_SYNC
 do
     to_sync_dir="$(dirname $to_sync_dir_file)"

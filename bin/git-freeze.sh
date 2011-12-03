81519b6ce27ee43984cdc03691cb70b43aac7914
Samuel Loury <konubinixweb@gmail.com>
2011-12-04 00:35:18 +0100

    Addition of git-freeze

---
+++ bin/git-freeze.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#set -x
+print () {
+	echo "$*" >&2
+}
+print "Adding tracked files"
+git add -u
+print "Removing deleted files"
+git ls-files --deleted | while read FILE
+do
+	git rm -f "$FILE"
+done
+print "Adding untracked files"
+git ls-files -o | while read FILE
+do
+	git add "$FILE"
+done

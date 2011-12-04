6b1858b8a58c52556a6da186b67b942535814ca2
Samuel Loury <konubinixweb@gmail.com>
2011-12-04 23:42:02 +0100

    Addition of the function konix/server-buffer-still-has-client-p copied from server-kill-buffer-query-function tempbuf now graces also buffers that have clients

--- config/elfiles/config/300-KONIX_functions.el
+++ config/elfiles/config/300-KONIX_functions.el
@@ -1,6 +1,21 @@
 ;; ################################################################################
 ;; General use function
 ;; ################################################################################
+(defun konix/server-buffer-still-has-client-p ()
+  "Function extracted from `server-kill-buffer-query-function' to know of the
+current buffer still has clients"
+  (not
+   (or
+	(not server-buffer-clients)
+	(let ((res t))
+	  (dolist (proc server-buffer-clients res)
+		(when (and (memq proc server-clients)
+				   (eq (process-status proc) 'open))
+		  (setq res nil))))
+	)
+   )
+  )
+
 (defun konix/kill-all-dired-buffers()
   "Kill all dired buffers. (took from http://www.emacswiki.org/emacs/KillingBuffers#toc3)"
   (interactive)
@@ -2065,29 +2080,29 @@ The behavior I want is :
     (ediff-maybe-checkout buf-to-patch)
 
     (ediff-with-current-buffer patch-diagnostics
-      (insert-buffer-substring patch-buf)
-      (message "Applying patch ... ")
-      ;; fix environment for gnu patch, so it won't make numbered extensions
-      (setq backup-style (getenv "VERSION_CONTROL"))
-      (setenv "VERSION_CONTROL" nil)
-      (setq patch-return-code
-			(call-process-region
-			 (point-min) (point-max)
-			 shell-file-name
-			 t   ; delete region (which contains the patch
-			 t   ; insert output (patch diagnostics) in current buffer
-			 nil ; don't redisplay
-			 shell-command-switch   ; usually -c
-			 (format "%s %s %s -o '%s' %s"
-					 ediff-patch-program
-					 ediff-patch-options
-					 ediff-backup-specs
-					 konix/ediff-patch-new-filename
-					 (expand-file-name true-source-filename))
-			 ))
+							   (insert-buffer-substring patch-buf)
+							   (message "Applying patch ... ")
+							   ;; fix environment for gnu patch, so it won't make numbered extensions
+							   (setq backup-style (getenv "VERSION_CONTROL"))
+							   (setenv "VERSION_CONTROL" nil)
+							   (setq patch-return-code
+									 (call-process-region
+									  (point-min) (point-max)
+									  shell-file-name
+									  t   ; delete region (which contains the patch
+									  t   ; insert output (patch diagnostics) in current buffer
+									  nil ; don't redisplay
+									  shell-command-switch   ; usually -c
+									  (format "%s %s %s -o '%s' %s"
+											  ediff-patch-program
+											  ediff-patch-options
+											  ediff-backup-specs
+											  konix/ediff-patch-new-filename
+											  (expand-file-name true-source-filename))
+									  ))
 
-      ;; restore environment for gnu patch
-      (setenv "VERSION_CONTROL" backup-style))
+							   ;; restore environment for gnu patch
+							   (setenv "VERSION_CONTROL" backup-style))
 
     (message "Applying patch ... done")
     (message "")
@@ -2101,7 +2116,7 @@ The behavior I want is :
 		(progn
 		  (with-output-to-temp-buffer ediff-msg-buffer
 			(ediff-with-current-buffer standard-output
-			  (fundamental-mode))
+									   (fundamental-mode))
 			(princ (format
 					"Patch program has failed due to a bad patch file,
 it couldn't apply all hunks, OR
@@ -2178,8 +2193,8 @@ you can still examine the changes via M-x ediff-files"
 		   buf-to-patch target-buf nil
 		   startup-hooks 'epatch))
     (ediff-with-current-buffer ctl-buf
-      (setq ediff-patchbufer patch-buf
-			ediff-patch-diagnostics patch-diagnostics))
+							   (setq ediff-patchbufer patch-buf
+									 ediff-patch-diagnostics patch-diagnostics))
 
     (bury-buffer patch-diagnostics)
     (message "Type `P', if you need to see patch diagnostics")

4a4b4eed5b565b2606dc4c1351fe150828cc9fe3
Samuel Loury <konubinixweb@gmail.com>
2011-12-05 08:08:38 +0100

    More uniform highlight-symbol navigation hotkeys, they both use S-f3

--- config/elfiles/config/600-KONIX_hotkeys.el
+++ config/elfiles/config/600-KONIX_hotkeys.el
@@ -579,8 +579,8 @@
 ;; highlight-symbol
 ;; ####################################################################################################
 (global-set-key (kbd "<C-f3>") 'highlight-symbol-at-point)
-(global-set-key (kbd "<S-f3>") 'highlight-symbol-next)
-(global-set-key (kbd "<M-f3>") 'highlight-symbol-prev)
+(global-set-key (kbd "<C-S-f3>") 'highlight-symbol-next)
+(global-set-key (kbd "<S-f3>") 'highlight-symbol-prev)
 
 ;; ####################################################################################################
 ;; hide show with hide comments

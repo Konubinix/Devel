Konubinix/Devel

commit 35e29cde209fea2b6d67e1f0ce7225f326e44f26

Addition of anything and the keybinding fast-key-map -> a for anything-map and anything-map -> a for anything command

config/elfiles/config/600-KONIX_hotkeys.el
@@ -1,4 +1,4 @@
-;; ####################################################################################################
+>;; ####################################################################################################
 ;; Unset the keys I want to free
 ;; ####################################################################################################
 (global-unset-key (kbd "C-<"))

commit 1f9fa1359c58f464afbc900867396b92ac2d8059

Reversing the > error introduced in 35e29cde209fea2b6d67e1f0ce7225f326e44f26

config/elfiles/config/600-KONIX_hotkeys.el
@@ -1,4 +1,4 @@
->;; ####################################################################################################
+;; ####################################################################################################
 ;; Unset the keys I want to free
 ;; ####################################################################################################
 (global-unset-key (kbd "C-<"))
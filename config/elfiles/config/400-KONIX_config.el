fb18aca9b5a8778b225ba9c597f0f7bcaa21cbcf
Samuel Loury <konubinixweb@gmail.com>
2011-12-08 22:55:40 +0100

    Allow the include facility in diary

--- config/elfiles/config/400-KONIX_config.el
+++ config/elfiles/config/400-KONIX_config.el
@@ -77,6 +77,8 @@
 	)
   )
 (add-hook 'list-diary-entries-hook 'diary-sort-entries t)
+(add-hook 'diary-list-entries-hook 'diary-include-other-diary-files)
+(add-hook 'diary-mark-entries-hook 'diary-mark-included-diary-files)
 
 ;; ******************************************************************************************
 ;; Auto install custo

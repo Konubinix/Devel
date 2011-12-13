1d6d3e4becbe13a36652c1a6460c608b896cc8ef
Samuel Loury <konubinixweb@gmail.com>
2011-12-13 10:45:26 +0100

    git-freeze is more verbose

--- bin/git-freeze.sh
+++ bin/git-freeze.sh
@@ -3,16 +3,18 @@
 print () {
 	echo "$*" >&2
 }
-print "Adding tracked files"
-git add -u
-print "Removing deleted files"
+print "# Adding tracked files"
+git add -u -v
+print "# Removing deleted files"
 git ls-files --deleted | while read FILE
 do
+	print "Deleting $FILE"
 	git rm -f "$FILE"
 done
-print "Adding untracked files"
+print "# Adding untracked files"
 git ls-files -o | while read FILE
 do
+	print "Adding $FILE"
 	git add "$FILE"
 done
 git commit -m "Freezing repo"

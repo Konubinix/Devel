fea07b295a0b5f680e601c55e1a5834403babaa4
Samuel Loury <konubinixweb@gmail.com>
2012-01-07 12:24:17 +0100

    Update konix_etags_includes_updates.sh so that it not only does the update recurively but it also updates the current tags

--- bin/konix_etags_includes_updates.sh
+++ bin/konix_etags_includes_updates.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+source _konix_etags_variables.sh
 
-TAGS_INCLUDES_FILE="./TAGS_INCLUDE"
-if [ ! -f "$TAGS_INCLUDES_FILE" ]
+TAGS_INCLUDES_FILE="./$TAGS_INCLUDES_FILE_NAME"
+
+if [ -f "$TAGS_INCLUDES_FILE" ]
 then
-	echo "$TAGS_INCLUDES_FILE must exist" >&2
-	exit 1
-fi
-
-IFS=$'\n'
-for INCLUDE_FILE in $(<$TAGS_INCLUDES_FILE)
-do
-	INCLUDE_DIR="$(dirname "$INCLUDE_FILE")"
-	cat<<EOF
-##################################################
-# UPDATING THE INCLUDE DIR $INCLUDE_DIR
-##################################################
+	IFS=$'\n'
+	for INCLUDE_FILE in $(<$TAGS_INCLUDES_FILE)
+	do
+		INCLUDE_DIR="$(dirname "$INCLUDE_FILE")"
+		cat<<EOF
+# #################################################
+# Recursively update the include dir $INCLUDE_DIR
+# #################################################
 EOF
-	(
-		cd "$INCLUDE_DIR"
-		konix_etags_create.sh
-	)
-done
+		(
+			cd "$INCLUDE_DIR"
+			konix_etags_includes_updates.sh
+		)
+	done
+fi
+cat<<EOF
+# ####################################################################################################
+# Update current dir $(pwd)
+# ####################################################################################################
+EOF
+konix_etags_create.sh -v

Konubinix/Devel

commit 7e34cde4b8f9d13beb6909b61f758086b9a3d036

Move the konix_display functions in the konix_notify lib, it is then used inside the konix_display and konix_display_unique scripts

lib/konix_notify.py
@@ -1,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def by_konubinix_notificator(message,unique=False):
+    import dbus
+    session = dbus.SessionBus()
+    if unique:
+        path = "/UniqueNotification"
+    else:
+        path = "/Notification"
+    logging.info("Opening communication with "+path)
+    notification = session.get_object('konubinix.notificator',
+                                      path)
+    logging.info("Displaying message "+message)
+    notification.Notify(message)
+
+def by_pynotify(message):
+    import pynotify
+    if not pynotify.init("Message"):
+        sys.exit(1)
+    n = pynotify.Notification("Message", message)
+    n.set_timeout(3000)
+    n.show()
+
+def by_pyosd(message):
+    import pyosd
+    p = pyosd.osd("-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-1", colour="green", pos=pyosd.POS_BOT,offset=40, align=pyosd.ALIGN_CENTER)
+    p.display(message)
+
+def main(message,unique=False):
+    try:
+        by_konubinix_notificator(message,unique)
+    except:
+        try:
+            by_pynotify(message)
+        except:
+            by_pyosd(message)

bin/konix_display.py
@@ -2,45 +2,10 @@
 # -*- coding:utf-8 -*-
 import sys
 import logging
+import konix_notify
 logging.basicConfig(level=logging.DEBUG)
 
-def by_konubinix_notificator(message,unique=False):
-    import dbus
-    session = dbus.SessionBus()
-    if unique:
-        path = "/UniqueNotification"
-    else:
-        path = "/Notification"
-    logging.info("Opening communication with "+path)
-    notification = session.get_object('konubinix.notificator',
-                                      path)
-    logging.info("Displaying message "+message)
-    notification.Notify(message)
-
-def by_pynotify(message):
-    import pynotify
-    if not pynotify.init("Message"):
-        sys.exit(1)
-    n = pynotify.Notification("Message", message)
-    n.set_timeout(3000)
-    n.show()
-
-def by_pyosd(message):
-    import pyosd
-    p = pyosd.osd("-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-1", colour="green", pos=pyosd.POS_BOT,offset=40, align=pyosd.ALIGN_CENTER)
-    p.display(message)
-
-def main(message,unique=False):
-    try:
-        by_konubinix_notificator(message,unique)
-    except:
-        try:
-            by_pynotify(message)
-        except:
-            by_pyosd(message)
-
-if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        exit(1)
-    message = ' '.join(sys.argv[1:])
-    main(message)
+if len(sys.argv) <= 1:
+    exit(1)
+message = ' '.join(sys.argv[1:])
+konix_notify.main(message)

config/fluxbox/bin/konix_display_unique.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import sys
-import display
+import konix_notify
 
 if __name__ == "__main__":
 	if len(sys.argv) <= 1:
 		exit(1)
 	message = ' '.join(sys.argv[1:])
-	display.main(message,True)
+	konix_notify.main(message,True)

commit 0a25cb878adc3b48f766221772d3cb64f5ab600e

Correction of the bug introduced in 7e34cde4b8f9d13beb6909b61f758086b9a3d036, forgot to import logging in the konix_notify library

lib/konix_notify.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import logging
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
 
 def by_konubinix_notificator(message,unique=False):
     import dbus
@@ -8,10 +11,10 @@ def by_konubinix_notificator(message,unique=False):
         path = "/UniqueNotification"
     else:
         path = "/Notification"
-    logging.info("Opening communication with "+path)
+    LOGGER.info("Opening communication with "+path)
     notification = session.get_object('konubinix.notificator',
                                       path)
-    logging.info("Displaying message "+message)
+    LOGGER.info("Displaying message "+message)
     notification.Notify(message)
 
 def by_pynotify(message):
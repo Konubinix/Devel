f17bcfc0abb58cd48bcda8b220a15f059cade8ed
Samuel Loury <konubiniweb@gmail.com>
2012-01-26 13:02:57 +0100

    The feed sync script is now verbose

--- bin/konix_feed_sync.sh
+++ bin/konix_feed_sync.sh
@@ -3,7 +3,7 @@
 if [ -z "$KONIX_FEED2IMAP_SKIP" ]
 then
 # do a rss -> imap work
-	feed2imap -f "$KONIX_PERSO_DIR/feed2imaprc"
+	feed2imap -v -f "$KONIX_PERSO_DIR/feed2imaprc"
 # clean the old rss
-	feed2imap-cleaner -f "$KONIX_PERSO_DIR/feed2imaprc"
+	feed2imap-cleaner -v -f "$KONIX_PERSO_DIR/feed2imaprc"
 fi

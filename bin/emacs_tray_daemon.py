c30c997a9c5c32fa1aa839527c72b29791a88beb
Samuel Loury <konubiniweb@gmail.com>
2012-02-13 12:14:43 +0100

    Make the emacs tray daemon more general, replacing the blue and red icon names with idle and notify

--- bin/emacs_tray_daemon.py
+++ bin/emacs_tray_daemon.py
@@ -21,26 +21,19 @@ class EmacsTrayDaemon(object):
         os.mkfifo(self.fifo_file_name)
         self.fifo = os.fdopen(os.open(self.fifo_file_name, os.O_RDONLY|os.O_NONBLOCK))
 
-    def init_icons(self, icons_dir):
-        self.blue_icon_file_name = os.path.join(icons_dir, "emacs_icon_blue.png")
-        self.red_icon_file_name = os.path.join(icons_dir, "emacs_icon_red.png")
-
-        self.blue_pixbuf = gtk.gdk.pixbuf_new_from_file(self.blue_icon_file_name)
-        self.red_pixbuf = gtk.gdk.pixbuf_new_from_file(self.red_icon_file_name)
-
-    def __init__(self, fifo_file_name, period):
+    def __init__(self, fifo_file_name, period, idle_icon_file_name, notify_icon_file_name):
         """Make sure the fifo exists and is a fifo"""
         self.period = period
         # ####################################################################################################
         # Init the icons
         # ####################################################################################################
-        share_dir = os.environ.get("KONIX_SHARE_DIR",
-                                   os.path.join(os.environ["HOME"],"share"))
-        icons_dir = os.path.join(share_dir, "icons")
-        self.init_icons(icons_dir)
+        self.idle_icon_file_name = idle_icon_file_name
+        self.notify_icon_file_name = notify_icon_file_name
+        self.idle_pixbuf = gtk.gdk.pixbuf_new_from_file(self.idle_icon_file_name)
+        self.notify_pixbuf = gtk.gdk.pixbuf_new_from_file(self.notify_icon_file_name)
 
         self.tray_icon = gtk.StatusIcon()
-        self.tray_icon.set_from_pixbuf(self.blue_pixbuf)
+        self.tray_icon.set_from_pixbuf(self.idle_pixbuf)
         self.tray_icon.set_visible(True)
         # ####################################################################################################
 
@@ -50,10 +43,10 @@ class EmacsTrayDaemon(object):
         command = self.fifo.read(1)
         if command == 'b':
             LOGGER.info("No more notification")
-            self.tray_icon.set_from_pixbuf(self.blue_pixbuf)
+            self.tray_icon.set_from_pixbuf(self.idle_pixbuf)
         elif command == 'B':
             LOGGER.info("Notification coming")
-            self.tray_icon.set_from_pixbuf(self.red_pixbuf)
+            self.tray_icon.set_from_pixbuf(self.notify_pixbuf)
         elif command == 'v':
             LOGGER.info("Becoming invisible")
             self.tray_icon.set_visible(False)
@@ -79,5 +72,14 @@ class EmacsTrayDaemon(object):
         self.clean()
 
 if __name__ == "__main__":
-    etd = EmacsTrayDaemon(os.path.join(tempfile.gettempdir(), "emacs_tray_daemon_control"), EMACS_TRAY_PERIOD)
+    share_dir = os.environ.get("KONIX_SHARE_DIR",
+                               os.path.join(os.environ["HOME"],"share"))
+    icons_dir = os.path.join(share_dir, "icons")
+
+    etd = EmacsTrayDaemon(os.path.join(tempfile.gettempdir(),
+                                       "emacs_tray_daemon_control"),
+                          EMACS_TRAY_PERIOD,
+                          os.path.join(icons_dir, "emacs_icon_blue.png"),
+                          os.path.join(icons_dir, "emacs_icon_red.png")
+                          )
     etd.main()

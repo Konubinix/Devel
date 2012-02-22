af00cb6bc20f8384c2e40d7081bc0eacfabe91ef
Samuel Loury <konubiniweb@gmail.com>
2012-02-22 18:01:35 +0100

    init_lib.sh now uses PYTHON_BIN that is set up by calling file

--- init_bin/konix_init_lib.sh
+++ init_bin/konix_init_lib.sh
@@ -12,7 +12,7 @@ import_env () {
     ENV_CUSTOM_FILE="$2"
     TEMP_FILE=$(mktemp -t "temp_file_for_import_env.XXXX")
     rm -f "${TEMP_FILE}"
-    python "${HOME}/init_bin/konix_get_env.py" "$ENV_CUSTOM_FILE" 2>/dev/null| python "${HOME}/init_bin/konix_quote_env_variables.py" > "${TEMP_FILE}"
+    "$PYTHON_BIN" "${HOME}/init_bin/konix_get_env.py" "$ENV_CUSTOM_FILE" 2>/dev/null| "$PYTHON_BIN" "${HOME}/init_bin/konix_quote_env_variables.py" > "${TEMP_FILE}"
     echo "" >> "${TEMP_FILE}"
     if [ -n "$IMPORT_ENV_DEBUG" ]
     then

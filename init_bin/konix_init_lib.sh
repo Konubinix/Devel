0dd07347d6e8290b723fa7a34ae003aa682ce0bb
Samuel Loury <konubiniweb@gmail.com>
2012-02-24 17:11:03 +0100

    Move the code that initializes the env in konix_init_lib.sh instead of the generated sh_var

--- init_bin/konix_init_lib.sh
+++ init_bin/konix_init_lib.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# put the python of this platform in front of the path
+# the only hacky stuf I need is the platform
+export WANTED_PLATFORM="$(source "${HOME}/init_bin/_konix_platform.sh")"
+PATH_SEPARATOR="$(cd "${HOME}/init_bin" && "./_konix_get_default_env.py" PATH_SEPARATOR)"
+export PYTHON_BIN="$(cd "${HOME}/init_bin" && "./_konix_get_default_env.py" PYTHON_BIN)"
+export PYTHON_PATH="$(cd "${HOME}/init_bin" && ./konix_dirname.py "$PYTHON_BIN")"
+# now, I am sure the python bin is set before running the init_lib
+# Import env variables
+
 # ####################################################################################################
 # Function needed to import my custom env
 # ####################################################################################################
--- install_shell.py
+++ install_shell.py
@@ -12,14 +12,6 @@ def install_shell():
         # ####################################################################################################
         SHRC_VAR_FILE_NAME = os.path.join(environ["HOME"], ".shrc_var")
         SHRC_VAR_FILE_CONTENT = """#!/bin/bash
-# put the python of this platform in front of the path
-# the only hacky stuf I need is the platform
-export WANTED_PLATFORM="$(source "${HOME}/init_bin/_konix_platform.sh")"
-PATH_SEPARATOR="$(cd "${HOME}/init_bin" && "./_konix_get_default_env.py" PATH_SEPARATOR)"
-export PYTHON_BIN="$(cd "${HOME}/init_bin" && "./_konix_get_default_env.py" PYTHON_BIN)"
-export PYTHON_PATH="$(cd "${HOME}/init_bin" && ./konix_dirname.py "$PYTHON_BIN")"
-# now, I am sure the python bin is set before running the init_lib
-# Import env variables
 source "${HOME}/init_bin/konix_init_lib.sh"
 # importing custom environnement variables
 import_env

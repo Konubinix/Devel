decf1e42d14e3cbbe11598f0151780ee71085c13
Samuel Loury <konubiniweb@gmail.com>
2012-03-01 17:59:04 +0100

    Addition of konix_org_pomodoro_tray_daemon and make use of it from the emacs mode

---
+++ bin/konix_pomodorow_tray_daemon.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import tempfile
+
+from konix_tray_daemon import TrayDaemon
+
+def main():
+    """
+    """
+    share_dir = os.environ.get("KONIX_SHARE_DIR",
+                               os.path.join(os.environ["HOME"],"share"))
+    icons_dir = os.path.join(share_dir, "icons")
+
+    mtd = TrayDaemon(os.path.join(tempfile.gettempdir(),
+                                       "pomodorow_tray_daemon_control"),
+                          200,
+                     {"i" : os.path.join(icons_dir, "pomodorow_idle.png"),
+                      "r" : os.path.join(icons_dir, "pomodorow_rings.png"),
+                      "b" : os.path.join(icons_dir, "pomodorow_end_break.png")
+                      },
+                     "i"
+                     )
+    mtd.main()
+
+if __name__ == "__main__":
+    main()

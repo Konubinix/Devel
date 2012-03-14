1d494708888ba6f59e693b9ddea9d807d0b6e27a
Samuel Loury <konubiniweb@gmail.com>
2012-03-14 16:27:20 +0100

    Adaptation of the import_env function so that they don't use the KONIX_ENV_DONE anymore

--- init_bin/konix_import_env.el
+++ init_bin/konix_import_env.el
@@ -1,10 +1,6 @@
-(defun konix/load-env-file (&optional force file)
-  (interactive "P")
+(defun konix/load-env-file (&optional file)
+  (interactive)
   (with-temp-buffer
-	(when force
-	  (setenv "KONIX_ENV_DONE")
-	  (message "Forcing the environment loading ")
-	  )
 	(call-process  python-bin nil (list t nil) nil (expand-file-name
 													 "~/init_bin/konix_get_env.py"
 													 )

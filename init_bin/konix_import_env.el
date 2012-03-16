196429d85aa97a978b0b48ffb9997c8d294168a6
Samuel Loury <konubiniweb@gmail.com>
2012-03-16 17:12:29 +0100

    konix/load-env-file now may be given a file interactively

--- init_bin/konix_import_env.el
+++ init_bin/konix_import_env.el
@@ -1,10 +1,10 @@
 (defun konix/load-env-file (&optional file)
-  (interactive)
+  (interactive "fEnv file : ")
   (with-temp-buffer
 	(call-process  python-bin nil (list t nil) nil (expand-file-name
 													 "~/init_bin/konix_get_env.py"
 													 )
-				  (if file file "")
+				  (if file (expand-file-name file) "")
 				  )
 	(goto-char (point-min))
 	(while (re-search-forward "^\\([^=]+\\)=\"\\(.+\\)\"$" nil t)

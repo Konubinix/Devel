a8f4467c2b3accbb28141e4ea03c2579d1c04a3a
Samuel Loury <konubinixweb@gmail.com>
2012-04-22 15:01:49 +0200

    Do not use truncate when getting time of today because it fails on 32 bits systems

--- config/elfiles/config/300-KONIX_functions.el
+++ config/elfiles/config/300-KONIX_functions.el
@@ -44,7 +44,12 @@
 (defun konix/insert-seconds-since-1970 ()
   "insert the number of second since the 00:00 1/1/1970"
   (interactive)
-  (insert (format "%s" (truncate (float-time))))
+  (insert
+   (replace-regexp-in-string
+	"\\(.+\\)\\..+" "\\1"
+	(int-to-string(float-time))
+	)
+   )
   )
 
 (defun konix/readlines (filename &optional empty_line_as_well)
@@ -2293,29 +2298,29 @@ The behavior I want is :
     (ediff-maybe-checkout buf-to-patch)
 
     (ediff-with-current-buffer patch-diagnostics
-	  (insert-buffer-substring patch-buf)
-	  (message "Applying patch ... ")
-	  ;; fix environment for gnu patch, so it won't make numbered extensions
-	  (setq backup-style (getenv "VERSION_CONTROL"))
-	  (setenv "VERSION_CONTROL" nil)
-	  (setq patch-return-code
-			(call-process-region
-			 (point-min) (point-max)
-			 shell-file-name
-			 t   ; delete region (which contains the patch
-			 t   ; insert output (patch diagnostics) in current buffer
-			 nil ; don't redisplay
-			 shell-command-switch   ; usually -c
-			 (format "%s %s %s -o '%s' %s"
-					 ediff-patch-program
-					 ediff-patch-options
-					 ediff-backup-specs
-					 konix/ediff-patch-new-filename
-					 (expand-file-name true-source-filename))
-			 ))
+							   (insert-buffer-substring patch-buf)
+							   (message "Applying patch ... ")
+							   ;; fix environment for gnu patch, so it won't make numbered extensions
+							   (setq backup-style (getenv "VERSION_CONTROL"))
+							   (setenv "VERSION_CONTROL" nil)
+							   (setq patch-return-code
+									 (call-process-region
+									  (point-min) (point-max)
+									  shell-file-name
+									  t   ; delete region (which contains the patch
+									  t   ; insert output (patch diagnostics) in current buffer
+									  nil ; don't redisplay
+									  shell-command-switch   ; usually -c
+									  (format "%s %s %s -o '%s' %s"
+											  ediff-patch-program
+											  ediff-patch-options
+											  ediff-backup-specs
+											  konix/ediff-patch-new-filename
+											  (expand-file-name true-source-filename))
+									  ))
 
-	  ;; restore environment for gnu patch
-	  (setenv "VERSION_CONTROL" backup-style))
+							   ;; restore environment for gnu patch
+							   (setenv "VERSION_CONTROL" backup-style))
 
     (message "Applying patch ... done")
     (message "")
@@ -2329,7 +2334,7 @@ The behavior I want is :
 		(progn
 		  (with-output-to-temp-buffer ediff-msg-buffer
 			(ediff-with-current-buffer standard-output
-			  (fundamental-mode))
+									   (fundamental-mode))
 			(princ (format
 					"Patch program has failed due to a bad patch file,
 it couldn't apply all hunks, OR
@@ -2406,8 +2411,8 @@ you can still examine the changes via M-x ediff-files"
 		   buf-to-patch target-buf nil
 		   startup-hooks 'epatch))
     (ediff-with-current-buffer ctl-buf
-	  (setq ediff-patchbufer patch-buf
-			ediff-patch-diagnostics patch-diagnostics))
+							   (setq ediff-patchbufer patch-buf
+									 ediff-patch-diagnostics patch-diagnostics))
 
     (bury-buffer patch-diagnostics)
     (message "Type `P', if you need to see patch diagnostics")

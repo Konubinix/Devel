fb53d961c5b358d6e4719ce910c401f72d9ce43b
Samuel Loury <konubiniweb@gmail.com>
2012-04-27 17:45:29 +0200

    Addition of the installation of the gdbinit and stl viewer

---
+++ install_gdbinit.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+import shutil
+
+from install_lib import *
+
+def install_gdbinit():
+    environ = get_environ()
+    # ####################################################################################################
+    # Generate a gdbinit file that sources the config ones
+    # ####################################################################################################
+    GDBINIT_FILE_NAME=os.path.expanduser("~/.gdbinit")
+    gdbinit_config=os.path.join(environ["KONIX_CONFIG_DIR"], "gdbinit").replace(" ", r"\ ")
+    gdbinit_stl_views=os.path.join(environ["KONIX_CONFIG_DIR"], "stl-views.gdb").replace(" ", r"\ ")
+    if os.path.exists(GDBINIT_FILE_NAME):
+        os.remove(GDBINIT_FILE_NAME)
+    with open(GDBINIT_FILE_NAME, "w") as gdbinitfile:
+        gdbinitfile.write('source %s\nsource %s\n' % (gdbinit_stl_views, gdbinit_config))
+
+    print "Successful installed gdbinit config"
+if __name__ == '__main__':
+    install_gdbinit()

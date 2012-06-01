239ad0f0a3df249bb73317eecb723903912180b5
Samuel Loury <konubiniweb@gmail.com>
2012-06-01 17:57:28 +0200

    Addition of a git log pick axe function

--- config/elfiles/config/600-KONIX_hotkeys.el
+++ config/elfiles/config/600-KONIX_hotkeys.el
@@ -684,6 +684,7 @@
 (define-key konix/git-global-map "l" 'konix/git-global-map-log)
 (define-key konix/git-global-map-log "r" 'konix/git/reflog)
 (define-key konix/git-global-map-log "l" 'konix/git/log)
+(define-key konix/git-global-map-log "p" 'konix/git/log/pick-axe)
 (define-key konix/git-global-map-log "f" 'konix/git/log/file)
 (define-key konix/git-global-map-log "b" 'konix/git/blame/file)
 (define-key konix/git-global-map-log "a" 'konix/git/alog)

eecce16e080c90ed86332e068a30246fbee78dc5
Samuel Loury <konubiniweb@gmail.com>
2012-07-12 17:36:17 +0200

    Big hack to handle cid in mails. the _konix_mail_content_id_adjust.py script now prints on the stdout the sed script to run on the final html file so that the cid: will be rename with the filename. It also saves the cid files into a directory given as parameter by the konix_view_html script.

--- bin/_konix_mail_content_id_adjust.py
+++ bin/_konix_mail_content_id_adjust.py
@@ -4,29 +4,48 @@
 from email.parser import FeedParser
 import sys
 import re
+import os
 
 PARSER = FeedParser()
 # open the mail file
 DATA = sys.stdin.read()
 PARSER.feed(DATA)
+CID_DIR=sys.argv[1]
 
 REPLACE_STUFFS=[]
 PARTS_TO_PARSE = [PARSER.close()]
+
+def getValueIgnoreCase(key, dico):
+    for dico_key in dico.keys():
+        if re.search(key, dico_key, re.I):
+            return dico[dico_key]
+    return None
+
 while len(PARTS_TO_PARSE) != 0:
     PART = PARTS_TO_PARSE.pop(0)
-    if "Content-ID" in PART.keys():
-        assert 'Content-Type' in PART.keys()
-        NAME = re.search('name="([^"]+)"', PART['Content-Type'])
-        CID = re.search('^<(.+)>$', PART['Content-ID'])
-        assert NAME
+    if getValueIgnoreCase("content-id", PART):
+        assert getValueIgnoreCase('Content-Type', PART)
+        NAME = re.search('name="([^"]+)"', getValueIgnoreCase('Content-Type',
+                                                              PART), re.I)
+        CID = re.search('^<(.+)>$', getValueIgnoreCase('Content-ID', PART), re.I)
+        TYPE = re.search('^[^/]+/([^ ]+)', getValueIgnoreCase('Content-Type',
+                                                                           PART),
+                         re.I)
+        if NAME:
+            NAME = NAME.group(1)
+        else:
+            NAME = CID.group(1)+ '.' + TYPE.group(1)
+        # write the attachment into the temp dir
+        f = open(os.path.join(CID_DIR, NAME), "wb")
+        f.write(PART.get_payload(None, True))
+        f.close()
         REPLACE_STUFFS.append(['cid:%s' % (CID.group(1)),
-                              NAME.group(1),])
+                              NAME,])
     PAYLOAD = PART.get_payload()
     if type(PAYLOAD) == list:
         PARTS_TO_PARSE = PARTS_TO_PARSE + PAYLOAD
+print "#!/bin/bash"
 for REPLACE_STUFF in REPLACE_STUFFS:
     FROM=REPLACE_STUFF[0]
     TO=REPLACE_STUFF[1]
-    sys.stderr.write(FROM+" "+TO+"\n")
-    DATA = re.sub(FROM, TO, DATA)
-print DATA
+    print "sed s/%s/%s/g" % (FROM, TO)
--- bin/konix_view_html.sh
+++ bin/konix_view_html.sh
@@ -22,13 +22,13 @@ tee "${HOME}/konix_view_html.msg" | {
 	#
 	# And somewhere in the mail something like cid:A, I must replace it by the
 	# associated FILE name
-	cat "$dir"/msg | _konix_mail_content_id_adjust.py  > "$dir"/msg2
+	cat "$dir"/msg | _konix_mail_content_id_adjust.py "$dir" > "$dir"/sed
 
-	if munpack -C "$dir" -t < "$dir"/msg2 2>&1 | tee "${OUT_FILE}" | grep -q 'Did not find'
+	if munpack -C "$dir" -t < "$dir"/msg 2>&1 | tee "${OUT_FILE}" | grep -q 'Did not find'
 	then
 	    # failed to unpack the mail, try to do it manually
-		sed -n '/[Hh][Tt][Mm][Ll]/,$p' "$dir"/msg2 > $dir/part1.html
-		rm "$dir"/msg2
+		sed -n '/[Hh][Tt][Mm][Ll]/,$p' "$dir"/msg > $dir/part1.html
+		rm "$dir"/msg
 	else
 		echo "Successfully munpacked msg" >> "${OUT_FILE}"
 		# rename the partN file to get partN.content_type
@@ -48,7 +48,9 @@ tee "${HOME}/konix_view_html.msg" | {
 		echo "nothing found munpacked" >> "${OUT_FILE}"
 	fi
 	for i in "$dir"/part*.[Hh][Tt][Mm][Ll]; do
-		x-www-browser "$i" &
+        i_new="$i.new.html"
+        cat "$i" | bash "$dir"/sed > "${i_new}"
+		x-www-browser "${i_new}" &
 		sleep 3
 		exit 0
 	done

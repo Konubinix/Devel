dee3aea4fc87c2e29e72b97789a999d474308e59
Samuel Loury <konubiniweb@gmail.com>
2012-07-19 10:15:44 +0200

    Start popwin only in case the client connects to the server because starting it at emacs start conflicts with the daemon

--- config/elfiles/config/400-KONIX_config.el
+++ config/elfiles/config/400-KONIX_config.el
@@ -570,8 +570,3 @@
   (setq explicit-bash-args '("-c" "export EMACS=; stty echo; bash"))
   (setq comint-process-echoes t)
   )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Use popwin to have temporary buffer less intrusives ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq-default display-buffer-function 'popwin:display-buffer)

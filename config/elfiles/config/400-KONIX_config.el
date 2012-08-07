83cd893fe9f00d62c737d9cfde3ca64c91d844c3
Samuel Loury <konubinixweb@gmail.com>
2012-08-07 23:00:12 +0200

    Default browsing with uzbl

--- config/elfiles/config/400-KONIX_config.el
+++ config/elfiles/config/400-KONIX_config.el
@@ -575,4 +575,5 @@
   (setq explicit-shell-file-name "bash")
   (setq explicit-bash-args '("-c" "export EMACS=; stty echo; bash"))
   (setq comint-process-echoes t)
+  (setq browse-url-browser-function 'konix/browse-url-uzbl)
   )

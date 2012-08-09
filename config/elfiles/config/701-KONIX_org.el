1cd43ec65c11f910b4e365245f829dac0fc13c4c
Samuel Loury <konubiniweb@gmail.com>
2012-08-09 12:00:54 +0200

    Make the project tag a non inherited one

--- config/elfiles/config/701-KONIX_org.el
+++ config/elfiles/config/701-KONIX_org.el
@@ -358,6 +358,7 @@ cursor stays in the org buffer."
 				("@phone" . ?o)
 				)
 			  )
+(setq-default org-tags-exclude-from-inheritance '("project"))
 (setq-default org-fast-tag-selection-single-key nil)
 (setq-default org-reverse-note-order t)
 (setq org-tag-alist nil)

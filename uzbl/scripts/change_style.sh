f5232b824c27645c02e71d7387d0651a000b320b
Samuel Loury <konubiniweb@gmail.com>
2012-08-10 17:41:14 +0200

    Update change_style so that it takes XDG_CONFIG_HOME instead of XDG_DATA_HOME because custom css are more part of a config for me than data

--- uzbl/scripts/change_style.sh
+++ uzbl/scripts/change_style.sh
@@ -6,12 +6,11 @@
 # I hope someone finds it useful. All comments and suggestions
 # are welcome.
 
-XDG_DATA_HOME=${XDG_DATA_HOME:-"$HOME/.local/share/"}
-
 # Set the path to the .css files
 # Every custom css style must be in a file named web_address.css
 # e.g. reddit.com.css
-STYLE_PATH=$XDG_DATA_HOME/uzbl/css
+UZBL_CONFIG=$XDG_CONFIG_HOME/uzbl
+STYLE_PATH=$UZBL_CONFIG/css
 
 STYLESHEET_SET=0
 
@@ -24,9 +23,5 @@ for i in $STYLE_PATH/*.css; do
         break
     fi
 done
-
-if [ $STYLESHEET_SET -eq 0 ]; then
-    echo "set stylesheet_uri = file://${STYLE_PATH}/default.css" > "$UZBL_FIFO"
-fi
 
 exit 0

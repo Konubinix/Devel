d7d00e9417695f7f24c815b64f76aff718ed31a2
Samuel Loury <konubiniweb@gmail.com>
2012-09-03 10:06:20 +0200

    Review the Effort_ALL global property to consider a broader range of efforts

--- config/elfiles/config/701-KONIX_org.el
+++ config/elfiles/config/701-KONIX_org.el
@@ -372,7 +372,7 @@ to be organized.
 (setq-default org-export-mark-todo-in-toc t)
 (setq-default org-export-with-tags t)
 (setq-default org-global-properties
-			  '(("Effort_ALL". "0:30 1:00 1:30 2:00 2:30 3:00 3:30 4:00")))
+			  '(("Effort_ALL". "0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00")))
 (setq-default org-hide-block-startup t)
 (setq-default org-hide-leading-stars t)
 (setq-default org-hierarchical-todo-statistics nil)

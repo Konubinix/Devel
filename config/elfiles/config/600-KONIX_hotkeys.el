110905eaeb3487e02f9dd52d4500be36aba3b972
Samuel Loury <konubiniweb@gmail.com>
2012-09-13 14:40:45 +0200

    Addition of a command to copy current kill to the X clipboard. Bind it to C-S-Y

--- config/elfiles/config/600-KONIX_hotkeys.el
+++ config/elfiles/config/600-KONIX_hotkeys.el
@@ -21,6 +21,7 @@
 ;; yank pop to more recent kill
 (define-key global-map (kbd "M-Y") 'konix/yank-pop-more-recent)
 (define-key global-map (kbd "C-M-y") 'konix/kill-ring-insert)
+(define-key global-map (kbd "C-S-Y") 'konix/kill-ring-to-clipboard)
 
 ;; multi occur
 (global-set-key (kbd "M-s M-o") 'multi-occur-in-matching-buffers)

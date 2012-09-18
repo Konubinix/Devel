056f6c593c7f4b2436d6934e5a2fd01209f5e32f
Samuel Loury <konubiniweb@gmail.com>
2012-09-18 09:50:19 +0200

    Addition of foreach in the cmake keywords

--- config/elfiles/config/700-KONIX_modes.el
+++ config/elfiles/config/700-KONIX_modes.el
@@ -2557,8 +2557,8 @@ GOT FROM : my-track-switch-buffer in https://github.com/antoine-levitt/perso/blo
 ;; ####################################################################################################
 ;; cmake
 ;; ####################################################################################################
-(defvar konix/cmake-beginning-of-defun "\\b\\(macro\\|function\\|if\\|else\\)(" "")
-(defvar konix/cmake-end-of-defun "\\b\\(end\\(macro\\|function\\|if\\)\\|else([^)]*)\\)" "")
+(defvar konix/cmake-beginning-of-defun "\\b\\(foreach\\|macro\\|function\\|if\\|else\\)(" "")
+(defvar konix/cmake-end-of-defun "\\b\\(end\\(foreach\\|macro\\|function\\|if\\)\\|else([^)]*)\\)" "")
 
 (defun konix/cmake-forward-sexp (&rest args)
   ;; the closing sexp is an end of defun with no beginning of defun behind

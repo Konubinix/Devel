b715b904e0362b25c953a053e25cbade8c1f7dca
Samuel Loury <konubinixweb@gmail.com>
2012-10-11 14:50:28 +0200

    Use gnutls-cli instead of openssl, it seems to work faster and more often

--- bin/konix_cert_dump.sh
+++ bin/konix_cert_dump.sh
@@ -2,7 +2,12 @@
 
 konix_cert_show.sh "$@" 2>/dev/null| \
     sed -n -e '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' \
-    -e '/i:/ {
-s/^.\+\(i\):/# \1/
+    -e '/^ - subject / {
+s/^ - /# /
 p
 }'
+#     sed -n -e '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' \
+#     -e '/i:/ {
+# s/^.\+\(i\):/# \1/
+# p
+# }'

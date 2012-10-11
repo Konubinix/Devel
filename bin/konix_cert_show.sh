eeb5a3cff03fdf2e603257ea755760cc16a07db6
Samuel Loury <konubinixweb@gmail.com>
2012-10-11 14:50:28 +0200

    Use gnutls-cli instead of openssl, it seems to work faster and more often

--- bin/konix_cert_show.sh
+++ bin/konix_cert_show.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo| openssl s_client -showcerts -connect "$@"
+#echo| openssl s_client -showcerts -CAfile "${KONIX_CA_FILE}" -connect "$@"
+echo| gnutls-cli --x509cafile "$KONIX_CA_FILE" --print-cert "$@"

f576cf94e6714f1a45ca8cc8bffb70d645de2608
Samuel Loury <konubinixweb@gmail.com>
2012-10-11 13:21:09 +0200

    Better namespace handling

---
+++ bin/konix_cert_dump.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+konix_cert_show.sh "$@" 2>/dev/null| \
+    sed -n -e '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' \
+    -e '/i:/ {
+s/^.\+\(i\):/# \1/
+p
+}'
--- bin/konix_dump_cert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-konix_show_cert.sh "$@" 2>/dev/null| sed -n '/BEGIN CERTIFICATE/,/END CERTIFICATE/p'

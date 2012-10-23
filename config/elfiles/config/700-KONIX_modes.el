f46ab397d5f56e81ebaf9ac5f5b9108a63375046
Samuel Loury <konubinixweb@gmail.com>
2012-10-23 15:34:30 +0200

    only go to Monospace 8 when in emacs24

--- config/elfiles/config/700-KONIX_modes.el
+++ config/elfiles/config/700-KONIX_modes.el
@@ -2249,7 +2249,9 @@ inspired from `notmuch-show-archive-thread-internal'"
   "Variable to indicate if it is the first time a client visits the daemon"
   )
 (defun konix/server-visit-hook ()
-  (set-default-font "Monospace 8")
+  (when (equal emacs-major-version 24)
+   (set-default-font "Monospace 8")
+   )
   (maxframe/maximize-frame)
   (when konix/first-visit
 	(pop-to-buffer "*Messages*")

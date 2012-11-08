d608f4e0d92d56617e18a2324c5ce3dcaae9889a
Samuel Loury <konubinixweb@gmail.com>
2012-11-08 18:31:18 +0100

    Addition of a vim install

---
+++ install_vim.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from install_lib import *
+
+def install_vim():
+    environ = get_environ()
+    replace_file_content(os.path.join(environ["HOME"] , ".vimrc"),
+                         "source %(KONIX_CONFIG_DIR)s/vimrc" % environ)
+
+    print "Successfully installed vim config"
+
+if __name__ == "__main__":
+    install_vim()

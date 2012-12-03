561b0c03c482bd39da94918b4553cacaf8211519
Samuel Loury <konubinixweb@gmail.com>
2012-12-03 15:11:46 +0100

    Allow the suffix p for pomodoro in times indicating 30 minutes time ranges.
    Also, start the effort global properties to 1 instead of 0

--- config/elfiles/config/701-KONIX_org.el
+++ config/elfiles/config/701-KONIX_org.el
@@ -871,8 +871,8 @@ to be organized.
 (setq-default org-global-properties
 			  '(
 				("Effort_ALL". "0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00")
-				("POMODORO_ALL". "0 1 2 3 4 5 6 7 8 9")
-				("DONE_POMODORO_ALL". "0 1 2 3 4 5 6 7 8 9")
+				("POMODORO_ALL". "1 2 3 4 5 6 7 8 9 0")
+				("DONE_POMODORO_ALL". "0 1 2 3 4 5 6 7 8 9 0")
 				))
 (setq-default org-hide-block-startup t)
 (setq-default org-hide-leading-stars t)
@@ -1129,6 +1129,7 @@ to be organized.
 (setq-default org-agenda-cmp-user-defined 'konix/org-cmp-deadlines-past-and-due-first)
 (setq-default org-agenda-compact-blocks nil)
 (setq-default org-agenda-columns-add-appointments-to-effort-sum t)
+(add-to-list 'org-effort-durations '("p" . 30))
 (setq-default org-agenda-sorting-strategy
 			  '(
 				;; Strategy for Weekly/Daily agenda

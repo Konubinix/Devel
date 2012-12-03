a8fa26c95b9cf182c33c7097b161b623ad9ca22b
Samuel Loury <konubinixweb@gmail.com>
2012-12-03 14:01:00 +0100

    Have the konix_mail_sync script exit prematurely rather than allowing in to continue in a undefinite state after a crash somewhere

--- bin/konix_mail_sync.sh
+++ bin/konix_mail_sync.sh
@@ -12,15 +12,15 @@ MAIL_TRAY_DAEMON_CTRL="/tmp/mail_tray_daemon_control"
 echo "?" > "$MAIL_TRAY_DAEMON_CTRL"
 echo "b" > "$MAIL_TRAY_DAEMON_CTRL"
 # make notmuch db consistent (earlier removed mail files etc)
-notmuch new --verbose
+notmuch new --verbose || exit 1
 
 #sync with imap server
-offlineimap -c "$KONIX_OFFLINEIMAPRC"
+offlineimap -c "$KONIX_OFFLINEIMAPRC" || exit 1
 #get messages from pop server
 # getmail
 
 #finally reflect externally changed maildir flags in notmuch tags
-notmuch new
+notmuch new || exit 1
 
 #get messages from the spool, but after notmuch new because I don't want to be
 #informed the cron jobs that did $0 ended...
@@ -30,10 +30,10 @@ notmuch new
 # mb2md -s "/var/spool/mail/${LOGNAME}" -d "${HOME}/Mail/mail.spool_${LOGNAME}"
 
 # this is useless since 0.5 -> notmuchsync -d -r --all --sync-deleted-tag
-konix_mail_init_tags.sh
-konix_mail_unnew.sh
+konix_mail_init_tags.sh || exit 1
+konix_mail_unnew.sh || exit 1
 
-konix_mail_tray_daemon_update.sh -d
+konix_mail_tray_daemon_update.sh -d || exit 1
+
 echo "B" > "$MAIL_TRAY_DAEMON_CTRL"
-
 echo "" > /tmp/konix_mail_tray_stamp

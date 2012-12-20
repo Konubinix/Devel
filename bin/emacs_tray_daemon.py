3dd66a88c45bfad61e28ae14567782feb75acd9f
Samuel Loury <konubinixweb@gmail.com>
2012-12-20 16:40:49 +0100

    Addition of the black emacs icon indicating an irc server has been disconnected

--- bin/emacs_tray_daemon.py
+++ bin/emacs_tray_daemon.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
                      {"i" : os.path.join(icons_dir, "emacs_icon_blue.png"),
                       "n" : os.path.join(icons_dir, "emacs_icon_yellow.png"),
                       "N" : os.path.join(icons_dir, "emacs_icon_red.png")
+                      "D" : os.path.join(icons_dir, "emacs_icon_black.png")
                       },
                      "i"
                      )

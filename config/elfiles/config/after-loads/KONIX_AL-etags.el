efbe124d4a76f2ea545e4f305490fb872f719542
Samuel Loury <konubinixweb@gmail.com>
2013-01-07 17:21:46 +0100

    Addition of konix/tag-find-file-of-tag, redefining tag-find-file-of-tag but using pop-to-buffer instead of switch-to-buffer, less intuitive

---
+++ config/elfiles/config/after-loads/KONIX_AL-etags.el
@@ -0,0 +1,34 @@
+;;; KONIX_AL-etags.el ---
+
+;; Copyright (C) 2013  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun konix/tag-find-file-of-tag (file)
+  "Reimplement `tag-find-file-of-tag' using `pop-to-buffer' instead of `switch-to-buffer'."
+  (let ((buf (tag-find-file-of-tag-noselect file)))
+	(pop-to-buffer buf)))
+
+(defalias 'tag-find-file-of-tag 'konix/tag-find-file-of-tag)
+(provide 'KONIX_AL-etags)
+;;; KONIX_AL-etags.el ends here

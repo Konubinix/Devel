eca624150602e89b2d48cc4dfbca1f3be6d9ab27
Samuel Loury <konubinixweb@gmail.com>
2013-01-09 17:33:14 +0100

    Make konix_icals2orgs.sh more configurable

--- bin/konix_icals2orgs.sh
+++ bin/konix_icals2orgs.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-cat "$KONIX_PERSO_DIR"/icals/* | ical2org > "$KONIX_PERSO_DIR/wiki/icals.org"
+SRC="$KONIX_PERSO_DIR/icals"
+DST="$KONIX_PERSO_DIR/wiki/icals.org"
+
+usage () {
+    cat<<EOF
+%0 -h -s SRC -d DST
+SRC : $SRC
+DST : $DST
+EOF
+}
+
+while getopts "hs:d:" opt; do
+    case $opt in
+        h)
+            usage
+            exit 0
+            ;;
+        s)
+            SRC="$OPTARG"
+            ;;
+        d)
+            DST="$OPTARG"
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+cat "$SRC"/* | ical2org > "$DST"

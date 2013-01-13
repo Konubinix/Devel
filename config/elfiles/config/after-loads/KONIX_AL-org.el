710ec6162e32b2ca8a8407ba5652ba9adcac2200
Samuel Loury <konubinixweb@gmail.com>
2013-01-13 18:30:50 +0100

    Do not export with sub or super scripts

--- config/elfiles/config/after-loads/KONIX_AL-org.el
+++ config/elfiles/config/after-loads/KONIX_AL-org.el
@@ -880,6 +880,7 @@ to be organized.
 (setq-default org-export-headline-levels 10)
 (setq-default org-export-mark-todo-in-toc t)
 (setq-default org-export-with-tags t)
+(setq-default org-export-with-sub-superscripts nil)
 (setq-default org-global-properties
 			  '(
 				("Effort_ALL". "0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00")

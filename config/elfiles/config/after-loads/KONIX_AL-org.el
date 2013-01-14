e5383382cdf75668b99fdeb63051f885b62bcc98
Samuel Loury <konubinixweb@gmail.com>
2013-01-14 19:29:51 +0100

    Add some finer grain value for effort because there are sometimes task that need quite less than a pomodoro

--- config/elfiles/config/after-loads/KONIX_AL-org.el
+++ config/elfiles/config/after-loads/KONIX_AL-org.el
@@ -883,7 +883,7 @@ to be organized.
 (setq-default org-export-with-sub-superscripts nil)
 (setq-default org-global-properties
 			  '(
-				("Effort_ALL". "0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00")
+				("Effort_ALL". "0:05 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00")
 				("POMODORO_ALL". "1 2 3 4 5 6 7 8 9 0")
 				("DONE_POMODORO_ALL". "0 1 2 3 4 5 6 7 8 9 0")
 				))

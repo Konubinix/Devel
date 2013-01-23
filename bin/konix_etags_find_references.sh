c64d5b98ab836acdbeba469e0baec04e118847d7
Samuel Loury <konubinixweb@gmail.com>
2013-01-23 18:18:06 +0100

    The konix_etags_find_references script now searches in the included tags recursively and ignore the case

--- bin/konix_etags_find_references.sh
+++ bin/konix_etags_find_references.sh
@@ -3,12 +3,16 @@
 TAGS_FILES=TAGS
 
 usage () {
-    echo "$0 [-h] [-t TAGS_FILES] expressions..."
+    echo "$0 [-h] [-r] [-i] [-t TAGS_FILES] expressions..."
     echo "\t-h\t: display this usage help"
+    echo "\t-r\t: recursive search"
+    echo "\t-i\t: ignore case"
     echo "\t-t <TAGS_FILES>\t: comma separated tags files, default to 'TAGS'"
 }
 
-while getopts "ht:" opt; do
+RECURSIVE=""
+CASE=""
+while getopts "hrit:" opt; do
     case $opt in
         h)
             usage
@@ -16,6 +20,12 @@ while getopts "ht:" opt; do
             ;;
         t)
             TAGS_FILES="$OPTARG"
+            ;;
+        r)
+            RECURSIVE="1"
+            ;;
+        i)
+            CASE="I"
             ;;
     esac
 done
@@ -30,8 +40,27 @@ then
     usage
     exit 3
 fi
+ALL_TAGS_FILES="${TAGS_FILES//,/
+}"
 
-IFS=$',' # in order to separate files
+# recursively find all the tags files
+if [ -n "$RECURSIVE" ]
+then
+    IFS=$',' # in order to separate files
+    for tag in $TAGS_FILES
+    do
+        new_tags=`konix_etags_list_tags_included.sh -i "$tag"`
+        if [ "$?" != "0" ]
+        then
+            echo "konix_etags_list_tags_included.sh -i $tag failed"
+            exit 1
+        fi
+        ALL_TAGS_FILES="${ALL_TAGS_FILES}
+$new_tags"
+    done
+fi
+
+IFS=$'\n'
 sed -n -e "
 # record the file name
 /^$/ {
@@ -39,10 +68,10 @@ sed -n -e "
    s/^\(.\+\),[0-9]\+$/\1/
    h
 }
-/^.\+$REF.\+$/ {
+/^.\+$REF.\+$/$CASE {
    /[]/ ! d
    G
    s/^\(.*\)\(.\+\)\(.\+\),\(.\+\)\n\(.\+\)$/\5:\3:\1 (\2, \4)/
    p
 }
-" $TAGS_FILES
+" $ALL_TAGS_FILES

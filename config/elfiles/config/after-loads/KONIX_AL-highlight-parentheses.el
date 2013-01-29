ec61045d0a71a130c0cc5678edee22b9fbf46d4c
Samuel Loury <konubinixweb@gmail.com>
2013-01-29 17:58:40 +0100

    Highlight the background of the parenthesis instead of the foreground to make them more apparent.

---
+++ config/elfiles/config/after-loads/KONIX_AL-highlight-parentheses.el
@@ -0,0 +1,38 @@
+;;; KONIX_AL-highlight-parentheses.el ---
+
+;; Copyright (C) 2013  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq-default
+ hl-paren-background-colors
+ '("salmon" "light pink" "orange1" "SpringGreen1")
+ )
+
+(setq-default
+ hl-paren-colors
+ nil
+ )
+
+(provide 'KONIX_AL-highlight-parentheses)
+;;; KONIX_AL-highlight-parentheses.el ends here

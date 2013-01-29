1589f223261130f7a7a599e75589f069e6bba47d
Samuel Loury <konubinixweb@gmail.com>
2013-01-29 18:32:47 +0100

    konix_emacs_open.sh now open editor:// scheme

--- bin/konix_emacs_open.sh
+++ bin/konix_emacs_open.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-konix_emacs_open_w3m.sh "$@"
+uri="$1"
+if [[ "$uri" =~ "editor://" ]]
+then
+    filename=`echo $uri|sed 's/.\+#file=\(.\+\)/\1/'`
+    ec "$filename"
+else
+    konix_emacs_open_w3m.sh "$@"
+fi

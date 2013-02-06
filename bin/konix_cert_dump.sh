65b2adc845c35e337f66ede26a27617838d2cff9
Samuel Loury <konubinixweb@gmail.com>
2013-02-06 17:57:40 +0100

    konix_cert_dump is now used to dump the content of a certificate

--- bin/konix_cert_dump.sh
+++ bin/konix_cert_dump.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-konix_cert_show.sh "$@" 2>/dev/null| \
-    sed -n -e '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' \
-    -e '/^ - subject / { # for gnutls output
-s/^.\+, issuer /# /
-p
-}' \
-    -e '/i:/ { # for openssl output
-s/^.\+\(i\):/# \1/
-p
-}'
+openssl x509 -in "$1" -noout -text

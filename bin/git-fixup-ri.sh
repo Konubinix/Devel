b5b7a0fd04c26bcd8b898a54d4f001d6f0b61fb5
Samuel Loury <konubinixweb@gmail.com>
2013-02-07 15:59:29 +0100

    git fixup ri now stashes local changes so that the rebase won't fail and pop the stash at the end of the command

--- bin/git-fixup-ri.sh
+++ bin/git-fixup-ri.sh
@@ -2,4 +2,24 @@
 
 TREEISH="$1"
 shift
-git-fixup.sh "$@" && git rebase -i "$@"
+echo "Commit the fixup">&2
+git-fixup.sh "$TREEISH"
+RES="$?"
+if [ "$RES" == "0" ]
+then
+    # now, do the ri
+    if git status --porcelain|grep -q ' M'
+    then
+        # stash before rebasing
+        echo "Stashing local changes">&2
+        git stash
+        STASHED=1
+    fi
+    echo "Performing the rebase">&2
+    git rebase -i "$@"
+    if [ "$STASHED" == "1" ]
+    then
+        echo "Popping the previously stashed changes">&2
+        git stash pop
+    fi
+fi

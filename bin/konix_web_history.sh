99299ba99cfbd664d32cb6148208a2f1dbbe21b3
Samuel Loury <konubinixweb@gmail.com>
2013-02-11 18:09:11 +0100

    the web history script may be launched on another file and defaults to the UZBL_HISTORY_FILE

--- bin/konix_web_history.sh
+++ bin/konix_web_history.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-HISTORY_=`konix_dmenu_custom.sh < "${UZBL_HISTORY_FILE}"|cut -f3 -d' '`
+# use the one given as parameter and fallback to the uzbl history file
+HISTORY_FILE="${1:-$UZBL_HISTORY_FILE}"
+HISTORY_=`tac "${HISTORY_FILE}"|konix_dmenu_custom.sh|cut -f3 -d' '`
 if [ -n "$HISTORY_" ]
 then
     "$BROWSER" "${HISTORY_}"

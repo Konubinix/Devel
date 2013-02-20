81dc7124d25b55041e83b29dd13fd5728ac42c23
Samuel Loury <konubinixweb@gmail.com>
2013-02-20 13:20:10 +0100

    import the env in the uzbl command popping last page

--- bin/konix_uzbl_pop_exited_page.sh
+++ bin/konix_uzbl_pop_exited_page.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source "$HOME/init_bin/konix_init_lib.sh"
+import_env
 UNDO="${UZBL_UNDOLIST_FILE:-${XDG_DATA_HOME:-~/.local/share}/uzbl/undolist}"
 UZBL="exec uzbl-browser"
 if [ -e $UNDO ]; then

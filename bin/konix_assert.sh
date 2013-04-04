86241a9eaa54d83f2ce07a5ee77f770e6607e19b
Samuel Loury <konubinixweb@gmail.com>
2013-04-04 18:18:30 +0200

    The assertion message of bash is now showing the context of the whole stack trace

--- bin/konix_assert.sh
+++ bin/konix_assert.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+show_context () {
+    FILE_NAME=$1
+    LINE_NUMBER=$2
+    CONTEXT=$3
+    cat -n "$FILE_NAME" \
+        | head -$((LINE_NUMBER + CONTEXT)) \
+        | tail -$((2 * CONTEXT)) \
+        | sed "/^ \+$LINE_NUMBER\t/ {
+    s/^ />/
+}"
+}
+
 eval "[ $* ]"
 if [ "$?" != "0" ]
 then
-	echo "Assert on condition $*"
+ 	echo "Assert on condition '$*'"
 	frame_number=0
-	while caller $frame_number
+	while caller $frame_number > /dev/null
 	do
+        caller $frame_number \
+            | sed 's/^\(.\+\) \(.\+\) \(.\+\)$/>>>> called by \2 at \3:\1/'
+        line_number=$(caller $frame_number|cut -f 1 -d' ')
+        file_name=$(caller $frame_number|cut -f 3 -d' ')
+        show_context $file_name $line_number 3
 		frame_number=$((frame_number + 1))
 	done
 	exit 1

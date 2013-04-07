341d5f94d812cf7d01a6580f028905300b932329
Samuel Loury <konubinixweb@gmail.com>
2013-04-07 14:35:18 +0200

    Take into account the -r argument given by mailx and convert it to -a to select the msmtp account

--- bin/konix_msmtp.sh
+++ bin/konix_msmtp.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-msmtp -t -oi -C "${KONIX_PERSO_DIR}/msmtprc" --logfile "$HOME/msmtp_log" "$@"
+# heilroom gives -r to specify the sender while msmtp expects -a or -f
+# find the position of a -r
+pos=0
+found=""
+for arg in "$@"
+do
+    if [ "$arg" == "-r" ]
+    then
+        found="$pos"
+    fi
+    pos=$((pos + 1))
+done
+#echo $found
+#echo "$@"
+# took from http://stackoverflow.com/questions/4827690/change-a-command-line-argument-bash
+if [ "$found" != "" ]
+then
+    if [ "$found" -gt "0" ]
+    then
+        set -- "${@:1:${found}}" "-a" "${@:$((found + 2 ))}"
+    else
+        set -- "-a" "${@:2}"
+    fi
+fi
+#echo "$@"
+#xmessage "$*"
+msmtp -t -oi -C "${KONIX_PERSO_DIR}/msmtprc" --aliases="${KONIX_MSMTP_ALIASES}" --logfile "$HOME/msmtp_log" "$@"

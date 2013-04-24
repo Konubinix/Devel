28b6702520f40df29b4ab33b52f3406252773928
Samuel Loury <konubinixweb@gmail.com>
2013-04-24 15:56:44 +0200

    Make gpg save and restore be more verbose.

--- bin/konix_gpg_restore.sh
+++ bin/konix_gpg_restore.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Restoring the public keys"
 gpg --import < "$KONIX_PERSO_DIR/gnupg/pubring.gpg"
+echo "Restoring the ownertrust"
 gpg --import-ownertrust < "$KONIX_PERSO_DIR/gnupg/trustdb.txt"
+echo "Done"
--- bin/konix_gpg_save.sh
+++ bin/konix_gpg_save.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 mkdir -p "$KONIX_PERSO_DIR/gnupg"
+echo "Saving the public keys"
 gpg --export > "$KONIX_PERSO_DIR/gnupg/pubring.gpg"
+echo "Saving the ownertrust"
 gpg --export-ownertrust > "$KONIX_PERSO_DIR/gnupg/trustdb.txt"
+echo "Done"

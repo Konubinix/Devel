1e305e6c880d598efdb9f144a9052c579ab41c8f
Samuel Loury <konubinixweb@gmail.com>
2013-04-25 16:32:32 +0200

    Add procmailrc and install it.

--- install.py
+++ install.py
@@ -60,6 +60,7 @@ substitute(os.path.join(environ["KONIX_CONFIG_DIR"], "B.blend"),    os.path.join
 substitute(os.path.join(environ["KONIX_CONFIG_DIR"], "screenrc"),   os.path.join(environ["HOME"], ".screenrc"))
 substitute(os.path.join(environ["KONIX_CONFIG_DIR"], "vimrc"),      os.path.join(environ["HOME"], ".vimrc"))
 substitute(os.path.join(environ["KONIX_CONFIG_DIR"], "terminator"), os.path.join(environ["HOME"], ".config/terminator/config"))
+substitute(os.path.join(environ["KONIX_CONFIG_DIR"], "procmailrc"), os.path.join(environ["HOME"], ".procmailrc"))
 
 if is_on_linux():
         substitute(os.path.join(environ["KONIX_CONFIG_DIR"], "fluxbox"),   os.path.join(environ["HOME"], ".fluxbox"))

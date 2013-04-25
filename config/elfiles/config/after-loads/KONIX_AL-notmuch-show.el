f34f133f7f33081729d9c68456ee91800a9dc988
Samuel Loury <konubinixweb@gmail.com>
2013-04-25 16:31:08 +0200

    Add an advice around the notmuch-hello-widget-search function so that, when employed with C-u, the results are reversed

---
+++ config/elfiles/config/after-loads/KONIX_AL-notmuch-show.el
@@ -0,0 +1,45 @@
+;;; KONIX_AL-notmuch-show.el ---
+
+;; Copyright (C) 2013  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defadvice notmuch-hello-widget-search (around reverse_order ())
+  "`current-prefix-arg' inverse the default reverse order"
+  (let (
+		(notmuch-search-oldest-first
+		 (not
+		  (equalp
+		   notmuch-search-oldest-first
+		   current-prefix-arg
+		   )
+		  )
+		 )
+		)
+	ad-do-it
+	)
+  )
+(ad-activate 'notmuch-hello-widget-search)
+
+(provide 'KONIX_AL-notmuch-show)
+;;; KONIX_AL-notmuch-show.el ends here

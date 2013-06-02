0f8b7ee5324d9bb8589d2d57f64a8d35f6c0e086
Samuel Loury <konubinixweb@gmail.com>
2013-06-02 21:58:20 +0200

    Now the shorten algorithm creates a shallow copy of perso and use it instead of the previous one.
    It then removes the shallow file to allow pull/push and clone from it.

--- bin/konix_perso-shorten.sh
+++ bin/konix_perso-shorten.sh
@@ -1,21 +1,42 @@
-#!/bin/bash -x
+#!/bin/bash
+
+# Algorithm: freeze the local copy, then clone only the 40 last commits to a
+# temporaty location ($KONIX_PERSO_DIR.tmp), if everything went ok, move the
+# current perso dir to $KONIX_PERSO_DIR.XXX.old (XXX being anything, got from
+# mktemp) and move the temporary clone to $KONIX_PERSO_DIR
 
 . "${KONIX_LIB_DIR}/lib_bash.sh"
 # init
-pushd "$KONIX_PERSO_DIR"
-# save current state
-git-freeze.sh
+TEMP_LOC="${KONIX_PERSO_DIR}.tmp"
+OLD_LOC="$(mktemp --suffix .old "${KONIX_PERSO_DIR}.XXX")"
 
-end(){
-    git co master
-    exit "${1:-1}"
+die ( ) {
+	MESSAGE="$*"
+	echo "$MESSAGE"
+	exit 1
 }
 
-# keep only the last 100 commits
-git branch -D latest_commit
-git co HEAD~100 -b latest_commit || end
-git reset --soft bottom || end
-git commit -m "squashed from bottom to latest_commit"
-FIRST_COMMIT=`git what-commit.sh`
-git rebase latest_commit master --onto "$FIRST_COMMIT"
-end 0
+message ( ) {
+	MESSAGE="$*"
+	echo "$MESSAGE"
+}
+
+# save current state
+message "Freezing the perso dir"
+pushd "$KONIX_PERSO_DIR"
+git-freeze.sh
+popd
+
+# clone it to the temporary location
+message "Cloning the 40 last commits to ${TEMP_LOC}"
+git clone --depth 40 "file://${KONIX_PERSO_DIR}/" "${TEMP_LOC}" || \
+	die "Failed to clone ${KONIX_PERSO_DIR}"
+
+message "Moving the perso dir to ${OLD_LOC}"
+mv "${KONIX_PERSO_DIR}" "${OLD_LOC}" || \
+	die "Could not move the perso dir to the old location"
+message "Moving the ${TEMP_LOC} to perso dir"
+mv "${TEMP_LOC}" "${KONIX_PERSO_DIR}" || \
+	die "Could not move the temp dir to the perso dir"
+message "Make the new repository not shallow to be able to refetch from it"
+rm "${KONIX_PERSO_DIR}/.git/shallow"

f287e1017e3fb5b42c3d8258487a5f12ba24acf9
Samuel Loury <konubinixweb@gmail.com>
2013-06-03 15:21:49 +0200

    Addition of some customization for org-clock

---
+++ config/elfiles/config/after-loads/KONIX_AL-org-clock.el
@@ -0,0 +1,50 @@
+;;; KONIX_AL-org-clock.el ---
+
+;; Copyright (C) 2013  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq-default org-clocktable-defaults  (list
+										:maxlevel 2
+										:lang org-export-default-language
+										:scope 'file
+										:block nil
+										:tstart nil
+										:tend nil
+										:step nil
+										:stepskip0 nil
+										:fileskip0 nil
+										:tags nil
+										:emphasize nil
+										:link nil
+										:narrow '120!
+										:indent t
+										:formula nil
+										:timestamp nil
+										:level nil
+										:tcolumns nil
+										:formatter nil)
+			  )
+
+(provide 'KONIX_AL-org-clock)
+;;; KONIX_AL-org-clock.el ends here

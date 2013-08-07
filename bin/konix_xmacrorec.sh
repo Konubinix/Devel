58f3f3247e8e16d60089c4b831ad5f7ee63d7b98
Samuel Loury <konubinixweb@gmail.com>
2013-08-07 18:02:19 +0200

    Use the home key instead of F1 to close a xmacro. F1 often launches the help of the running program

--- bin/konix_xmacrorec.sh
+++ bin/konix_xmacrorec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 touch "$HOME/.konix_in_xmacro"
-xmacrorec2 -k 67 > $HOME/.xmacro && konix_display.py "Macro added in $HOME/.xmacro"
+xmacrorec2 -k 133 > $HOME/.xmacro && konix_display.py "Macro added in $HOME/.xmacro"
 rm "$HOME/.konix_in_xmacro"

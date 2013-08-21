644f686111dc0be2e23ed53814ac2f47e28455d0
Samuel Loury <konubinixweb@gmail.com>
2013-08-21 16:05:09 +0200

    Allow the user to specify the resolution of the pdf reducing tool

--- bin/konix_pdf_reduce_gs.sh
+++ bin/konix_pdf_reduce_gs.sh
@@ -12,7 +12,9 @@ DST="${2:-${SRC%.*}_reduced.${SRC#*.}}"
 # - /printer and /prepress: high-quality settings used for printing PDFs.
 QUAL="${3:-/ebook}"
 
-echo "Reducing $SRC and putting the result in $DST at qual $QUAL"
+RES="${4:-72}"
+
+echo "Reducing $SRC and putting the result in $DST at qual $QUAL with resolution=${RES}"
 gs \
 	-sDEVICE=pdfwrite \
 	-dNOPAUSE \
@@ -21,11 +23,11 @@ gs \
 	-dEmbedAllFonts=true \
 	-dSubsetFonts=true \
 	-dColorImageDownsampleType=/Bicubic \
-	-dColorImageResolution=72 \
+	-dColorImageResolution="${RES}" \
 	-dGrayImageDownsampleType=/Bicubic \
-	-dGrayImageResolution=72 \
+	-dGrayImageResolution="${RES}" \
 	-dMonoImageDownsampleType=/Bicubic \
-	-dMonoImageResolution=72 \
+	-dMonoImageResolution="${RES}" \
 	-dCompatibilityLevel=1.3 \
 	-dPDFSETTINGS="${QUAL}" \
 	-sOutputFile="${DST}" \

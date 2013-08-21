221c54bf7aba5089b28d71c46bfca7e2be70f9cd
Samuel Loury <konubinixweb@gmail.com>
2013-08-21 16:04:48 +0200

    In case the konix_feed_sync script fails somewhere, write this failure in stderr
    this way, the cron task will send me a mail when it happens

--- bin/konix_feed_sync.sh
+++ bin/konix_feed_sync.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # do a rss -> imap work
-feed2imap -v -f "$KONIX_PERSO_DIR/feed2imaprc"
+feed2imap -v -f "$KONIX_PERSO_DIR/feed2imaprc" | while read line
+do
+    if echo "$line" | grep -q 'FATAL\|ERROR'
+    then
+        echo "$line" >&2
+    else
+        echo "$line"
+    fi
+done
 # clean the old rss
 #feed2imap-cleaner -f "$KONIX_PERSO_DIR/feed2imaprc"

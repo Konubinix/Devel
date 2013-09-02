bb745cfb5d93288e8279dccfaf875604e5d23c08
Samuel Loury <konubinixweb@gmail.com>
2013-09-02 17:12:06 +0200

    Added the completion for the ordered property

--- config/elfiles/config/after-loads/KONIX_AL-org.el
+++ config/elfiles/config/after-loads/KONIX_AL-org.el
@@ -1131,6 +1131,7 @@ to be organized.
 				("Effort_ALL". "0:05 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 7:59")
 				("POMODORO_ALL". "1 2 3 4 5 6 7 8 9 0")
 				("DONE_POMODORO_ALL". "0 1 2 3 4 5 6 7 8 9 0")
+				("ORDERED_ALL". "t")
 				))
 (setq-default org-habit-graph-column 100)
 (setq-default org-hide-block-startup t)

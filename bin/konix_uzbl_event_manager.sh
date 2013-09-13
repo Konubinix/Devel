9e739d7542d32bd9e57b9ebba50511be9d87676b
Samuel Loury <konubinixweb@gmail.com>
2013-09-13 11:27:04 +0200

    Updated uzbl and my configuration about it

--- bin/konix_uzbl_event_manager.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-uzbl-event-manager -d "$KONIX_SHARE_DIR/uzbl/plugins" -va start
--- share/uzbl/scripts/konix_auth.py
+++ share/uzbl/scripts/konix_auth.py
@@ -1,27 +1,78 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*-
 
-import subprocess
 import os
-import netrc
-import sys
-import auth                     # local import
 import gtk
+import sys
+import netrc
 
-if __name__ == "__main__":
-    # $1 authentication zone unique identifier (may be used as 'key')
-    # $2 domain part of URL that requests authentication
-    # $3 authentication realm
-    # $4 FALSE if this is the first attempt to authenticate, TRUE otherwise
+def responseToDialog(entry, dialog, response):
+    dialog.response(response)
+
+def getText(authInfo, authHost, authRealm):
+    dialog = gtk.MessageDialog(
+        None,
+        gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+        gtk.MESSAGE_QUESTION,
+        gtk.BUTTONS_OK_CANCEL,
+        None)
+    dialog.set_markup('%s at %s' % (authRealm, authHost))
+
+    login = gtk.Entry()
+    password = gtk.Entry()
+    password.set_visibility(False)
+
+    login.connect("activate", responseToDialog, dialog, gtk.RESPONSE_OK)
+    password.connect("activate", responseToDialog, dialog, gtk.RESPONSE_OK)
+
+    hbox = gtk.HBox();
+
+    vbox_entries = gtk.VBox();
+    vbox_labels = gtk.VBox();
+
+    vbox_labels.pack_start(gtk.Label("Login:"), False, 5, 5)
+    vbox_labels.pack_end(gtk.Label("Password:"), False, 5, 5)
+
+    vbox_entries.pack_start(login)
+    vbox_entries.pack_end(password)
+
+    dialog.format_secondary_markup("Please enter username and password:")
+    hbox.pack_start(vbox_labels, True, True, 0)
+    hbox.pack_end(vbox_entries, True, True, 0)
+
+    dialog.vbox.pack_start(hbox)
+    dialog.show_all()
+    rv = dialog.run()
+
+    output = {
+        'username': login.get_text(),
+        'password': password.get_text()
+    }
+    dialog.destroy()
+    return rv, output
+
+def tryNetrc(authInfo, authHost, authRealm):
     net = netrc.netrc()
-    authenticators = net.authenticators(sys.argv[2])
+    authenticators = net.authenticators(authHost)
+    rv = gtk.RESPONSE_NO
+    output = {}
+
     if authenticators:
         (login, account, password) = authenticators
-        print login
-        print password
+        rv = gtk.RESPONSE_OK
+        output["username"] = login
+        output["password"] = password
+    return rv, output
+
+if __name__ == '__main__':
+    fifo = open(os.environ.get('UZBL_FIFO'), 'w')
+    me, info, host, realm = sys.argv
+    rv, output = tryNetrc(info, host, realm)
+    if (rv != gtk.RESPONSE_OK):
+        rv, output = getText(info, host, realm)
+
+    if (rv == gtk.RESPONSE_OK):
+        print >> fifo, 'auth "%s" "%s" "%s"' % (
+            info, output['username'], output['password']
+        )
     else:
-        rv, output = auth.getText(sys.argv[1], sys.argv[2], sys.argv[3])
-        if (rv == gtk.RESPONSE_OK):
-            print output;
-        else:
-            exit(1)
+        exit(1)

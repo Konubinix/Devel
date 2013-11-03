03fd0cdad1d789f4b5c609a1d06ba8dc0108a8cd
Samuel Loury <konubinixweb@gmail.com>
2013-11-03 19:45:00 +0100

    Use the login and hostname as email of the committer in the git-freeze binary

--- bin/git-freeze.sh
+++ bin/git-freeze.sh
@@ -9,4 +9,14 @@ print "# Removing deleted files"
 git-rm-deleted-files.sh
 print "# Adding untracked files"
 git-add-untracked-files.sh
+# taken from https://github.com/svend/home-bin/blob/master/git-autocommit
+hostname=`hostname`
+dnsdomainname=`dnsdomainname 2>/dev/null || true`
+if [ -n "$dnsdomainname" ]; then
+        hostname="$hostname.$dnsdomainname"
+fi
+
+if [ -z "$GIT_COMMITTER_EMAIL" ]; then
+        export GIT_COMMITTER_EMAIL=`whoami`"@$hostname"
+fi
 git commit -m "Freezing of repo by $LOGNAME at $HOSTNAME"

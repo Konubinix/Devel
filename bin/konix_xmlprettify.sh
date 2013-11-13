5a31bf10ab4cc326ee5de3a9247e1264db755475
Samuel Loury <konubinixweb@gmail.com>
2013-11-13 17:59:28 +0100

    Added a script prettifying an XML document using xmllint

---
+++ bin/konix_xmlprettify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE="${1}"
+[ -n "${FILE}" ] || { echo "Must provide a file as argument" ; exit 1 ; }
+[ -e "${FILE}" ] || { echo "${FILE} must exist" ; exit 1 ; }
+
+TMP=`mktemp`
+trap "rm ${TMP}" 0
+
+xmllint --format "${FILE}" > "${TMP}"
+
+mv "${TMP}" "${FILE}" && trap "" 0
+echo "DONE"

4d9167846f8eb9e5f9b965e78623b0ee9a9cefa5
Samuel Loury <konubinixweb@gmail.com>
2013-11-15 10:00:58 +0100

    Adapted konix_spell to allow to specify the lang

--- bin/konix_spell.sh
+++ bin/konix_spell.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-# Taken from https://github.com/svend/home-bin/blob/master/spell
+# Inspired from https://github.com/svend/home-bin/blob/master/spell
 # Check spelling of arguments
 
-echo "$@" | aspell -a
+usage () {
+	cat<<EOF
+$0 [-h] [-a aspell_args] [-l lang] words
+-h show this help and exits
+-a aspell additional arguments
+-l lang
+EOF
+}
+ASPELL_ARGS=""
+LANG_ARG=""
+while getopts "ha:l:" opt; do
+	case $opt in
+		h)
+			usage
+			exit 0
+			;;
+		a)
+			ASPELL_ARGS="$OPTARG"
+			;;
+		l)
+			LANG_ARG="-l $OPTARG"
+			;;
+	esac
+done
+shift $((OPTIND-1))
+echo "$@" | aspell -a $LANG_ARG $ASPELL_ARGS

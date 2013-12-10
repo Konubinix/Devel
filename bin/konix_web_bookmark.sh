57e3d026e9331e1278d41ed0b2f55cbf0a9a4e6e
Samuel Loury <konubinixweb@gmail.com>
2013-12-10 18:02:30 +0100

    A bookmark may be obtained by evaling something. This is useful for bookmarks needing sessions id or pre authentication.

--- bin/konix_web_bookmark.sh
+++ bin/konix_web_bookmark.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 BOOKMARK=`konix_dmenu_custom.sh < "${UZBL_BOOKMARKS_FILE}"|cut -f1 -d' '`
+# if the scheme indicates so, the BOOKMARK must be evaled
+if echo "$BOOKMARK" | grep -q -e "^eval://"
+then
+    BOOKMARK="${BOOKMARK/eval:/}"
+    BOOKMARK="$(eval "echo "`echo "$BOOKMARK"`"")"
+fi
 if [ -n "$BOOKMARK" ]
 then
     "$BROWSER" "$BOOKMARK"

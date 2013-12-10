90ce3c3986e722724febeec8f2fcb29d5bd4d007
Samuel Loury <konubinixweb@gmail.com>
2013-12-10 18:13:12 +0100

    Added a binding to launch org-publish

--- config/elfiles/config/600-KONIX_hotkeys.el
+++ config/elfiles/config/600-KONIX_hotkeys.el
@@ -559,6 +559,7 @@
 
 (define-key 'konix/org-global-map (kbd "M-e") 'konix/org-adjust-effort)
 (define-key 'konix/org-global-map "j" 'konix/org-jump-to)
+(define-key 'konix/org-global-map "P" 'org-publish)
 (define-key 'konix/org-global-map "g" 'konix/org-clock-goto)
 (define-key 'konix/org-global-map "b" 'konix/org-goto-bookmarks)
 (define-key 'konix/org-global-map "O" 'org-clock-out)

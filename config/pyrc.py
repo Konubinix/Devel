221a13179b8cea6d32d763929e939e855b7c7880
Samuel Loury <konubinixweb@gmail.com>
2013-12-11 17:08:40 +0100

    Handle history as well as readline in interactive python

--- config/pyrc.py
+++ config/pyrc.py
@@ -1,15 +1,20 @@
-try:
+# make sure the tab command performs completion
+import readline, rlcompleter
+readline.parse_and_bind("tab: complete")
+
+# handle history, taken from http://docs.python.org/2/tutorial/interactive.html
+import atexit
+import os
+
+historyPath = os.path.expanduser("~/.pyhistory")
+# read the previous history if possible
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+
+# save the history when the interpreter exits
+def save_history(historyPath=historyPath):
     import readline
-except ImportError:
-    print "Module readline not available."
-else:
-    import rlcompleter
-    class TabCompleter(rlcompleter.Completer):
-        """Completer that supports indenting"""
-        def complete(self, text, state):
-            if not text:
-                return ('    ', None)[state]
-            else:
-                return rlcompleter.Completer.complete(self, text, state)
-    readline.set_completer(TabCompleter().complete)
-    readline.parse_and_bind("tab: complete")
+    readline.write_history_file(historyPath)
+atexit.register(save_history)
+
+del os, atexit, rlcompleter, readline, save_history, historyPath

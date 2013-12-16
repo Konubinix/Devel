ed0a12c13da73f20c069e8251d3f4868333e3f16
Samuel Loury <konubinixweb@gmail.com>
2013-12-16 18:05:46 +0100

    Make sure zirco_open works well with http:// urls and file:// ones

--- bin/konix_zirco_open.sh
+++ bin/konix_zirco_open.sh
@@ -1,4 +1,22 @@
 #! /bin/bash
 
 
-am start -a android.intent.action.VIEW -t text/html -n org.zirco/.ui.activities.MainActivity -d file://$(readlink -f "$1")
+FILE="$1"
+if test -e "${FILE}" && ! echo "$FILE"|grep -q -e '^/'
+then
+        FILE="$(pwd)/${FILE}"
+fi
+FILE="$(echo "${FILE}"|sed 's/%/%25/g'|sed 's/&/%26/g'|sed 's/=/%3D/g'|sed 's/@/%40/g'|sed 's/+/%2B/g')"
+#FILE="$(readlink -f "$1")"
+if test -n "$FILE"
+then
+FILE="$(echo ${FILE}|sed 's-^/home-/data/home-')"
+FILE="file://$FILE"
+else
+FILE="$1"
+fi
+am start \
+        -a android.intent.action.VIEW \
+        -t text/html \
+        -n org.zirco/.ui.activities.MainActivity \
+        -d "${FILE}"

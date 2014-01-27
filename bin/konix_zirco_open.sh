5bc64bfdd43b9adaf83d29df5c077ca227e6b49f
Samuel Loury <konubinixweb@gmail.com>
2014-01-27 17:58:00 +0100

    Zirco_open now convert the file using the good encoding and is compatible with android 4

--- bin/konix_zirco_open.sh
+++ bin/konix_zirco_open.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 
+FILE="$1"
+FILE="$(echo "$FILE"|iconv -f utf-8 -t iso-8859-1)"
+test -n "$FILE" || { echo "Must provide an argument" ; exit 1 ; }
 
-FILE="$1"
-if test -e "${FILE}" && ! echo "$FILE"|grep -q -e '^/'
+if test -e "${FILE}" 
 then
-        FILE="$(pwd)/${FILE}"
+        if ! echo "$FILE"|grep -q -e '^/'
+        then
+            FILE="$(pwd)/${FILE}"
+        fi
+        FILE="$(echo ${FILE}|sed 's-^/home-/data/home-')"
+        FILE="file://$FILE"
 fi
+
 FILE="$(echo "${FILE}"|sed 's/%/%25/g'|sed 's/&/%26/g'|sed 's/=/%3D/g'|sed 's/@/%40/g'|sed 's/+/%2B/g')"
-#FILE="$(readlink -f "$1")"
-if test -n "$FILE"
-then
-FILE="$(echo ${FILE}|sed 's-^/home-/data/home-')"
-FILE="file://$FILE"
-else
-FILE="$1"
-fi
 am start \
+        --user 0 \
         -a android.intent.action.VIEW \
         -t text/html \
         -n org.zirco/.ui.activities.MainActivity \

7f102e21b1d8bf30e0b3694524e6dd9b20e8f90c
Samuel Loury <konubinixweb@gmail.com>
2014-01-27 18:14:44 +0100

    Added some android opening scripts using am

---
+++ bin/konix_android_browser_open.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+FILE="$1"
+FILE="$(echo "$FILE"|iconv -f utf-8 -t iso-8859-1)"
+test -n "$FILE" || { echo "Must provide an argument" ; exit 1 ; }
+
+if test -e "${FILE}" 
+then
+        if ! echo "$FILE"|grep -q -e '^/'
+        then
+            FILE="$(pwd)/${FILE}"
+        fi
+        FILE="$(echo ${FILE}|sed 's-^/home-/data/home-')"
+        FILE="file://$FILE"
+fi
+
+FILE="$(echo "${FILE}"|sed 's/%/%25/g'|sed 's/&/%26/g'|sed 's/=/%3D/g'|sed 's/@/%40/g'|sed 's/+/%2B/g')"
+am start \
+        --user 0 \
+        -a android.intent.action.VIEW \
+        -t text/html \
+        -n com.android.browser/.BrowserActivity \
+        -d "${FILE}"

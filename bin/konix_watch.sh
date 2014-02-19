61f3d678003ef01d0e4057faa29af455e3766651
Samuel Loury <konubinixweb@gmail.com>
2014-02-19 11:11:34 +0100

    Added a script to watch the execution of a program and inform the user via zenity when it ends.

---
+++ bin/konix_watch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source "${KONIX_LIB_DIR}/lib_bash.sh"
+konix_assert_var KONIX_LOG_DIR
+konix_assert_var KONIX_LOG_ERR_DIR
+
+zenity --help > /dev/null || {
+    echo "No way to inform you of program ending"
+    exit 1
+}
+program_name="$(basename "${1}")"
+OUTPUT_FILE="${KONIX_LOG_DIR}/${program_name}"
+ERROR_FILE="${KONIX_LOG_ERR_DIR}/${program_name}"
+cat <<EOF|tee -a "${OUTPUT_FILE}" >> "${ERROR_FILE}"
+########################################
+`date`: Launched '$*'
+########################################
+EOF
+"$@" >> "${OUTPUT_FILE}" 2>> "${ERROR_FILE}"
+zenity --info --text "Command line '$*' ended"

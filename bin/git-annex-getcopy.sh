67d4549ea73e4c472875658459f5a13a261b9b9d
Samuel Loury <konubinixweb@gmail.com>
2014-02-20 17:24:23 +0100

    Added a script to make sure to git annex get a file before copying it

---
+++ bin/git-annex-getcopy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source "${KONIX_LIB_DIR}/parano.sh"
+
+usage () {
+    cat<<EOF
+$0 -t <TO> <REMOTE> [args]
+
+Get a file and copy it to the given remote.
+args are given to the copy command
+EOF
+}
+
+while getopts "ht:" opt; do
+    case $opt in
+        h)
+            usage
+            exit 0
+            ;;
+        t)
+            TO="$OPTARG"
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+FILE="${1}"
+shift
+echo "Getting ${FILE} and copying it to ${TO}"
+set -x
+git annex get "${FILE}"
+git annex copy --to "${TO}" "${FILE}" "$@"

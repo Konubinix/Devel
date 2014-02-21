363753b5169cefa3d88cb5490aac0c83e667f61e
Samuel Loury <konubinixweb@gmail.com>
2014-02-21 07:48:20 +0100

    Added some customization about org-attach

---
+++ config/elfiles/config/after-loads/KONIX_AL-org-attach.el
@@ -0,0 +1,35 @@
+;;; KONIX_AL-org-attach.el ---                       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(defun konix/org-attach-open (file)
+  (find-file
+   (org-attach-expand file)
+   )
+  )
+
+(org-add-link-type "attach" 'konix/org-attach-open)
+
+(provide 'KONIX_AL-org-attach)
+;;; KONIX_AL-org-attach.el ends here

b22707e2fbef68d1c388b889be0967f11786443e
Samuel Loury <konubinixweb@gmail.com>
2014-03-07 07:47:32 +0100

    Do not overwrite the initial xml file if xmllint failed

--- bin/konix_xmlprettify.sh
+++ bin/konix_xmlprettify.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+die () {
+    echo "$*" >&2
+    exit 1
+}
+
 FILE="${1}"
 [ -n "${FILE}" ] || { echo "Must provide a file as argument" ; exit 1 ; }
 [ -e "${FILE}" ] || { echo "${FILE} must exist" ; exit 1 ; }
@@ -7,7 +12,7 @@ FILE="${1}"
 TMP=`mktemp`
 trap "rm ${TMP}" 0
 
-xmllint --format "${FILE}" > "${TMP}"
+xmllint --format "${FILE}" > "${TMP}" || die "xmllint failed"
 
 mv "${TMP}" "${FILE}" && trap "" 0
 echo "DONE"

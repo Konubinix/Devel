db5317aab739f282eeb84210a74aea56eb996543
Samuel Loury <konubinixweb@gmail.com>
2014-03-14 12:31:54 +0100

    encrypt and decrypt public gpg information

--- bin/konix_gpg_save.sh
+++ bin/konix_gpg_save.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -eu
 mkdir -p "$KONIX_PERSO_DIR/gnupg"
 echo "Saving the public keys"
-gpg --export > "$KONIX_PERSO_DIR/gnupg/pubring.gpg"
+gpg --export --armor \
+    | gpg --encrypt --sign --recipient "$(konix_gpgdir_default_key.py)" > "${KONIX_PERSO_DIR}/gnupg_nd/pubring.gpg"
 echo "Saving the ownertrust"
-gpg --export-ownertrust > "$KONIX_PERSO_DIR/gnupg/trustdb.txt"
+gpg --export-ownertrust --armor \
+    | gpg --encrypt --sign --recipient "$(konix_gpgdir_default_key.py)" > "${KONIX_PERSO_DIR}/gnupg_nd/trustdb.txt.gpg"
 echo "Done"

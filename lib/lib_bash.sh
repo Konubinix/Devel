3ca8adc15ff768d6ed65dbf7fe060df4732fd588
Samuel Loury <konubinixweb@gmail.com>
2014-04-01 08:53:25 +0200

    Put colors code in a separate file only sourced in interactive mode

--- lib/lib_bash.sh
+++ lib/lib_bash.sh
@@ -1,44 +1,40 @@
-if [ -n "$TERM" ] \
-    && [ -x "/usr/bin/tput" ] \
-    && [ "`tput colors`" -ge "8" ]
-then
-    export COLOR_RESET=`tput sgr0`
-    export COLOR_BOLD=`tput bold`
-    export COLOR_UL=`tput smul`
+# declare colors to allow scripts with -u to work correctly
+export COLOR_RESET=""
+export COLOR_BOLD=""
+export COLOR_UL=""
+export COLOR_FG_BLACK=""
+export COLOR_FG_RED=""
+export COLOR_FG_GREEN=""
+export COLOR_FG_YELLOW=""
+export COLOR_FG_BLUE=""
+export COLOR_FG_MAGENTA=""
+export COLOR_FG_CYAN=""
+export COLOR_FG_WHITE=""
+export COLOR_BG_BLACK=""
+export COLOR_BG_RED=""
+export COLOR_BG_GREEN=""
+export COLOR_BG_YELLOW=""
+export COLOR_BG_BLUE=""
+export COLOR_BG_MAGENTA=""
+export COLOR_BG_CYAN=""
+export COLOR_BG_WHITE=""
+export COLOR_BLACK=""
+export COLOR_BLUE=""
+export COLOR_GREEN=""
+export COLOR_CYAN=""
+export COLOR_RED=""
+export COLOR_PURPLE=""
+export COLOR_BROWN=""
+export COLOR_LIGHT_GRAY=""
+export COLOR_DARK_GRAY=""
+export COLOR_LIGHT_BLUE=""
+export COLOR_LIGHT_GREEN=""
+export COLOR_LIGHT_CYAN=""
+export COLOR_LIGHT_RED=""
+export COLOR_LIGHT_PURPLE=""
+export COLOR_YELLOW=""
+export COLOR_WHITE=""
 
-    export COLOR_FG_BLACK=`tput setaf 0`
-    export COLOR_FG_RED=`tput setaf 1`
-    export COLOR_FG_GREEN=`tput setaf 2`
-    export COLOR_FG_YELLOW=`tput setaf 3`
-    export COLOR_FG_BLUE=`tput setaf 4`
-    export COLOR_FG_MAGENTA=`tput setaf 5`
-    export COLOR_FG_CYAN=`tput setaf 6`
-    export COLOR_FG_WHITE=`tput setaf 7`
-    export COLOR_BG_BLACK=`tput setab 0`
-    export COLOR_BG_RED=`tput setab 1`
-    export COLOR_BG_GREEN=`tput setab 2`
-    export COLOR_BG_YELLOW=`tput setab 3`
-    export COLOR_BG_BLUE=`tput setab 4`
-    export COLOR_BG_MAGENTA=`tput setab 5`
-    export COLOR_BG_CYAN=`tput setab 6`
-    export COLOR_BG_WHITE=`tput setab 7`
-fi
-export COLOR_BLACK=0
-export COLOR_BLUE=4
-export COLOR_GREEN=2
-export COLOR_CYAN=6
-export COLOR_RED=1
-export COLOR_PURPLE=5
-export COLOR_BROWN=3
-export COLOR_LIGHT_GRAY=7
-export COLOR_DARK_GRAY=8
-export COLOR_LIGHT_BLUE=12
-export COLOR_LIGHT_GREEN=10
-export COLOR_LIGHT_CYAN=14
-export COLOR_LIGHT_RED=9
-export COLOR_LIGHT_PURPLE=13
-export COLOR_YELLOW=11
-export COLOR_WHITE=15
 
 SSH_AGENT_SOURCE=/tmp/ssh-agent-source
 

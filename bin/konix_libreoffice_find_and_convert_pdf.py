4e7c43ec69cddb1b0f7ab29cd6c7d0e160b5896d
Samuel Loury <konubinixweb@gmail.com>
2014-04-11 17:52:57 +0200

    Added two useful scripts to convert microsoft office documents in pdf

---
+++ bin/konix_libreoffice_find_and_convert_pdf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import fnmatch
+import os
+import re
+import subprocess
+import shlex
+
+matches = []
+for root, dirnames, filenames in os.walk('.'):
+  dirnames = [d for d in dirnames if not re.match("^\..+$", d)]
+  for filename in fnmatch.filter(filenames, '*.doc?') + \
+      fnmatch.filter(filenames, '*.ppt?') + \
+      fnmatch.filter(filenames, '*.xls?'):
+      matches.append(os.path.join(root, filename))
+def guess_pdf(doc):
+  return re.sub("(doc|ppt|xls).?$", "pdf", doc)
+
+to_be_created_doc = [doc for doc in matches if not os.path.exists(guess_pdf(doc))]
+if len(to_be_created_doc) == 0:
+  print "Nothing to do"
+for doc in to_be_created_doc:
+  print "-- Converting %s" % doc
+  subprocess.call(shlex.split("konix_libreoffice_convert_pdf.sh '%s'" % doc))

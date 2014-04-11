e09befa43297f30de45d92e095119e72229758e1
Samuel Loury <konubinixweb@gmail.com>
2014-04-11 12:41:17 +0200

    Much simpler implementation relying on the ouput of git annex metadata

--- bin/gatlstags.sh
+++ bin/gatlstags.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-cd `git rev-parse --git-dir`
-
-dump_met_files () {
-    git ls-files --with-tree=git-annex|grep '/'|grep '.log.met$'|while read sha
-    do
-        git cat-file -p git-annex:${sha} 2> /dev/null
-        # echo "+++ $sha" >&2
-        # git cat-file -p git-annex:${sha} >&2
-    done
-}
-
-dump_met_files \
-    |cut -d ' ' -f 2,3 \
-    |grep -v '^[\n\r\t ]*$' \
-    |sed -r 's/-[^ ]+//' \
-    |grep '\+' \
-    |sort \
-    |uniq -c \
-    |sort -n
+cd `git rev-parse --show-toplevel`
+git annex metadata|grep "^  "|grep -v lastchanged|sort|uniq -c|sort -n

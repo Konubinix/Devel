eeeb7ee45f030a0495bed745479c40ddb8200286
Samuel Loury <konubinixweb@gmail.com>
2014-04-11 12:35:20 +0200

    The script now ignore removed tags

--- bin/gatlstags.sh
+++ bin/gatlstags.sh
@@ -6,7 +6,16 @@ dump_met_files () {
     git ls-files --with-tree=git-annex|grep '/'|grep '.log.met$'|while read sha
     do
         git cat-file -p git-annex:${sha} 2> /dev/null
+        # echo "+++ $sha" >&2
+        # git cat-file -p git-annex:${sha} >&2
     done
 }
 
-dump_met_files | cut -d ' ' -f 2,3 |grep -v '^[\n\r\t ]*$' | sort | uniq -c | sort -n
+dump_met_files \
+    |cut -d ' ' -f 2,3 \
+    |grep -v '^[\n\r\t ]*$' \
+    |sed -r 's/-[^ ]+//' \
+    |grep '\+' \
+    |sort \
+    |uniq -c \
+    |sort -n

d5eb73a29c1fb98c090b9dcb3a9bbd33b044f7c1
Samuel Loury <konubinixweb@gmail.com>
2014-04-15 18:13:10 +0200

    Added an example of dodo.py file used to recompute pdf files from doc, ppt, xls... files. see #3285

---
+++ prog/generate_pdfs_dodo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import fnmatch
+import os
+import re
+import subprocess
+import shlex
+
+docs = []
+for root, dirnames, filenames in os.walk('.'):
+  dirnames = [d for d in dirnames if not re.match("^\..+$", d)]
+  for filename in fnmatch.filter(filenames, '*.doc*') + \
+      fnmatch.filter(filenames, '*.ppt*') + \
+      fnmatch.filter(filenames, '*.xls*'):
+      docs.append(os.path.join(root, filename))
+def guess_pdf(doc):
+  return re.sub("(doc|ppt|xls).?$", "pdf", doc)
+
+def task_generate_pdfs():
+  for doc in docs:
+    pdf = guess_pdf(doc)
+    yield {
+      'name' : "Generate %s" % pdf,
+      'actions': ['konix_libreoffice_convert_pdf.sh "%s"' % doc],
+      'file_dep': [doc],
+      'targets': [pdf],
+    }

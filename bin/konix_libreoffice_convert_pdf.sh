af8532cfeac26fcc8528440f8e43df8f998257a2
Samuel Loury <konubinixweb@gmail.com>
2014-04-15 18:11:23 +0200

    Remove the pdf file before recreating it. see #3285

--- bin/konix_libreoffice_convert_pdf.sh
+++ bin/konix_libreoffice_convert_pdf.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 FILE="$1"
+PDF="$(echo "${FILE}"|sed -r 's/^(.+)\.[^.]+$/\1/').pdf"
+if [ -e "${PDF}" ]
+then
+    rm "${PDF}"
+fi
+
 OUTDIR="$(dirname "${FILE}")"
+
 soffice --headless --convert-to pdf --outdir "$OUTDIR" "$FILE"

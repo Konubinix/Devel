aeaea46158c2b87aed642ce5a3607fb48e3b3981
Samuel Loury <konubinixweb@gmail.com>
2014-04-23 10:47:06 +0200

    I prefer the use of bash when launching org babel shell

---
+++ config/elfiles/config/after-loads/KONIX_AL-ob-sh.el
@@ -0,0 +1,30 @@
+;;; KONIX_AL-ob-sh.el ---
+
+;; Copyright (C) 2014  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq-default org-babel-sh-command "bash")
+
+(provide 'KONIX_AL-ob-sh)
+;;; KONIX_AL-ob-sh.el ends here

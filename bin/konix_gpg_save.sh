b3f3f1db510af7b8932366de6663cfb75d3401f8
Samuel Loury <konubinixweb@gmail.com>
2014-05-02 21:50:04 +0200

    Save the pubring and the ownertrust only if possible

--- bin/konix_gpg_save.sh
+++ bin/konix_gpg_save.sh
@@ -2,10 +2,15 @@
 
 set -eu
 mkdir -p "$KONIX_PERSO_DIR/gnupg"
-echo "Saving the public keys"
-gpg --export --armor \
-    | gpg --encrypt --sign --recipient "$(konix_gpgdir_default_key.py)" > "${KONIX_PERSO_DIR}/gnupg_nd/pubring.gpg"
-echo "Saving the ownertrust"
-gpg --export-ownertrust --armor \
-    | gpg --encrypt --sign --recipient "$(konix_gpgdir_default_key.py)" > "${KONIX_PERSO_DIR}/gnupg_nd/trustdb.txt.gpg"
-echo "Done"
+if [ -f "~/.gnupg/pubring.gpg" ]
+then
+    echo "Saving the public keys"
+    gpg --export --armor \
+        | gpg --encrypt --sign --recipient "$(konix_gpgdir_default_key.py)" > "${KONIX_PERSO_DIR}/gnupg_nd/pubring.gpg"
+    echo "Saving the ownertrust"
+    gpg --export-ownertrust --armor \
+        | gpg --encrypt --sign --recipient "$(konix_gpgdir_default_key.py)" > "${KONIX_PERSO_DIR}/gnupg_nd/trustdb.txt.gpg"
+    echo "Done"
+else
+    echo "No public gpg info to save"
+fi

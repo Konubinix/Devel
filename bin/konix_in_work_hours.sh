231de54b321cba8afb58d226d916d33fcdb20d0b
Samuel Loury <konubinixweb@gmail.com>
2014-06-17 17:50:51 +0200

    Add a script to determine if I am in work hours.
    It is useful in scripts automatically computing the context to perform GTD

---
+++ bin/konix_in_work_hours.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+WEEK_NUMBER="$(date +%u)"
+HOUR="$(date +%H)"
+
+[ ${WEEK_NUMBER} -gt 0 ] \
+    &&  [ ${WEEK_NUMBER} -lt 6 ] \
+    &&  [ ${HOUR} -gt 8 ] \
+    &&  [ ${HOUR} -lt 17 ]

0575a153dfbee376edc534318badae3f369a8d73
Samuel Loury <konubinixweb@gmail.com>
2014-06-17 17:00:52 +0200

    Make sure a non bare repository indicates so to avoid branch overlapping and loss of data

--- bin/git-annex-perso_remoteprehook_ssh.sh
+++ bin/git-annex-perso_remoteprehook_ssh.sh
@@ -28,5 +28,6 @@ konix_assert_var URL
 konix_assert_var LOCATION
 cat <<EOF|ssh -t $PORT "$URL"
 cd "$LOCATION"
+\${HOME}/Prog/devel/bin/konix_do_cron_job.sh git-bare-fixup.sh
 \${HOME}/Prog/devel/bin/konix_do_cron_job.sh git-annex-freeze.sh
 EOF

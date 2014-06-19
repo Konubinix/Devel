c7044022d36126481b7241f6fc719e7ba1936ac3
Samuel Loury <konubinixweb@gmail.com>
2014-06-19 16:07:15 +0200

    Allow decisions to be asked faster using deferred actions

--- bin/konix_aptitude_tag.sh
+++ bin/konix_aptitude_tag.sh
@@ -4,12 +4,39 @@ set -eu
 DATE="$(date +%s)"
 DATE_READABLE="$(date -d @${DATE})"
 LIST_PACKAGES="$(mktemp)"
-trap "rm '${LIST_PACKAGES}'" 0
+LIST_COMMANDS="$(mktemp)"
+# echo "LIST_COMMANDS=${LIST_COMMANDS}"
+# echo "LIST_PACKAGES=${LIST_PACKAGES}"
+trap "rm '${LIST_COMMANDS}' '${LIST_PACKAGES}'" 0
 
 echo "Computing the list of packages"
 aptitude search '~i ?not(~M) ?not(?user-tag(.))' > "${LIST_PACKAGES}"
 NUMBER="$(wc -l "${LIST_PACKAGES}"|cut -f1 -d' ')"
 LAST_RES="d"
+
+flush_commands () {
+	while read -u 11 name command
+	do
+		echo "Applying ${command} to ${name}"
+		if [ "${command}" == "d" ]
+		then
+			echo "Removing packages ${name}"
+			sudo aptitude purge "${name}"
+		elif [ "${command}" == "a" ]
+		then
+			echo "Marking ${name} as automatically installed"
+			sudo aptitude markauto "${name}"
+		else
+			TAG="${DATE_READABLE} (${DATE}):${command}"
+			echo "Applying to ${name} the user tag '${TAG}'"
+			sudo aptitude add-user-tag "${TAG}" "${name}"
+		fi
+	done 11<"${LIST_COMMANDS}"
+	echo -n "" > "${LIST_COMMANDS}"
+	read -p "Press Enter to continue"
+}
+trap "rm '${LIST_COMMANDS}' '${LIST_PACKAGES}'" 0
+
 while read -u 10 line
 do
     NAME="$(echo "$line" | sed -r 's/^[^ ]+ +([^ ]+).+$/\1/')"
@@ -18,22 +45,16 @@ do
 	NUMBER=$((NUMBER-1))
     aptitude show "${NAME}"
     read -p "> Why is $NAME still here (${LAST_RES}) ? " RES
-    if [ "${RES}" == "" ]
-    then
-        RES="${LAST_RES}"
-    fi
-    LAST_RES="${RES}"
-    if [ "${RES}" == "d" ]
-    then
-        echo "Removing packages ${NAME}"
-        sudo aptitude purge "${NAME}"
-    elif [ "${RES}" == "a" ]
-    then
-        echo "Marking ${NAME} as automatically installed"
-        sudo aptitude markauto "${NAME}"
-    else
-        TAG="${DATE_READABLE} (${DATE}):${RES}"
-        echo "Applying to ${NAME} the user tag '${TAG}'"
-        sudo aptitude add-user-tag "${TAG}" "${NAME}"
-    fi
+	if [ "${RES}" == "f" ]
+	then
+		flush_commands
+	else
+		if [ "${RES}" == "" ]
+		then
+			RES="${LAST_RES}"
+		fi
+		LAST_RES="${RES}"
+		echo "${NAME} ${RES}" >> "${LIST_COMMANDS}"
+	fi
 done 10<"${LIST_PACKAGES}"
+flush_commands

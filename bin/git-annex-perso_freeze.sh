71cce379d5c5f752924809e8909517402e54b34c
Samuel Loury <konubinixweb@gmail.com>
2014-06-20 08:02:46 +0200

    Add the diskreserve config setting in the git annex perso freeze command

--- bin/git-annex-perso_freeze.sh
+++ bin/git-annex-perso_freeze.sh
@@ -4,6 +4,9 @@ source "git-annex-perso_lib.sh"
 
 gaps_log "Re run the config in case it changed"
 gaps_launch_config
+# this is a general configuration that avoid a lot of trouble when almost
+# running out of disk space
+git config annex.diskreserve 50M
 
 gaps_log "Freezing the repo"
 git-annex-freeze.sh

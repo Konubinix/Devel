5f9e53159eaecf7137e4653300f950bd9016c8f3
Samuel Loury <konubinixweb@gmail.com>
2014-06-23 10:38:06 +0200

    Add a script to import an ofx file into ledger

---
+++ bin/konix_ledger_import_ofx.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eu
+OFX_FILE_PATH="$(readlink -f "${1}")"
+OFX_FILE_NAME="$(basename "${OFX_FILE_PATH}")"
+FID=1
+DATE="$(date)"
+ACCOUNT="$(echo "${OFX_FILE_NAME}"|sed -r "s/^([^_]+)_([^_]+)_.+$/\2:\1/")"
+
+# ledger does not like brackets
+sed -i 's/[()]/_/g' "${OFX_FILE_PATH}"
+
+# ledger-autosync does not explicitly encode output
+export PYTHONIOENCODING=utf_8
+
+CMD="ledger-autosync --assertions --fid 1 -a ${ACCOUNT}"
+if ! ledger accounts|grep -q "${ACCOUNT}"
+then
+    CMD="${CMD} --initial"
+fi
+CMD="$CMD ${OFX_FILE_PATH}"
+{
+    echo ""
+    echo "; ${DATE}"
+    echo "; import command: $CMD"
+    echo ""
+    ${CMD}
+    echo "; end of import"
+} >> "${LEDGER_FILE}"

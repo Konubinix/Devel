fe37d666bca51fa78c4f3bea45d7ec83496f269d
Samuel Loury <konubinixweb@gmail.com>
2014-07-01 15:09:58 +0200

    Get the buffer/show-all feature from the compilation module to the more general functions

--- config/elfiles/config/300-KONIX_functions.el
+++ config/elfiles/config/300-KONIX_functions.el
@@ -14,6 +14,32 @@
 	)
   )
 
+(defun konix/buffer/show-all (buffer_list)
+  (delete-other-windows)
+  (let* (
+		 (size_of_each 0)
+		 (direction_horizontal nil);(> (window-width) (window-height)))
+		 (size_function
+		  (if direction_horizontal
+			  'window-width
+			'window-height
+			)
+		  )
+		 )
+	(setq size_of_each
+		  (/
+		   (funcall size_function)
+		   (length buffer_list)
+		   )
+		  )
+	(dotimes (i (1- (length buffer_list)))
+	  (switch-to-buffer (nth i buffer_list))
+	  (split-window nil (- (funcall size_function) size_of_each) direction_horizontal)
+	  )
+	(switch-to-buffer (car (last buffer_list)))
+	)
+  )
+
 (defun konix/time-string-to-hours (timestring)
   (if (string-match "\\([0-9]+\\):\\([0-9]+\\)" timestring)
 	  (let* (

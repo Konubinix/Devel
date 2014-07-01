f9830986414fcbe77342fcf8a4c0eaec816d1a5b
Samuel Loury <konubinixweb@gmail.com>
2014-07-01 15:09:58 +0200

    Get the buffer/show-all feature from the compilation module to the more general functions

--- config/elfiles/KONIX_compilation.el
+++ config/elfiles/KONIX_compilation.el
@@ -542,30 +542,7 @@ PARAM : a string with parameters given to make
 
 (defun konix/compile/buffer/show-all ()
   (interactive)
-  (delete-other-windows)
-  (let* (
-		 (compil_list (konix/compile/buffer/_get))
-		 (size_of_each 0)
-		 (direction_horizontal nil);(> (window-width) (window-height)))
-		 (size_function
-		  (if direction_horizontal
-			  'window-width
-			'window-height
-			)
-		  )
-		 )
-	(setq size_of_each
-		  (/
-		   (funcall size_function)
-		   (length compil_list)
-		   )
-		  )
-	(dotimes (i (1- (length compil_list)))
-	  (switch-to-buffer (nth i compil_list))
-	  (split-window nil (- (funcall size_function) size_of_each) direction_horizontal)
-	  )
-	(switch-to-buffer (car (last compil_list)))
-	)
+  (konix/buffer/show-all (konix/compile/buffer/_get))
   )
 
 (defun konix/compile/buffer/clean-all ()

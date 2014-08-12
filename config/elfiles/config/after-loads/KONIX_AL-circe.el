93ea163ca44e084fcdbb7c9927c6b4d1d371c43f
Samuel Loury <konubinixweb@gmail.com>
2014-08-12 08:22:02 +0200

    Add some customizations of circe and lui

---
+++ config/elfiles/config/after-loads/KONIX_AL-circe.el
@@ -0,0 +1,87 @@
+;;; KONIX_AL-circe.el ---
+
+;; Copyright (C) 2014  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defface circe-my-message-face
+  '(
+	(
+	 ((class color)
+	  (background dark))
+	 (:foreground "green")
+	 )
+	(
+	 ((class color)
+	  (background light))
+	 (:foreground "green")
+	 )
+	)
+  ""
+  )
+
+(defface circe-originator-face
+  '(
+	(
+	 ((class color)
+	  (background dark))
+	 (:foreground "blue")
+	 )
+	(
+	 ((class color)
+	  (background light))
+	 (:foreground "blue")
+	 )
+	)
+  ""
+  )
+
+(defun konix/circe-list-connected-network ()
+  (mapcar
+   (lambda (buffer)
+	 (with-current-buffer
+		 buffer
+	   circe-server-network)
+	 )
+   (circe-server-buffers)
+   )
+  )
+
+(defun konix/circe-jump-network (network)
+  (interactive
+   (list
+	(completing-read "Network: " (konix/circe-list-connected-network))
+	)
+   )
+  (catch 'return
+    (dolist (buffer (circe-server-buffers))
+	  (when (with-current-buffer buffer (string= network circe-server-network))
+		(pop-to-buffer buffer)
+		(throw 'return t)
+		)
+	  )
+	)
+  )
+
+(provide 'KONIX_AL-circe)
+;;; KONIX_AL-circe.el ends here

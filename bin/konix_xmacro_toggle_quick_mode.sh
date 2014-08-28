4fb1e3962defe61093b4fc9173a509272baaa767
Samuel Loury <konubinixweb@gmail.com>
2014-08-28 16:04:58 +0200

    Make xmacro toggle quick more verbose

--- bin/konix_xmacro_toggle_quick_mode.sh
+++ bin/konix_xmacro_toggle_quick_mode.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+set -eu
 XMACRO_FILE="$HOME/.konix_xmacro_quick_mode"
+
 if [ -e "$XMACRO_FILE" ]
 then
     konix_display.py "Toggling xmacro mode off"
+    set +e
     cat "$XMACRO_FILE" | xargs kill
+    set -e
     rm "$XMACRO_FILE"
 else
-    konix_display.py "Toggling xmacro mode on"
     xbindkeys -n -f "$KONIX_CONFIG_DIR/xbindkeys/xmacro" &
-    echo $! > "$XMACRO_FILE"
+    pid=$!
+    if [ "$?" == "0" ]
+    then
+        konix_display.py "Toggling xmacro mode on, use F12 to rerun"
+        echo "$pid" > "$XMACRO_FILE"
+    else
+        konix_display.py "Could not create the bind key"
+    fi
 fi

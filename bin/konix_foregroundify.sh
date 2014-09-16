dba206c79b6afbfca783985acbbc69f27c8a0cff
Samuel Loury <konubinixweb@gmail.com>
2014-09-16 09:16:05 +0200

    Add a script that foregroundify a daemon, provided it may be given a pid file to write to

---
+++ bin/konix_foregroundify.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+pidfile="$1"
+shift
+command="$@"
+
+# Proxy signals
+function kill_app(){
+    kill $(cat "$pidfile")
+    exit 0 # exit okay
+}
+trap "kill_app" SIGINT SIGTERM
+
+# Launch daemon
+$command
+sleep 2
+
+# Loop while the pidfile and the process exist
+while [ -f "$pidfile" ] && kill -0 $(cat "$pidfile")
+do
+    sleep 5
+done
+exit 1000 # exit unexpected

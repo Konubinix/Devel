6bc746da8784c0823b4f2202b42e6f5405cf1db0
Samuel Loury <konubinixweb@gmail.com>
2014-09-18 17:26:33 +0200

    ask for automatic metadata generation in git annex

--- bin/git-annex-perso_freeze.sh
+++ bin/git-annex-perso_freeze.sh
@@ -6,7 +6,8 @@ gaps_log "Re run the config in case it changed"
 gaps_launch_config
 # this is a general configuration that avoid a lot of trouble when almost
 # running out of disk space
-git config annex.diskreserve 50M
+git -c core.bare=false config annex.diskreserve 50M
+git -c core.bare=false config annex.genmetadata true
 
 gaps_log "Freezing the repo"
 git-annex-freeze.sh

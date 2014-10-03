957becf22ff84406c288b44bb8c7fc2e4ba5ef30
Samuel Loury <konubinixweb@gmail.com>
2014-10-03 21:19:42 +0200

    Display the org clock string in the frame title as well
    It is MUUUUCH more easy to see this way

--- config/elfiles/config/after-loads/KONIX_AL-org.el
+++ config/elfiles/config/after-loads/KONIX_AL-org.el
@@ -1610,6 +1610,7 @@ to be organized.
 (setq-default org-clock-persist (quote clock))
 (setq-default org-clock-persist-file (expand-file-name "org-clock-save.el" user-emacs-directory))
 (setq-default org-clock-persist-query-save t)
+(setq-default org-clock-clocked-in-display 'both)
 (setq-default org-clock-report-include-clocking-task t)
 (setq-default org-columns-default-format "%CATEGORY %TODO %90ITEM %1PRIORITY
 %10Effort{:} %10CLOCKSUM %10CLOCKSUM_T %ORDERED %allocate %BLOCKER")

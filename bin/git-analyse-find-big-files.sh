aaf6243bc8802efe6bac0d4665d032d8f9b483fa
Samuel Loury <konubinixweb@gmail.com>
2014-10-14 18:26:58 +0200

    Add some commands to help introspect git packs

---
+++ bin/git-analyse-find-big-files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# http://naleid.com/blog/2012/01/17/finding-and-purging-big-files-from-git-history
+if ! [ -f bigobjects.txt ]
+then
+    echo "need to launch git-find-big-blobs.sh"
+    git-find-big-blobs.sh > bigobjects.txt
+fi
+
+if ! [ -f allfileshas.txt ]
+then
+    echo "need to launch git-generate-blob-to-sha-list.sh"
+    git-generate-blob-to-sha-list.sh > allfileshas.txt
+fi
+rm -f bigtosmall.txt
+
+for SHA in `cut -f 1 -d\  < bigobjects.txt`; do
+echo $(grep $SHA bigobjects.txt) $(grep $SHA allfileshas.txt) | awk '{print $1,$3,$7}' >> bigtosmall.txt
+done;
---
+++ bin/git-generate-blob-to-sha-list.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# http://naleid.com/blog/2012/01/17/finding-and-purging-big-files-from-git-history
+git rev-list --objects --all

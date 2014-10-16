d665c556865a3c9b8fd649bb6ebd39fae4798b9a
Samuel Loury <konubinixweb@gmail.com>
2014-10-16 08:38:28 +0200

    Add a script to aggressively prune unreachable objects
    It is useful to clean old git annexes that grew a lot

---
+++ bin/git-prune-aggressive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# http://stackoverflow.com/questions/7432912/git-cant-find-blob-want-to-get-rid-of-it-from-pack
+echo "Expires the reflog"
+git reflog expire --expire-unreachable=now --all
+echo "Reconstruct the packs with -A to unpack loose objects"
+git repack -Ad
+echo "Prune loose objects"
+git gc --prune=now

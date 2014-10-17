6548e65426843eb3c08759d5ec91dbc9d711b74a
Samuel Loury <konubinixweb@gmail.com>
2014-10-17 08:45:27 +0200

    Add an hint to have a more aggressive pruning

--- bin/git-prune-aggressive.sh
+++ bin/git-prune-aggressive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # http://stackoverflow.com/questions/7432912/git-cant-find-blob-want-to-get-rid-of-it-from-pack
+echo "You should think about removing the wip branches that may point to old content"
 echo "Expires the reflog"
 git reflog expire --expire-unreachable=now --all
 echo "Reconstruct the packs with -A to unpack loose objects"

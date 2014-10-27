6f784944c1cc32850a725bdc9fa15df5292f5418
Samuel Loury <konubinixweb@gmail.com>
2014-10-27 12:21:04 +0100

    Use the new path to the debian chroot

--- bin/konix_android_intent_open.sh
+++ bin/konix_android_intent_open.sh
@@ -3,7 +3,7 @@
 FILE="${1}"
 FILE="$(readlink -f "$FILE")"
 MIME="$(mimetype -b $FILE)"
-FILE_DROID="$(echo $FILE|sed 's-/home-/data/home-')"
+FILE_DROID="$(echo $FILE|sed 's-/home-/data/debian/home-')"
 am start --user 0 -a android.intent.action.VIEW -t "${MIME}" -d "file://${FILE_DROID}"
 #-t text/html -d file:///sdcard/tpt/telepathy.freedesktop.org/doc/book/index.html
 #-n org.zirco/.ui.activities.MainActivity

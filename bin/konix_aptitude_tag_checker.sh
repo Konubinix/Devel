8e2abfe082ab2844b1cb8dd7d519ab25bacfe04b
Samuel Loury <konubinixweb@gmail.com>
2014-11-07 09:37:27 +0100

    Add a script to warn the user about not tagged aptitude packages

---
+++ bin/konix_aptitude_tag_checker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# use this in cron with something like
+# 0 1 * * * path_to_here/konix_aptitude_tag_checker.sh
+
+LIST_PACKAGES="$(mktemp)"
+trap "rm '${LIST_PACKAGES}'" 0
+
+aptitude search '~i ?not(~M) ?not(?user-tag(.))' > "${LIST_PACKAGES}"
+if [ "$(wc -l < "${LIST_PACKAGES}")" == "0" ]
+then
+    exit 0
+else
+    echo "Those packages should have a tag" >&2
+    echo "################################" >&2
+    cat "${LIST_PACKAGES}" >&2
+    exit 1
+fi

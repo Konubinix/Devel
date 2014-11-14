cb1d5d9580b68edd48e833b40dc89438bec3cedd
Samuel Loury <konubinixweb@gmail.com>
2014-11-14 08:04:42 +0100

    Attempt to clean a bit the organisation of the script

--- bin/konix_timer.py
+++ bin/konix_timer.py
@@ -18,6 +18,9 @@ run = False
 
 EVT_CURRENT_TIME_ID = wx.NewId()
 
+########################
+## Notification stuff ##
+########################
 def bell():
     konix_notify.main("ALARM !")
     try:
@@ -34,11 +37,19 @@ def bell():
     except:
         pass
 
+###########################
+## Time management stuff ##
+###########################
 class CurrentTime(wx.PyEvent):
     def __init__(self, current_time):
         wx.PyEvent.__init__(self)
         self.SetEventType(EVT_CURRENT_TIME_ID)
         self.current_time = current_time
+
+def onCurrentTime(evt):
+    global window
+    with a_lock:
+        window["time"].text = str(evt.current_time)
 
 # http://wiki.wxpython.org/LongRunningTasks
 def timer():
@@ -52,21 +63,6 @@ def timer():
                 # indicate the current time
                 wx.PostEvent(window.app, CurrentTime(current_time))
 
-def onCurrentTime(evt):
-    global window
-    with a_lock:
-        window["time"].text = str(evt.current_time)
-
-def on_load(evt):
-    print "initializing!"
-    global time_widget, initial_time, window
-    time_widget = evt.window['time']
-    # make sure the window will receive the current time event
-    window = evt.window
-    window.app.Connect(-1, -1, EVT_CURRENT_TIME_ID, onCurrentTime)
-    initial_time = int(time_widget.text)
-    thread.start_new_thread(timer, ())
-
 def start():
     global time_widget, run, initial_time
     with a_lock:
@@ -75,20 +71,6 @@ def start():
         else:
             initial_time = int(time_widget.text)
         run = True
-
-def on_start_click(evt):
-    start()
-
-def on_time_change(evt):
-    global time_widget, run, initial_time
-    current_time = int(evt.window['time'].text or 0)
-    if current_time <= 0 and run:
-        run = False
-        bell()
-
-def on_time_keypress(evt):
-    if evt.key == 13:
-        start()
 
 def stop():
     global time_widget, run
@@ -101,9 +83,9 @@ def stop():
         except:
             pass
 
-def on_stop_click(evt):
-    stop()
-
+##########################
+## Remote control stuff ##
+##########################
 class MyService(rpyc.Service):
     def exposed_start(self):
         start()
@@ -114,8 +96,41 @@ def start_rpyc_server():
     server = ThreadedServer(MyService, port = 12345)
     server.start()
 
-if __name__ == '__main__':
+###############
+## Gui stuff ##
+###############
+def on_load(evt):
+    print "initializing!"
+    global time_widget, initial_time, window
+    time_widget = evt.window['time']
+    # make sure the window will receive the current time event
+    window = evt.window
+    window.app.Connect(-1, -1, EVT_CURRENT_TIME_ID, onCurrentTime)
+    initial_time = int(time_widget.text)
+    thread.start_new_thread(timer, ())
 
+
+def on_time_change(evt):
+    global time_widget, run, initial_time
+    current_time = int(evt.window['time'].text or 0)
+    if current_time <= 0 and run:
+        run = False
+        bell()
+
+def on_time_keypress(evt):
+    if evt.key == 13:
+        start()
+
+def on_start_click(evt):
+    start()
+
+def on_stop_click(evt):
+    stop()
+
+##########
+## Main ##
+##########
+if __name__ == '__main__':
     thread.start_new_thread(start_rpyc_server, ())
     window = gui.load()
     gui.main_loop()

648d69065614bc985d9b2b845c8492084fda96a8
Samuel Loury <konubinixweb@gmail.com>
2014-11-20 08:45:27 +0100

    First attempt to create a library of babel

---
+++ config/elfiles/config/after-loads/KONIX_AL-ob-lob.el
@@ -0,0 +1,44 @@
+;;; KONIX_AL-ob-lob.el ---                           -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq-default org-babel-lob-files
+			  (list
+			   (expand-file-name "lob.org" config-dir)
+			   (expand-file-name "config/lob.org" perso-dir)
+			   )
+			  )
+
+(mapc
+ (lambda (file)
+   (when (file-exists-p file)
+	(org-babel-lob-ingest file)
+	)
+   )
+ org-babel-lob-files
+ )
+
+(provide 'KONIX_AL-ob-lob)
+;;; KONIX_AL-ob-lob.el ends here

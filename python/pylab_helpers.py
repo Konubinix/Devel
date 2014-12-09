73725feff4a1cda3a9f7a0fe5c0dd96c0ab1b82f
Samuel Loury <konubinixweb@gmail.com>
2014-12-09 14:18:26 +0100

    Add a helper for pylab

---
+++ python/pylab_helpers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import itertools
+from matplotlib.pyplot import subplots
+import numpy
+
+def scatter_plots(inputs, names=None, title=None, nrows=4):
+  """inputs: lines of vectors"""
+  inputs = numpy.array(inputs)
+  len_of_input = len(inputs[0])
+  assert names is None or len_of_input == len(names), (
+    "The names and the inputs must have the same length"
+  )
+
+  ij = [
+    (i, j)
+    for i, j in itertools.product(
+        range(len_of_input),
+        range(len_of_input))
+    if i < j]
+
+  ncols = len(ij) / nrows
+  f, sp = subplots(nrows, ncols)
+  if title:
+    f.suptitle(title)
+  for a, (i,j) in zip(sp.flat, ij):
+    a.scatter(inputs[:, i], inputs[:, j])
+    a.set_title("{} vs {}".format(names and names[i] or i,
+                                  names and names[j] or j))

83cea014d7f9219a04bde531a5e108188a762e9a
Samuel Loury <konubinixweb@gmail.com>
2014-12-16 14:52:28 +0100

    Add an helper library

---
+++ python/konix_helpers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import collections
+
+def namedtuples_default_values(name, keys, default_values=None):
+    default_values = default_values or {}
+    parent = collections.namedtuple(
+        name,
+        keys,
+    )
+    res = type(
+        name,
+        (parent,),
+        {}
+    )
+    def __new__(cls, **kwarg):
+        attrs = {k: default_values.get(k, None) for k in keys}
+        attrs.update(kwarg)
+        return parent.__new__(cls, **attrs)
+    setattr(res, "__new__", __new__)
+    return res

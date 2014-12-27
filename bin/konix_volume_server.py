e3be23af0e152991903eb1d5403f7e087f71ec88
Samuel Loury <konubinixweb@gmail.com>
2014-12-27 16:32:11 +0100

    Add a way to control the volume of a server from the phone

---
+++ bin/konix_volume_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import os
+import subprocess
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+# Create server
+server = SimpleXMLRPCServer(("0.0.0.0", 9633),
+                            requestHandler=RequestHandler)
+server.register_introspection_functions()
+
+def get_volume():
+    """Get the volume"""
+    p=subprocess.Popen(
+        ["konix_get_volume.sh"],
+        stdout=subprocess.PIPE)
+    return int(p.stdout.read())
+server.register_function(get_volume)
+
+def set_volume(volume):
+    """Get the volume"""
+    os.system("konix_set_volume.sh {}".format(volume))
+    return True
+server.register_function(set_volume)
+
+server.serve_forever()

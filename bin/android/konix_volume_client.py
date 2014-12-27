05dc7ea28734d748b2d888817b817042a37e494a
Samuel Loury <konubinixweb@gmail.com>
2014-12-27 16:02:17 +0000

    Add the OK button used to exit the application

--- bin/android/konix_volume_client.py
+++ bin/android/konix_volume_client.py
@@ -3,15 +3,16 @@
 
 import xmlrpclib
 import os
+import sys
 server = xmlrpclib.ServerProxy(os.environ["KONIX_VOLUME_SERVER"])
 
 from konix_android import droid
-
 droid.dialogCreateSeekBar(
     int(server.get_volume()),
     150,
     "Volume",
     "Volume remote control")
+droid.dialogSetPositiveButtonText("OK")
 droid.dialogShow()
 
 while droid.eventWaitFor("dialog"):
@@ -20,5 +21,8 @@ while droid.eventWaitFor("dialog"):
         result = droid.eventPoll().result
     if type(result) == list:
         result = result[0]
+    if result["data"]["which"] == "positive":
+        # OK pressed
+        sys.exit(0)
     progress = result["data"]["progress"]
     server.set_volume(str(progress) + "%")

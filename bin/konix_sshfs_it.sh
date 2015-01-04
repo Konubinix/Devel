b543dc6090d5089ce4ab7179822481e9763d1437
Samuel Loury <konubinixweb@gmail.com>
2015-01-04 15:22:36 +0100

    Make sure the sshfs commands follows the symbolic links

--- bin/konix_sshfs_it.sh
+++ bin/konix_sshfs_it.sh
@@ -7,4 +7,4 @@ REMOTE_PATH="${2}"
 set -u
 DIR="${KONIX_SSHFS_ROOT}/${MACHINE}"
 mkdir -p "${DIR}"
-sshfs "${MACHINE}:${REMOTE_PATH}" "${DIR}"
+sshfs -ofollow_symlinks "${MACHINE}:${REMOTE_PATH}" "${DIR}"

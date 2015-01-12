e0aee0a0a61c2302c8d26196e8ea350eb1bcdf8f
Samuel Loury <konubinixweb@gmail.com>
2015-01-12 15:59:56 +0100

    The libreoffice convert pdf command now keeps the original extension

--- bin/konix_libreoffice_convert_pdf.sh
+++ bin/konix_libreoffice_convert_pdf.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
+set -ue
 FILE="$1"
-PDF="$(echo "${FILE}"|sed -r 's/^(.+)\.[^.]+$/\1/').pdf"
+EXT="${FILE##*.}"
+FILE_DOUBLE_EXT="${FILE}.${EXT}"
+PDF="$(echo "${FILE_DOUBLE_EXT}"|sed -r 's/^(.+)\.[^.]+$/\1/').pdf"
 if [ -e "${PDF}" ]
 then
-    rm "${PDF}"
+    rm -f "${PDF}"
 fi
-
+if [ -e "${FILE_DOUBLE_EXT}" ]
+then
+    rm -f "${FILE_DOUBLE_EXT}"
+fi
+cp "${FILE}" "${FILE_DOUBLE_EXT}"
+trap "rm -f '${FILE_DOUBLE_EXT}'" 0
 OUTDIR="$(dirname "${FILE}")"
 
-soffice --headless --convert-to pdf --outdir "$OUTDIR" "$FILE"
+soffice --headless --convert-to pdf --outdir "$OUTDIR" "${FILE_DOUBLE_EXT}"

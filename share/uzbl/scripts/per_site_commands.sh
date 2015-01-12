4774b66e3906296dcf56d1df61980a2ef472e52e
Samuel Loury <konubinixweb@gmail.com>
2015-01-12 16:04:59 +0100

    More verbose script to ease debugging

--- share/uzbl/scripts/per_site_commands.sh
+++ share/uzbl/scripts/per_site_commands.sh
@@ -6,6 +6,7 @@ export UZBL_DATA_DIR
 COMMANDS_FILES="${KONIX_UZBL_PER_SITE_COMMANDS_FILES:-${UZBL_DATA_DIR%%/}/uzbl_commands.conf}"
 
 MY_DIR="$(dirname "$0")"
+
 IFS=":"
 for COMMAND_FILE in ${COMMANDS_FILES}
 do

98d7f9bd23f9dc87367f6f9e2516a58e324c4eb4
Samuel Loury <konubinixweb@gmail.com>
2015-01-26 08:30:18 +0100

    No more writing of gaps_log. I never look at them anyway

--- bin/logaps.sh
+++ bin/logaps.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -x
 
-tmp_log=`mktemp`
 cd "${KONIX_PERSO_DIRS}"
-mkdir -p "${KONIX_PERSO_DIR}/data/gaps_logs/"
-LOG="${KONIX_PERSO_DIR}/data/gaps_logs/gaps_log-$(hostname)_$(date "+%y%m%d_%H%M%S").txt"
 echo "# ######################################################################
 # Fixing Remotes
 # ######################################################################"
@@ -11,7 +8,4 @@ mr -s fix
 echo "# ######################################################################
 # Gapsing Remotes
 # ######################################################################"
-mr -s gaps -d "$@" 2>&1 | tee "${tmp_log}"
-RES=${PIPESTATUS[0]}
-mv "${tmp_log}" "${LOG}"
-exit $RES
+mr -s gaps -d "$@"

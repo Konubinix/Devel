7b291dca2221958d45fe461ec4bba7ecbb3cd520
Samuel Loury <konubinixweb@gmail.com>
2015-01-26 08:42:00 +0100

    Rename some helpers to make them more identifiable

--- python/pylab_helpers.py
+++ python/pylab_helpers.py
@@ -6,10 +6,17 @@ import matplotlib
 from matplotlib.pyplot import subplots
 import numpy
 
-def scatter_plots(inputs, names=None, title=None, nrows=4):
-  """inputs: lines of vectors"""
+def timeseries_scatter_plots(inputs, names=None, title=None, nrows=4):
+  """inputs: matrix of measures
+  each line is a measure. len(inputs) is the number of measures. len(inputs[0])
+  is the number of dimensions of the measure.
+
+This function plots the scatter plot comparing the distribution of point for each
+dimension compared with other dimensions
+  """
   inputs = numpy.array(inputs)
   len_of_input = len(inputs[0])
+  names = names or inputs.dtype.names or [str(i) for i in xrange(len_of_input)]
   assert names is None or len_of_input == len(names), (
     "The names and the inputs must have the same length"
   )
@@ -30,10 +37,18 @@ def scatter_plots(inputs, names=None, title=None, nrows=4):
     a.set_title("{} vs {}".format(names and names[i] or i,
                                   names and names[j] or j))
 
-def plot_all(inputs, names=None, title=None, nrows=4):
-  """inputs: lines of vectors"""
+def timeseries_plot_all(inputs, names=None, title=None, nrows=4):
+  """inputs: matrix of measures
+  each line is a measure. len(inputs) is the number of measures. len(inputs[0])
+  is the number of dimensions of the measure.
+
+This function plots all measured series according to each dimension. X values
+are the occurrence, Y values are the measured values. Each plot represents a
+dimension.
+  """
   len_of_input = len(inputs[0])
-  assert names is None or len_of_input == len(names), (
+  names = names or inputs.dtype.names or [str(i) for i in xrange(len_of_input)]
+  assert len_of_input == len(names), (
       "The names and the inputs must have the same length"
   )
   nrows = min(nrows, len_of_input)
@@ -43,6 +58,6 @@ def plot_all(inputs, names=None, title=None, nrows=4):
       sp = numpy.array([sp,])
   if title:
       f.suptitle(title)
-  for figure, name in zip(sp.flat, inputs.dtype.names):
-      figure.plot(inputs[name])
+  for figure, input, name in zip(sp.flat, inputs.T, names):
+      figure.plot(input)
       figure.set_title(name)

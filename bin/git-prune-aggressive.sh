02d2d8d44ffcb91401a1f839f4fe3f6890948934
Samuel Loury <konubinixweb@gmail.com>
2015-02-06 07:23:02 +0000

    Prune ALL the reflog

--- bin/git-prune-aggressive.sh
+++ bin/git-prune-aggressive.sh
@@ -3,7 +3,7 @@
 # http://stackoverflow.com/questions/7432912/git-cant-find-blob-want-to-get-rid-of-it-from-pack
 echo "You should think about removing the wip branches that may point to old content"
 echo "Expires the reflog"
-git reflog expire --expire-unreachable=now --all
+git reflog expire --expire=all --expire-unreachable=all --all
 echo "Reconstruct the packs with -A to unpack loose objects"
 git repack -Ad
 echo "Prune loose objects"

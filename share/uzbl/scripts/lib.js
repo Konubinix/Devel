dfbda028ab78a1d4b81f76de7f798ac31fe15bd5
Samuel Loury <konubinixweb@gmail.com>
2015-02-17 08:20:38 +0100

    Notify the scroll level in uzbl

--- share/uzbl/scripts/lib.js
+++ share/uzbl/scripts/lib.js
@@ -1,16 +1,16 @@
 function findPos(obj) {
     var curleft = curtop = 0;
     if (obj.offsetParent) {
-	do {
-	    curleft += obj.offsetLeft;
-	    curtop += obj.offsetTop;
-	} while (obj = obj.offsetParent);
-	return [curleft,curtop];
+		do {
+			curleft += obj.offsetLeft;
+			curtop += obj.offsetTop;
+		} while (obj = obj.offsetParent);
+		return [curleft,curtop];
     }
 }
 
 var my_scroll_tid = null;
-var scroll_diff = 1800;
+var scroll_diff = 1200;
 var scroll_sense = 0;
 var scroll_delta = 100;
 var scroll_max = 2000;
@@ -25,18 +25,19 @@ function update_scroll()
 {
     if(scroll_diff > scroll_max)
     {
-	scroll_diff = scroll_max;
+		scroll_diff = scroll_max;
     }
     if(scroll_diff < 0)
     {
-	scroll_diff = 0;
+		scroll_diff = 0;
     }
     clearTimeout(my_scroll_tid);
     my_scroll_tid = null;
     if(scroll_diff * scroll_sense != 0)
     {
-	my_scroll_tid = setInterval(my_scroll, scroll_max - Math.abs(scroll_diff));
+		my_scroll_tid = setInterval(my_scroll, scroll_max - Math.abs(scroll_diff));
     }
+	return [scroll_diff, scroll_sense];
 }
 
 function scroll_down ()
@@ -44,9 +45,9 @@ function scroll_down ()
     scroll_sense = scroll_sense - 1;
     if(scroll_sense < -1)
     {
-	scroll_sense = -1;
+		scroll_sense = -1;
     }
-    update_scroll();
+    return update_scroll();
 }
 
 function scroll_up ()
@@ -54,19 +55,19 @@ function scroll_up ()
     scroll_sense = scroll_sense + 1;
     if(scroll_sense > 1)
     {
-	scroll_sense = 1;
+		scroll_sense = 1;
     }
-    update_scroll();
+    return update_scroll();
 }
 
 function scroll_faster ()
 {
     scroll_diff = scroll_diff + scroll_delta;
-    update_scroll();
+    return update_scroll();
 }
 
 function scroll_slower ()
 {
     scroll_diff = scroll_diff - scroll_delta;
-    update_scroll();
+    return update_scroll();
 }

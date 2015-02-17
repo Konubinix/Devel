567f6df096b2a0967134eb300a21ee024517effb
Samuel Loury <konubinixweb@gmail.com>
2015-02-17 08:14:44 +0100

    Add an helper to insert a timezone into a naive datetime

--- python/datetime_helpers.py
+++ python/datetime_helpers.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 from datetime import datetime as dt, timedelta as td
+
+# cf http://stackoverflow.com/questions/4563272/how-to-convert-a-python-utc-datetime-to-a-local-datetime-using-only-python-stand/13287083#13287083
+def datetime_naive_insert_local_timezone(datetime_):
+    datetime_.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)

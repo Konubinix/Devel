5457a06714a46448842504726a128d3c21c32022
Samuel Loury <konubinixweb@gmail.com>
2015-02-25 17:44:15 +0100

    Add a ugly hack to handle several accounts

--- bin/konix_gcal.py
+++ bin/konix_gcal.py
@@ -11,11 +11,16 @@ parser.add_argument('-m','--make-place',
                     ones, this is very intrusive but prevents any conflicts of appointment.""",
                     action="store_true")
 
+parser.add_argument('-a','--account',
+                    help="""TODO.""",
+                    type=str,
+                    default="")
+
 parser.add_argument('argv', nargs=argparse.REMAINDER)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    program = konix_gcalib.GCall(make_place=args.make_place)
+    program = konix_gcalib.GCall(make_place=args.make_place, account=args.account)
     if len(args.argv) > 0:
         command_line = args.argv[0]
         if len(args.argv) > 1:

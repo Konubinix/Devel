90ec0ed7c01fd1399573774ec43e59944d7f06c3
Samuel Loury <konubinixweb@gmail.com>
2015-03-17 18:17:45 +0100

    Do not use renrot anymore since it polute the exif tags

--- bin/konix_images_rename_rotate.sh
+++ bin/konix_images_rename_rotate.sh
@@ -8,13 +8,23 @@ do_on_directory () {
     echo "### DO ON ${DIR}"
     pushd "${DIR}"
     # renaming part
-    renrot -n "%y%m%d_%H%M%S_#Model#" --no-rotate *.JPG *.jpg
-    # rotating part
-    renrot --no-rename *.JPG *.jpg
+    for file in *.JPG *.jpg
+    do
+        echo "# handling '${file}'"
+        konix_image_reorient.sh "${file}"
+        NEW_NAME="`konix_exif_new_name.py ${file}`"
+        if [ "${NEW_NAME}" != "${file}" ]
+        then
+            echo "${file} -> ${NEW_NAME}"
+            mv "${file}" "${NEW_NAME}"
+        else
+            echo "${file} already has a stable name"
+        fi
+    done
     popd
 }
 
 find -type d | while read dir
-do
-    do_on_directory "${dir}"
-done
+               do
+                   do_on_directory "${dir}"
+               done

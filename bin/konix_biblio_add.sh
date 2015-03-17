575fa9a84257fb5dfaa43255bc9d7b4501816acd
Samuel Loury <konubinixweb@gmail.com>
2015-03-17 18:16:58 +0100

    Improve the bibliography tools

--- bin/konix_biblio_add.sh
+++ bin/konix_biblio_add.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
+source "${KONIX_LIB_DIR}/konix_xapers_lib.sh"
+
 set -eu
 tri="${KONIX_BIBLIO_DIR}/tri"
 mkdir -p "${tri}"
 cd "${tri}"
-git annex addurl --fast "$@"
+dled_file="$(git annex addurl "$@" | grep addurl | sed -r 's/^addurl (.+) \(downloading.+$/\1/')"
+if [ "$?" != "0" ]
+then
+    exit 1
+fi
+
+if [ -z "${dled_file}" ]
+then
+    echo "Failed to process the url" >&2
+    exit 1
+fi
+# see the file
+mimeopen "${dled_file}" &
+
+# adjust the environment
+export KONIX_XAPERS_ADD_TAGS="`konix_gtk_entry.py -n -t "Tags" -i "${KONIX_XAPERS_ADD_TAGS}"`"
+unset KONIX_XAPERS_ADD_AUTO_PROMPT
+unset KONIX_XAPERS_ADD_AUTO_VIEW
+export KONIX_XAPERS_ADD_AUTO_MOVE=1
+konix_xapers_gtk_guess_source
+
+konix_xapers_add.sh --file="${dled_file}" "${SOURCE}"
--- bin/konix_uzbl_biblio_add.sh
+++ bin/konix_uzbl_biblio_add.sh
@@ -3,6 +3,12 @@
 set -eu
 URI="${1}"
 konix_display.py "Adding ${URI} to the biblio"
-konix_biblio_add.sh "${URI}" && \
-    konix_display.py "Added ${URI} to the biblio" || \
-        konix_display.py "FAILED TO ADD ${URI} TO THE BIBLIO"
+RES="$(konix_biblio_add.sh "${URI}" 2>&1)"
+if [ "$?" == "0" ]
+then
+    konix_display.py "Added ${URI} to the biblio with result
+$RES"
+else
+    konix_display.py "FAILED TO ADD ${URI} TO THE BIBLIO
+$RES"
+fi

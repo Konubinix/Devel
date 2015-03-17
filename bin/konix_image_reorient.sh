f504fe9390430c5388c288cf0879ad0f0a40cac6
Samuel Loury <konubinixweb@gmail.com>
2015-03-17 18:17:45 +0100

    Do not use renrot anymore since it polute the exif tags

--- bin/konix_image_reorient.sh
+++ bin/konix_image_reorient.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 INPUT="${1}"
+
+if exiftool -p '$Orientation' "${INPUT}"|grep -q "Horizontal"
+then
+    echo "Already Horizontal"
+    exit 0
+fi
+
 EXT=".${INPUT##*.}"
 TMP_FILE="$(mktemp --suffix "${EXT}")"
 

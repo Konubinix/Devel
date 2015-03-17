970dac3ac9f1b1107524d076634cc422b691d647
Samuel Loury <konubinixweb@gmail.com>
2015-03-17 18:16:32 +0100

    I want to save all the installed packages, not only the unknown ones

--- bin/konix_aptitude_save.sh
+++ bin/konix_aptitude_save.sh
@@ -3,5 +3,5 @@
 set -eux
 DIR="${KONIX_PERSO_DIR}/${HOSTNAME}/aptitude_nd"
 mkdir -p "${DIR}"
-aptitude search '~i ?not(~M) ?not(?user-tag(.))' > "${DIR}/list"
+aptitude search '~i ?not(~M)' > "${DIR}/list"
 cp "/var/lib/aptitude/pkgstates" "${DIR}/state"

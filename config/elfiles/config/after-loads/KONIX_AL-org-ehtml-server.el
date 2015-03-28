b713676b31a4410680a2c4e9822f54d61abd2c9a
Samuel Loury <konubinixweb@gmail.com>
2015-03-28 18:44:01 +0100

    Start some customization of org-ehtml

---
+++ config/elfiles/config/after-loads/KONIX_AL-org-ehtml-server.el
@@ -0,0 +1,33 @@
+;;; KONIX_AL-org-ehtml-server.el ---                 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq-default org-ehtml-docroot org-directory)
+(setq-default org-ehtml-everything-editable t)
+(setq-default org-ehtml-allow-agenda t)
+
+
+(provide 'KONIX_AL-org-ehtml-server)
+;;; KONIX_AL-org-ehtml-server.el ends here

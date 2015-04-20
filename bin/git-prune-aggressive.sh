20227e7d9d9064d84c90a4d7b88d42d8f14add15
Samuel Loury <konubinixweb@gmail.com>
2015-04-20 14:21:34 +0200

    The messages are now easier to see

--- bin/git-prune-aggressive.sh
+++ bin/git-prune-aggressive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # http://stackoverflow.com/questions/7432912/git-cant-find-blob-want-to-get-rid-of-it-from-pack
-echo "You should think about removing the wip branches that may point to old content"
-echo "Expires the reflog"
+echo "## You should think about removing the wip branches that may point to old content"
+echo "## Expires the reflog"
 git reflog expire --expire=all --expire-unreachable=all --all
-echo "Reconstruct the packs with -A to unpack loose objects"
+echo "## Reconstruct the packs with -A to unpack loose objects"
 git repack -Ad
-echo "Prune loose objects"
+echo "## Prune loose objects"
 git gc --aggressive --prune=now

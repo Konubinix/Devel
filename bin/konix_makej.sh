bc433850d79d33015223af9be01f301e573eda56
Samuel Loury <konubinixweb@gmail.com>
2015-04-20 14:22:13 +0200

    Add an option to set the maximum load of the computer when compiling

--- bin/konix_makej.sh
+++ bin/konix_makej.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 USED_JOBS="${JOBS:-$(($(nproc) + 1))}"
-make -j$USED_JOBS "$@"
+if [ -n "${KONIX_MAKE_LOAD_LIMIT}" ]
+then
+    if [ "${KONIX_MAKE_LOAD_LIMIT}" == "auto" ]
+    then
+        LOAD_LIMIT_ARGS="-l $(expr 2 \* $(nproc) - 1)"
+    else
+        LOAD_LIMIT_ARGS="-l ${KONIX_MAKE_LOAD_LIMIT}"
+    fi
+else
+    LOAD_LIMIT_ARGS=""
+fi
+
+make -j$USED_JOBS ${LOAD_LIMIT_ARGS} "$@"

66057eafbbcf6456791db548c9b967b37abd0c1a
Samuel Loury <konubinixweb@gmail.com>
2015-05-26 08:27:44 +0200

    Canonize the PATH environment variable in find_next_in_path to avoid infinite recursions due to duplicates

---
+++ bin/konix_path_canonize.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import argparse
+import sys
+import os
+parser = argparse.ArgumentParser(description="""Canonize a PATH environment variable. It outputs on stdout the version of
+PATH given in stdin with duplicates removed and path canalized.""")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    content = sys.stdin.read().strip()
+    paths = content.split(os.path.pathsep)
+    result = []
+    for path in paths:
+        canonized_path = os.path.realpath(path)
+        if canonized_path not in result:
+            result.append(canonized_path)
+    sys.stdout.write(os.path.pathsep.join(result))

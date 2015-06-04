f36c20dbaaec56a1d2e81193a92043c4f7bf164f
Samuel Loury <konubinixweb@gmail.com>
2015-06-04 08:02:33 +0200

    Addition of some bash libraries to allow to wait for some interactive command

---
+++ lib/pslib.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+PS_WAITING_TIME=2 # seconds
+PS_LS=ps_ls
+PS_SELECT=ps_select
+PS_LS_LINE_TO_PID=ps_ls_line_to_pid
+
+function ps_uid () {
+    id -u
+}
+
+function ps_ls () {
+    pgrep -u "$(ps_uid)" --list-full ''
+}
+
+function ps_ls_line_to_pid () {
+    cut -f 1 -d ' '
+}
+
+function ps_select () {
+    local lines="$("${PS_LS}")"
+    local choice
+    read -p "$(echo "${lines}" | cat -n)
+Which one?" choice
+    local chosen_line="$(echo "${lines}"|head -n"${choice}"|tail -n1)"
+    local chosen_pid="$(echo ${chosen_line}|"${PS_LS_LINE_TO_PID}")"
+    echo "${chosen_pid}"
+}
+
+function ps_wait () {
+    local chosen_match="$1"
+    local chosen_pid
+    if [ -n "${chosen_match}" ]
+    then
+        chosen_pid="$("${PS_LS}"|grep "${chosen_match}"|head -n1|"${PS_LS_LINE_TO_PID}")"
+    else
+        chosen_pid="$("${PS_SELECT}")"
+    fi
+    if [ -z "${chosen_pid}" ]
+    then
+        echo "Could not select a process" >&2
+        return 1
+    fi
+    echo "Waiting for ${chosen_pid} ($(ps --no-headers -p "${chosen_pid}" -o args))"
+    while ps -p "${chosen_pid}" > /dev/null
+    do
+        sleep "${PS_WAITING_TIME}"
+    done
+}
+
+function ps_xeyes () {
+    ps_wait "$@"
+    xeyes
+}

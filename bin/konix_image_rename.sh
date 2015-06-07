b145658e5a1553b46070912a4699d619f1c73b80
Samuel Loury <konubinixweb@gmail.com>
2015-06-07 21:23:05 +0200

    Attempt to make the image renamer work even if the image is not in cwd

--- bin/konix_image_rename.sh
+++ bin/konix_image_rename.sh
@@ -3,7 +3,9 @@
 set -eu
 file="$1"
 NEW_NAME="`konix_exif_new_name.py ${file}`"
-if [ "${NEW_NAME}" != "${file}" ]
+NEW_NAME_CP="$(readlink -f "${NEW_NAME}")"
+FILE_CP="$(readlink -f "${file}")"
+if [ "${NEW_NAME_CP}" != "${FILE_CP}" ]
 then
     echo "${file} -> ${NEW_NAME}"
     mv "${file}" "${NEW_NAME}"

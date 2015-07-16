a55047d47da1fdbd1a14ff481fc17252407b6c87
Samuel Loury <konubinixweb@gmail.com>
2015-07-16 09:03:52 +0200

    Add simple wrappers to use gp, gpy and rbf as simple scikit models

---
+++ python/konix_gptools.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import gptools
+from sklearn.base import BaseEstimator
+from abc import ABCMeta, abstractmethod
+
+import six
+
+class GP(six.with_metaclass(ABCMeta, BaseEstimator)):
+    def fit(self, X, y, n_jobs=1):
+        self.gp = gptools.gaussian_process.GaussianProcess(
+            gptools.Matern52Kernel(X.shape[1]),
+            X=X,
+            y=y)
+        return self
+
+    def predict(self, X):
+        return self.gp.predict(X)[0]
---
+++ python/konix_rbf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from scipy.interpolate import Rbf
+
+from sklearn.base import BaseEstimator
+from abc import ABCMeta, abstractmethod
+
+import six
+
+class RBF(six.with_metaclass(ABCMeta, BaseEstimator)):
+    def fit(self, X, y, n_jobs=1):
+        values = [a for a in X.T] + [y.ravel(),]
+        self.rbf = Rbf(*values, function="gaussian")
+        return self
+
+    def predict(self, X):
+        values = [a for a in X.T]
+        return self.rbf(*values)

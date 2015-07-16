2b7729d3c55aceb180b859082d8b4e1f43325bfd
Samuel Loury <konubinixweb@gmail.com>
2015-07-16 08:51:39 +0200

    Convert __ as spaces in evaluated bookmarks

--- bin/konix_web_bookmark.sh
+++ bin/konix_web_bookmark.sh
@@ -4,7 +4,7 @@ BOOKMARK=`konix_dmenu_custom.sh < "${UZBL_BOOKMARKS_FILE}"|cut -f1 -d' '`
 # if the scheme indicates so, the BOOKMARK must be evaled
 if echo "$BOOKMARK" | grep -q -e "^eval://"
 then
-    BOOKMARK="$(echo "${BOOKMARK}"|sed 's-^eval://--')"
+    BOOKMARK="$(echo "${BOOKMARK}"|sed 's-^eval://--'|sed 's/__/ /')"
     BOOKMARK="$(eval "echo "`echo "$BOOKMARK"`"")"
 fi
 if [ -n "$BOOKMARK" ]

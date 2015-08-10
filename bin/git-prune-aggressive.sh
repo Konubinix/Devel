e76150e3bbcd7c6bebcec204d6d9202c8398e90a
Samuel Loury <konubinixweb@gmail.com>
2015-08-10 17:11:26 +0200

    prune aggressive makes sure there are no views on the way

--- bin/git-prune-aggressive.sh
+++ bin/git-prune-aggressive.sh
@@ -2,6 +2,10 @@
 
 # http://stackoverflow.com/questions/7432912/git-cant-find-blob-want-to-get-rid-of-it-from-pack
 echo "## You should think about removing the wip branches that may point to old content"
+echo "## Remove the annex views"
+git-annex-views-remove.sh
+echo "## Those branches remain, consider whether they are all up to date"
+git branch -a
 echo "## Expires the reflog"
 git reflog expire --expire=all --expire-unreachable=all --all
 echo "## Reconstruct the packs with -A to unpack loose objects"

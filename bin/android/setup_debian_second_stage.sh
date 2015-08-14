417268d5e03d3eab7087d1c90d1142a41b6d74ab
Samuel Loury <konubinixweb@gmail.com>
2015-08-14 22:39:31 +0200

    Cleaner scripts to setup the android image

--- bin/android/setup_debian_second_stage.sh
+++ bin/android/setup_debian_second_stage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+distro=testing
+export LANG=C
+
+/debootstrap/debootstrap --second-stage
+
 cat <<EOF > /etc/apt/sources.list
 deb http://ftp.fr.debian.org/debian/ testing main contrib non-free
 deb-src http://ftp.fr.debian.org/debian/ testing main contrib non-free
 EOF
+apt-get update
 apt-get install aptitude
 aptitude update
 aptitude install vim git-annex python bash-completion openssh-server screen myrepos

1e7f80b31ac8b4409812a3f8aa29f1b4f47d1e48
Samuel Loury <konubinixweb@gmail.com>
2015-09-03 12:39:50 +0200

    Fix recoll calls

--- python/konix_recolllib.py
+++ python/konix_recolllib.py
@@ -13,14 +13,15 @@ import glob
 def find_recoll_in_hierarchy(directory):
     path = pathlib.Path(directory).absolute()
     for parent in [path,] + list(path.parents):
-        candidate = os.path.join(directory, "recoll")
-        if os.path.exists(candidate):
+        candidate = parent.joinpath("recoll")
+        if candidate.exists():
             logger.debug("Found {}".format(candidate))
             return candidate
     return None
 
 def call_recoll(program="recoll", args=None):
     args = args or []
+    args = args + sys.argv[1:]
     recoll_dir = find_recoll_in_hierarchy(".")
     environ = os.environ
     environ["RECOLL_EXTRA_DBS"] = os.pathsep.join(glob.glob(os.path.expanduser("~/.recoll/*")))
@@ -28,8 +29,8 @@ def call_recoll(program="recoll", args=None):
     environ["TMPDIR"] = os.path.expanduser("~/tmp")
     if recoll_dir:
         logger.info("Using {}".format(recoll_dir))
-        subprocess.call([program, "-c", recoll_dir] + sys.argv[1:], env=environ)
+        subprocess.call([program, "-c", str(recoll_dir)] + args, env=environ)
     else:
         environ["RECOLL_ACTIVE_EXTRA_DBS"] = environ["RECOLL_EXTRA_DBS"]
         logger.info("Don't use any recoll dir")
-        subprocess.call([program,] + sys.argv[1:], env=environ)
+        subprocess.call([program,] + args, env=environ)

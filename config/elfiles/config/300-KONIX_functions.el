dee3d9b0341bcd5641efe5e0caa84e0c14996a6c
Samuel Loury <konubinixweb@gmail.com>
2015-09-16 22:09:08 +0200

    Add a way to get access to the server buffer clients if it exists and get an empty list otherwise

--- config/elfiles/config/300-KONIX_functions.el
+++ config/elfiles/config/300-KONIX_functions.el
@@ -1,12 +1,19 @@
 ;; ################################################################################
 ;; General use function
 ;; ################################################################################
+(defun konix/server-buffer-clients()
+  (if (boundp server-buffer-clients)
+	  server-buffer-clients
+	'()
+	)
+  )
+
 (defun konix/buffer-has-client-p (&optional buffer)
   (setq buffer (or buffer (current-buffer)))
   (with-current-buffer buffer
 	(not
 	 (let ((res t))
-	   (dolist (proc server-buffer-clients res)
+	   (dolist (proc (konix/server-buffer-clients) res)
 		 (when (and (memq proc server-clients)
 					(eq (process-status proc) 'open))
 		   (setq res nil))))
@@ -381,7 +388,7 @@ make the line non empty"
 (defun konix/not-client-p (buffer)
   (with-current-buffer buffer
 	(not
-	 server-buffer-clients
+	 (konix/server-buffer-clients)
 	 )
 	)
   )
@@ -391,9 +398,9 @@ make the line non empty"
 current buffer still has clients"
   (not
    (or
-	(not server-buffer-clients)
+	(not (konix/server-buffer-clients))
 	(let ((res t))
-	  (dolist (proc server-buffer-clients res)
+	  (dolist (proc (konix/server-buffer-clients) res)
 		(when (and (memq proc server-clients)
 				   (eq (process-status proc) 'open))
 		  (setq res nil))))
@@ -694,14 +701,14 @@ retourne ('fichier','extension')."
 (defun konix/notify-gtk (msg &optional above_all)
   (let (
 		(args `(
-			   "-n"
-			   "--info"
-			   "-b"
-			   ,konix/notify-gtk/background-color
-			   "-T"
-			   "10000"
-			   "-t"
-			   ))
+				"-n"
+				"--info"
+				"-b"
+				,konix/notify-gtk/background-color
+				"-T"
+				"10000"
+				"-t"
+				))
 		)
 	(when above_all
 	  (add-to-list
@@ -903,29 +910,29 @@ The behavior I want is :
     (ediff-maybe-checkout buf-to-patch)
 
     (ediff-with-current-buffer patch-diagnostics
-	  (insert-buffer-substring patch-buf)
-	  (message "Applying patch ... ")
-	  ;; fix environment for gnu patch, so it won't make numbered extensions
-	  (setq backup-style (getenv "VERSION_CONTROL"))
-	  (setenv "VERSION_CONTROL" nil)
-	  (setq patch-return-code
-			(call-process-region
-			 (point-min) (point-max)
-			 shell-file-name
-			 t   ; delete region (which contains the patch
-			 t   ; insert output (patch diagnostics) in current buffer
-			 nil ; don't redisplay
-			 shell-command-switch   ; usually -c
-			 (format "%s %s %s -o '%s' %s"
-					 ediff-patch-program
-					 ediff-patch-options
-					 ediff-backup-specs
-					 konix/ediff-patch-new-filename
-					 (expand-file-name true-source-filename))
-			 ))
+							   (insert-buffer-substring patch-buf)
+							   (message "Applying patch ... ")
+							   ;; fix environment for gnu patch, so it won't make numbered extensions
+							   (setq backup-style (getenv "VERSION_CONTROL"))
+							   (setenv "VERSION_CONTROL" nil)
+							   (setq patch-return-code
+									 (call-process-region
+									  (point-min) (point-max)
+									  shell-file-name
+									  t   ; delete region (which contains the patch
+									  t   ; insert output (patch diagnostics) in current buffer
+									  nil ; don't redisplay
+									  shell-command-switch   ; usually -c
+									  (format "%s %s %s -o '%s' %s"
+											  ediff-patch-program
+											  ediff-patch-options
+											  ediff-backup-specs
+											  konix/ediff-patch-new-filename
+											  (expand-file-name true-source-filename))
+									  ))
 
-	  ;; restore environment for gnu patch
-	  (setenv "VERSION_CONTROL" backup-style))
+							   ;; restore environment for gnu patch
+							   (setenv "VERSION_CONTROL" backup-style))
 
     (message "Applying patch ... done")
     (message "")
@@ -939,7 +946,7 @@ The behavior I want is :
 		(progn
 		  (with-output-to-temp-buffer ediff-msg-buffer
 			(ediff-with-current-buffer standard-output
-			  (fundamental-mode))
+									   (fundamental-mode))
 			(princ (format
 					"Patch program has failed due to a bad patch file,
 it couldn't apply all hunks, OR
@@ -1016,8 +1023,8 @@ you can still examine the changes via M-x ediff-files"
 		   buf-to-patch target-buf nil
 		   startup-hooks 'epatch))
     (ediff-with-current-buffer ctl-buf
-	  (setq ediff-patchbufer patch-buf
-			ediff-patch-diagnostics patch-diagnostics))
+							   (setq ediff-patchbufer patch-buf
+									 ediff-patch-diagnostics patch-diagnostics))
 
     (bury-buffer patch-diagnostics)
     (message "Type `P', if you need to see patch diagnostics")

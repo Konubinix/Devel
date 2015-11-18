63c0bfe32745e2aaf85a73b4bf9d27921552512e
Samuel Loury <konubinixweb@gmail.com>
2015-11-18 08:15:33 +0100

    I cannot stand looking at the minibuffer and another buffer for completion
    Trying ivy now

--- config/elfiles/config/950-init-stuff.el
+++ config/elfiles/config/950-init-stuff.el
@@ -1,14 +1,10 @@
 ;; Maximize the current frame because I like it
 (require 'maxframe)
 (add-hook 'window-setup-hook 'maxframe/maximize-frame t)
-;; ;; icicles rocks
-;; (load-library "icicles")
-;; (icy-mode t)
-(setq-default helm-command-prefix-key "<f2> h")
-(require 'helm)
-(require 'helm-config)
 
-(require 'helm-descbinds)
+(setq-default ivy-use-virtual-buffers t)
+(require 'ivy)
+(ivy-mode)
 
 ;; till I find out how vc works, I send manually my custom git mode
 (load-library "KONIX_git")

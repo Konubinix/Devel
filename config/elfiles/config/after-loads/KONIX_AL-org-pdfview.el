afd55a90fbf1137356e78b24667266a20210175c
Samuel Loury <konubinixweb@gmail.com>
2015-11-26 17:04:42 +0100

    Add pdf-tools

---
+++ config/elfiles/config/after-loads/KONIX_AL-org-pdfview.el
@@ -0,0 +1,31 @@
+;;; KONIX_AL-org-pdfview.el ---                      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(add-to-list 'org-file-apps '("\\.pdf\\'" . org-pdfview-open))
+(add-to-list 'org-file-apps '("\\.pdf::\\([[:digit:]]+\\)\\'" . org-pdfview-open))
+
+(provide 'KONIX_AL-org-pdfview)
+;;; KONIX_AL-org-pdfview.el ends here

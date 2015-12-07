9ab61eb08f874c3b570a508a3b849394fcf813ef
Samuel Loury <konubinixweb@gmail.com>
2015-12-07 08:00:32 +0100

    Helpful uniquify function that preserves the order

--- python/interactive_helpers.py
+++ python/interactive_helpers.py
@@ -20,3 +20,21 @@ false = False
 true = True
 f = false
 t = true
+
+@Pipe
+def uniquify_preserve_order(seq, idfun=None):
+    # from http://www.peterbe.com/plog/uniqifiers-benchmark
+    # order preserving
+    if idfun is None:
+        def idfun(x): return x
+    seen = {}
+    result = []
+    for item in seq:
+        marker = idfun(item)
+        # in old Python versions:
+        # if seen.has_key(marker)
+        # but in new ones:
+        if marker in seen: continue
+        seen[marker] = 1
+        result.append(item)
+    return result

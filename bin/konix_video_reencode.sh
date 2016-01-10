1ef992bc91c86f6c39077c2b6d2402cfd8953722
Samuel Loury <konubinixweb@gmail.com>
2016-01-10 21:25:43 +0100

    Don't remove the initial video. Handbrake has some weird bug that gets rid of the initial audio track

--- bin/konix_video_reencode.sh
+++ bin/konix_video_reencode.sh
@@ -2,5 +2,5 @@
 
 video="$1"
 video_no_ext="${video%.*}"
-HandBrakeCLI -2 -Z "Universal" -i "${video}" -o "${video_no_ext}.mkv"
-rm -rf "${video}"
+HandBrakeCLI -2 -Z "Universal" -i "${video}" -o "${video_no_ext}.mpg"
+#rm -rf "${video}"

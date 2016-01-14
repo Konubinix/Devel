e0025eab6877591b2297bec1b245bd54cf262da3
Samuel Loury <konubinixweb@gmail.com>
2016-01-14 17:10:38 +0100

    Split the cryptonite script

--- bin/konix_cryptonit.sh
+++ bin/konix_cryptonit.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-set -ue
+set -eu
+FILE="$1"
+FILE_FULL="$(readlink -f "${FILE}")"
+if grep -q "cryptoff" "${FILE_FULL}"
+then
+    echo "${FILE_FULL} already crypted"
+    exit 1
+fi
+if ! echo "${FILE_FULL}" | grep -q "^${KONIX_PERSO_DIR}"
+then
+    echo "${FILE_FULL} should be in ${KONIX_PERSO_DIR}"
+    exit 1
+fi
 CRYPTON="${KONIX_PERSO_DIR}/crypton"
 CRYPTOFF="${HOME}/cryptoff"
-mkdir "${CRYPTOFF}"
-encfs "${CRYPTON}" "${CRYPTOFF}"
+
+FILE_CRYPTOFF="${CRYPTOFF}/${FILE_FULL##${KONIX_PERSO_DIR}/}"
+DIR_CRYPTOFF="$(dirname "${FILE_CRYPTOFF}")"
+read -p "Preparing to replace
+${FILE_FULL}
+by a symlink to
+${FILE_CRYPTOFF}
+Is that OK? " res
+if [ "${res}" != "y" ]
+then
+    echo Aborting
+    exit 1
+fi
+set -x
+mkdir -p "${DIR_CRYPTOFF}"
+mv "${FILE_FULL}" "${FILE_CRYPTOFF}"
+ln -s "${FILE_CRYPTOFF}" "${FILE_FULL}"

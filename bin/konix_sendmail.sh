4a3b1255648f46d7a0de17f874cb7d0eaea954ac
Samuel Loury <konubinixweb@gmail.com>
2016-01-14 17:10:56 +0100

    Make sure the pymsmstpq.queue exists before sending the mail

--- bin/konix_sendmail.sh
+++ bin/konix_sendmail.sh
@@ -26,4 +26,5 @@ then
 fi
 #echo "$@"
 #xmessage "$*"
+mkdir -p "${HOME}/cryptoff/.pymsmtpq.queue"
 pymsmtpq -t --read-envelope-from -oi -C "${KONIX_PERSO_DIR}/msmtprc" --aliases="${KONIX_MSMTP_ALIASES}" --logfile "${KONIX_MSMTP_LOG}" "$@"

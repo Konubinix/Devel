e656fa93ae01a4ce865ecab4380d5558f4f3cbf2
Samuel Loury <konubinixweb@gmail.com>
2016-01-19 21:33:21 +0100

    Handbrake may loose the audio. Use a avconv command explicitly instead

--- bin/konix_video_reencode.sh
+++ bin/konix_video_reencode.sh
@@ -2,5 +2,17 @@
 
 video="$1"
 video_no_ext="${video%.*}"
-HandBrakeCLI -2 -Z "Universal" -i "${video}" -o "${video_no_ext}.mkv"
-#rm -rf "${video}"
+#HandBrakeCLI -2 -Z "Universal" -i "${video}" -o "${video_no_ext}.mkv"
+avconv -i "${video}" \
+	   -r 30 \
+	   -f mp4 \
+	   -s 720x576  \
+	   -vcodec libx264 \
+	   -vprofile high \
+	   -preset slower \
+	   -b:v 2000k \
+	   -maxrate 2000k \
+	   -acodec libvo_aacenc \
+	   -ab 196k \
+	   "${video_no_ext}.mkv"
+rm -rf "${video}"

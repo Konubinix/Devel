5419345294c5925211d0114b90f38af0f43c8f55
Samuel Loury <konubinixweb@gmail.com>
2016-01-25 21:57:51 +0100

    Use supervisord to keep the gpg-agent alive

--- bin/konix_do_with_gpg_agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "${HOME}/init_bin/konix_init_lib.sh"
-import_env
-source "${KONIX_LIB_DIR}/lib_bash.sh"
-gpg_agent_start_KONIX
-
-export DISPLAY=:0.0
-eval "$@"
---
+++ bin/konix_gpg_agent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gpg-agent \
+	--daemon \
+	--enable-ssh-support \
+	--default-cache-ttl "${KONIX_GPG_CACHE_TTL}" \
+	--default-cache-ttl-ssh "${KONIX_GPG_CACHE_TTL_SSH}" \
+	--write-env-file "$GPG_INFO_FILE_NAME" \
+	sleep 2246400000
+# sleep for 100 years

cf5363cec5bc79b6cd18b6dc53e545b4cc9d9d80
Samuel Loury <konubinixweb@gmail.com>
2016-01-28 21:49:22 +0100

    Add the max ttl

--- bin/konix_gpg_agent.sh
+++ bin/konix_gpg_agent.sh
@@ -5,6 +5,8 @@ gpg-agent \
 	--enable-ssh-support \
 	--default-cache-ttl "${KONIX_GPG_CACHE_TTL}" \
 	--default-cache-ttl-ssh "${KONIX_GPG_CACHE_TTL_SSH}" \
+	--max-cache-ttl "${KONIX_MAX_GPG_CACHE_TTL}" \
+	--max-cache-ttl-ssh "${KONIX_MAX_GPG_CACHE_TTL_SSH}" \
 	--write-env-file "$GPG_INFO_FILE_NAME" \
 	sleep 2246400000
 # sleep for 100 years

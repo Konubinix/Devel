ca49172481e7ad0a99013a30ee542bb20a251883
Samuel Loury <konubinixweb@gmail.com>
2016-01-29 08:01:52 +0100

    Use the extpass command if available

--- bin/konix_crypton_mount.sh
+++ bin/konix_crypton_mount.sh
@@ -4,4 +4,9 @@ set -ue
 CRYPTON="${KONIX_PERSO_DIR}/crypton"
 CRYPTOFF="${HOME}/cryptoff"
 mkdir -p "${CRYPTOFF}"
-encfs "${CRYPTON}" "${CRYPTOFF}"
+if which konix_dump_crypton-passphrase.sh > /dev/null
+then
+    encfs --extpass=konix_dump_crypton-passphrase.sh "${CRYPTON}" "${CRYPTOFF}"
+else
+    encfs "${CRYPTON}" "${CRYPTOFF}"
+fi

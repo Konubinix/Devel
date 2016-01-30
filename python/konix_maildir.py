b7e6eb36edc2681fe266306ed01f04549e718d52
Samuel Loury <konubinixweb@gmail.com>
2016-01-30 11:47:17 +0100

    Move the mail library to a python module

---
+++ python/konix_maildir.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import email
+import datetime
+import mailbox
+import html2text
+
+def add_to_maildir(from_, to, subject, date, pure_text_prefix, content, directory):
+    content_lines = content.splitlines()
+
+    text_content = pure_text_prefix + u"\n\n" + html2text.html2text(content)
+    html_content = pure_text_prefix + u"\n\n" + content
+
+    text_content = text_content.encode("utf-8")
+    html_content = html_content.encode("utf-8")
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = from_
+    msg['To'] = to
+    if date:
+        if isinstance(date, basestring):
+            msg['Date'] = date
+        else:
+            msg["Date"] = email.utils.formatdate((int(date.strftime("%s"))), date)
+    plain_part = MIMEText(text_content, _subtype='plain', _charset="utf-8")
+    html_part = MIMEText(html_content, _subtype='html', _charset="utf-8")
+    msg.attach(plain_part)
+    msg.attach(html_part)
+    box = mailbox.Maildir(directory)
+    return box.add(str(msg))

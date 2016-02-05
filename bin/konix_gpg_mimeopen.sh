7bcdc163f425f214a02f28cdaf5a15620715754d
Samuel Loury <konubinixweb@gmail.com>
2016-02-05 12:37:10 +0100

    Add a script to mimeopen a gpg-ed file

---
+++ bin/konix_gpg_mimeopen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILE_PATH="${1}"
+FILE_NAME="${FILE_PATH##*/}"
+set -ue
+DIR="$(mktemp -d)"
+FILE_PATH_NO_GPG="${DIR}/${FILE_NAME%%.gpg}"
+trap 'shred "${DIR}"/* ; rm -r "${DIR}"' 0
+cd "${DIR}"
+gpg --output "${FILE_PATH_NO_GPG}" --decrypt "${FILE_PATH}"
+mimeopen "${FILE_PATH_NO_GPG}"

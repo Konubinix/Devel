c3b3a03395ed9ba53b0183861e06e7c523d3e40e
Samuel Loury <konubinixweb@gmail.com>
2016-02-11 21:51:18 +0100

    Separate the format functions and the add_to_maildir one

--- python/konix_mail.py
+++ python/konix_mail.py
@@ -8,7 +8,28 @@ import datetime
 import mailbox
 import html2text
 
-def add_to_maildir(from_, to, subject, date, pure_text_prefix, content, directory):
+def format_markdown(from_, to, subject, date, content):
+    import markdown
+    m = markdown.Markdown()
+    html_content = m.convert(content)
+    content_lines = content.splitlines()
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = from_
+    msg['To'] = to
+    if date:
+        if isinstance(date, basestring):
+            msg['Date'] = date
+        else:
+            msg["Date"] = email.utils.formatdate((int(date.strftime("%s"))), date)
+    plain_part = MIMEText(content, _subtype='plain', _charset="utf-8")
+    html_part = MIMEText(html_content, _subtype='html', _charset="utf-8")
+    msg.attach(plain_part)
+    msg.attach(html_part)
+    return msg
+
+def format_mail(from_, to, subject, date, pure_text_prefix, content):
     content_lines = content.splitlines()
 
     text_content = pure_text_prefix + u"\n\n" + html2text.html2text(content)
@@ -30,5 +51,9 @@ def add_to_maildir(from_, to, subject, date, pure_text_prefix, content, director
     html_part = MIMEText(html_content, _subtype='html', _charset="utf-8")
     msg.attach(plain_part)
     msg.attach(html_part)
+    return msg
+
+def add_to_maildir(from_, to, subject, date, pure_text_prefix, content, directory):
+    msg = format_mail(from_, to, subject, date, pure_text_prefix, content)
     box = mailbox.Maildir(directory)
     return box.add(str(msg))

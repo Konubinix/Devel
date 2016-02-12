c210b8b70f9772997c362b517131b86ca448a74e
Samuel Loury <konubinixweb@gmail.com>
2016-02-12 08:19:56 +0100

    Make the notification daemon much cleaner

--- bin/konix_notify_daemon.py
+++ bin/konix_notify_daemon.py
@@ -10,44 +10,38 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 class NotificationException(dbus.DBusException):
-	_dbus_error_name = 'konubinix.notificator.NotificationException'
+        _dbus_error_name = 'konubinix.notificator.NotificationException'
 
 class Notification(dbus.service.Object):
-	notification = pynotify.Notification("Message", "")
-	notification.set_timeout(3000);
-
-	@dbus.service.method("konubinix.notificator",
-						 in_signature='si', out_signature='')
-	def Notify(self, message, show_time):
-                logging.info("Notifying message %s for time %s" % (message, show_time))
-		notification = pynotify.Notification("Message", message)
-                logging.debug("Notification element : "+str(notification))
-		notification.set_timeout(show_time);
-		print notification.show()
-
-class UniqueNotification(Notification):
-	notification = pynotify.Notification("Message", "")
-	notification.set_timeout(3000);
-
-	@dbus.service.method("konubinix.notificator",
-						 in_signature='si', out_signature='')
-	def Notify(self, message, show_time):
-                logging.info("Notifying unique message "+message)
-		self.notification.set_timeout(show_time);
-		self.notification.update("Message", message)
-		self.notification.show()
+    notification = pynotify.Notification("Message", "")
+    @dbus.service.method("konubinix.notificator",
+                         in_signature='sibs', out_signature='')
+    def Notify(self, message, show_time, unique, type_):
+        logging.info(
+            "Notifying message {}{} for time {} of type {}".format(
+                "unique " if unique else "",
+                message,
+                show_time,
+                type_,
+            )
+        )
+        if unique:
+            notif = self.notification
+            notif.update("Message", message)
+        else:
+            notif = pynotify.Notification("Message", message)
+        notif.set_timeout(show_time);
+        notif.show()
 
 if __name__ == '__main__':
-	pynotify.init("Konubinix Notificator")
-	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    pynotify.init("Konubinix Notificator")
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
-	session_bus = dbus.SessionBus()
-	name = dbus.service.BusName("konubinix.notificator", session_bus)
-        logging.debug("Registering on debus service "+str(name))
-	Notification(session_bus, '/Notification')
-	Notification.notification.set_timeout(3000)
-	UniqueNotification(session_bus, '/UniqueNotification')
+    session_bus = dbus.SessionBus()
+    name = dbus.service.BusName("konubinix.notificator", session_bus)
+    logging.debug("Registering on debus service "+str(name))
+    notif = Notification(session_bus, '/Notification')
 
-	mainloop = gobject.MainLoop()
-	print "Running Konubinix Notificator."
-	mainloop.run()
+    mainloop = gobject.MainLoop()
+    print "Running Konubinix Notificator."
+    mainloop.run()

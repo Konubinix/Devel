2066691f1650defc5ab020fe5fd9d043931c1eff
Samuel Loury <konubinixweb@gmail.com>
2016-02-24 14:01:27 +0100

    Add a script to move to the maths env

---
+++ bin/envmaths35.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /home/sam/.virtualenvs/maths35/bin/activate
+
+is_sourced () {
+    ! [ "$BASH_SOURCE" == "$0" ]
+}
+
+if ! is_sourced
+then
+    if [ "$#" == "0" ]
+    then
+        echo "source the script or use it to wrap a command line"
+        exit 1
+    fi
+    exec "$@"
+fi

1431373392d8ddd6870eaca0310b5afada53a655
Samuel Loury <konubinixweb@gmail.com>
2016-03-17 21:13:15 +0100

    Make the script work with directories

--- bin/konix_image_rename.sh
+++ bin/konix_image_rename.sh
@@ -2,13 +2,13 @@
 
 set -eu
 file="$1"
-NEW_NAME="`konix_exif_new_name.py ${file}`"
+NEW_NAME="`konix_exif_new_name.py "${file}"`"
 NEW_NAME_CP="$(readlink -f "${NEW_NAME}")"
 FILE_CP="$(readlink -f "${file}")"
 if [ "${NEW_NAME_CP}" != "${FILE_CP}" ]
 then
-    echo "${file} -> ${NEW_NAME}"
+    echo "'${file}' -> '${NEW_NAME}'"
     mv "${file}" "${NEW_NAME}"
 else
-    echo "${file} already has a stable name"
+    echo "'${file}' already has a stable name"
 fi

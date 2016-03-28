b3f5ecb24ad8a9d5a6b0cc98692ce3def7992c6f
Samuel Loury <konubinixweb@gmail.com>
2016-03-28 18:53:25 +0200

    Add some helpers

--- python/opencv_helpers.py
+++ python/opencv_helpers.py
@@ -4,6 +4,8 @@
 from matplotlib import  pyplot as plt
 import matplotlib as mpl
 from pylab_helpers import *
+from matplotlib.widgets import Slider, Button, RadioButtons
+import numpy as np
 
 def mpl_cv2_image_mode_local():
     plt.tick_params(
@@ -24,3 +26,32 @@ def mpl_cv2_image_mode_global():
     mpl.rcParams["axes.grid"] = False
     mpl.rcParams["image.cmap"] = "gray"
     mpl_layout_bigger()
+
+def imshow(image):
+    if len(image.shape) == 2:
+        return plt.imshow(image)
+    else:
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+def points(rc_or_points):
+    return np.concatenate(
+        (
+            rc_or_points[:, :, 1],
+            rc_or_points[:, :, 0]
+        ),
+        axis=1).reshape(
+            rc_or_points.shape
+        )
+
+def cvargwhere(mask):
+    res = np.argwhere(mask)
+    x, y = res.shape
+    return points(res.reshape(x, 1, y))
+
+def drawrectangle(i, points, *args, **kwargs):
+    x, y, w, h = points
+    cv2.rectangle(i, (x, y), (x + w, y + h), *args, **kwargs)
+
+def drawhoughlinesp(img, lines):
+    for x1,y1,x2,y2 in lines[0]:
+        cv2.line(img,(x1,y1),(x2,y2), 255, 3)

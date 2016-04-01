fb705cadcf4efb2709582f47183a941be6bb306b
Samuel Loury <konubinixweb@gmail.com>
2016-04-01 17:19:47 +0200

    Add a way to observe changes

---
+++ python/konix_observers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from collections import defaultdict
+
+class Observer(object):
+    def __init__(self):
+        self.id = None
+        self.hooks = []
+
+observers = defaultdict(Observer)
+
+if __name__ == "__main__":
+    def hook_on_i():
+        from matplotlib import pyplot as plt
+        plt.imshow(i)
+        plt.draw_if_interactive()
+    observers["i"].hooks.append(hook_on_i)

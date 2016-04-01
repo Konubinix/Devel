7d1c45652a5c7a0053f9a72842e9556c88bf287f
Samuel Loury <konubinixweb@gmail.com>
2016-04-01 17:22:08 +0200

    Add a simple odo_helper

---
+++ python/odo_helpers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from odo import odo
+
+def odo_sql_circular_foreign_keys_workaround():
+    import sqlalchemy as sa
+    from datashape import discover
+    @discover.register(sa.ForeignKey, sa.sql.FromClause)
+    def do_not_discover_foreign_key_relationship(fk, parent, parent_measure=None):
+        return {}
+
+    import odo.backends.sql
+    odo.backends.sql.discover_foreign_key_relationship = do_not_discover_foreign_key_relationship
+    print("You can now load databases with circular foreign keys")

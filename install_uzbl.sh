8d7779665f984b7a6fd70daa1d440391b7a5bfac
Samuel Loury <konubinixweb@gmail.com>
2016-04-05 22:42:04 +0200

    Udpate uzbl to a version using webkit2. Yeeeepeee !

--- install_uzbl.sh
+++ install_uzbl.sh
@@ -5,4 +5,6 @@ echo "sai libwebkit2gtk-3.0-dev"
 cd uzbl
 rm -rf install
 make clean
+mkdir -p install/lib/python3.5/site-packages/
+export PYTHONPATH+=":$(pwd)/install/lib/python3.5/site-packages/"
 make PREFIX=$(pwd)/install install

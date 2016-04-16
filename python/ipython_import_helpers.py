54add2c5d8570661b9bee908993586f43c3d1e0a
Samuel Loury <konubinixweb@gmail.com>
2016-04-16 17:31:25 +0200

    Reorganization of the helpers

--- python/ipython_import_helpers.py
+++ python/ipython_import_helpers.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 def import_module_n_helpers(module, ip, populate=True):
     try:
         ip.user_ns[module] = importlib.import_module(module)
-        helpers_name = "ipython_helpers.helpers_{}".format(module)
+        helpers_name = "ipython_helpers_{}".format(module)
         helpers = importlib.import_module(helpers_name)
         ip.user_ns[helpers_name] = helpers
         if populate:

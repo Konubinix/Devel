d662c3acebdda756ea74c706e9f379340f1b2ca4
Samuel Loury <konubinixweb@gmail.com>
2016-04-16 17:30:51 +0200

    Use my new implementation of Pipe

--- python/ipython_completers.py
+++ python/ipython_completers.py
@@ -5,29 +5,10 @@ import pipe
 import IPython
 ip = IPython.get_ipython()
 
-_piped_funcs = set(
-    [s
-     for s in dir(pipe)
-     if (
-             hasattr(getattr(pipe, s), '__ror__')
-             and s != 'Pipe'
-     )
- ]
-    )
-class _newPipe(pipe.Pipe, object):
-    def __init__(self, function):
-        super(_newPipe, self).__init__(function)
-        _piped_funcs.add(function.__name__)
-
-    def __ror__(self, other):
-        self.res = self.function(other)
-        return self.res
-
-pipe.Pipe = _newPipe
 def _pipe_completer(self, event):
     import re
     symbol = re.match('(?P<symbol>.+\\|)', event.symbol).group("symbol")
-    return [symbol + name for name in _piped_funcs]
+    return [symbol + function.__name__ for function in pipe.pipe_functions]
 
 def _greedy_completer(self, event):
     import re
@@ -35,8 +16,8 @@ def _greedy_completer(self, event):
     line = re.match('(?P<line>.+)\\.', event.line).group("line")
     try:
         obj = eval(line, ip.user_global_ns, ip.user_ns)
-    except Exception, e:
-        print e
+    except Exception(e):
+        print(e)
     return [symbol + s for s in dir(obj)]
 
 ip.set_hook('complete_command', _greedy_completer, re_key = '.+\\|.+?\\.[a-zA-Z_-]*$')

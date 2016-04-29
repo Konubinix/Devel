0f603fba2a3b3fdb3f86b141fcec4b6cc923afc1
Samuel Loury <konubinixweb@gmail.com>
2016-04-29 11:07:08 +0200

    Do not hardcode the number of characters at the beginning of the line anymore

--- bin/konix_debian_show_big_packages.sh
+++ bin/konix_debian_show_big_packages.sh
@@ -2,7 +2,7 @@
 # inspired from a work from ThArGos
 
 export LC_ALL=C
-for package in `aptitude -O installsize search ~i | tac | cut -c 5- | cut -d ' ' -f 1`
+for package in `aptitude -O installsize search ~i | tac | sed -r 's|^i +([^ ]+) - .+$|\1|'`
 do
     echo -e "`aptitude reinstall -sy ${package} | grep archive | sed -e "s@.*/\(.*\)B of archives.*@\1@"` \t${package}"
 done

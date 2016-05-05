a09fd14080d8da0a5c34abbc96997aea83a65f6e
Samuel Loury <konubinixweb@gmail.com>
2016-05-05 19:43:11 +0200

    Add some helpers

--- python/ipython_helpers_pandas.py
+++ python/ipython_helpers_pandas.py
@@ -17,4 +17,18 @@ def pandas_dump_hdf(df, file_name, prefix_key="", *args, **kwargs):
             **kwargs
         )
 
+def pandas_mpl_imshow_adjust_ticks(df, x_step=5, y_step=5):
+    import matplotlib.pyplot as plt
+    plt.xticks(
+        range(0, df.shape[0], x_step),
+        df.index[0::x_step]
+    )
+    plt.yticks(
+        range(0, df.shape[1], y_step),
+        df.columns[0::y_step]
+    )
+
 pd.DataFrame.dump_hdf = pandas_dump_hdf
+
+def pd_stripna(input_dataframe):
+    return input_dataframe.dropna(axis=0, how="all").dropna(axis=1, how="all")

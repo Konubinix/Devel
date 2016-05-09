a3304ed6a2a12e66f6fc47fe97fe22f5c3a0cea6
Samuel Loury <konubinixweb@gmail.com>
2016-05-09 21:37:53 +0200

    Add the leaves_only argument

--- python/ipython_helpers_h5py.py
+++ python/ipython_helpers_h5py.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-def hdf_keys(hdf_file, recursive=False):
+import h5py
+import re
+
+def hdf_keys(hdf_file, recursive=False, leaves_only=False):
     import h5py
     file_ = h5py.File(hdf_file, mode="r")
-    keys = []
+    keys = set()
     if recursive:
-        file_.visit(lambda e: keys.append(e))
+        def handle_key(e):
+            if not leaves_only or isinstance(file_[e], h5py._hl.dataset.Dataset):
+                e = re.sub("^(.+)/(index|values)$", r"\1", e)
+                keys.add(e)
+        file_.visit(handle_key)
     else:
-        keys = list(file_)
+        keys = set(file_)
     file_.close()
     return keys

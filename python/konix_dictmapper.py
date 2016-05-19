77714c405a43d581f54f00e3885465a64b669756
Samuel Loury <konubinixweb@gmail.com>
2016-05-19 16:42:11 +0200

    Add a code to map a dictionnary into an objet

---
+++ python/konix_dictmapper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+class DictMapper(object):
+    def __init__(self, dict_):
+        self.dict_ = dict_
+
+    def __dir__(self):
+        return ["dict_"] + list(self.dict_.keys())
+
+    def __getattr__(self, key):
+        if key == "dict_":
+            return super(DictMapper, self).__getattr__(key)
+        value = self.dict_[key]
+        if isinstance(value, dict) or type(value) == dict:
+            return DictMapper(value)
+        return value

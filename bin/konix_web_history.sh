935699120c3fed305760e29d6988b397fa09df56
Samuel Loury <konubinixweb@gmail.com>
2016-06-08 13:15:02 +0200

    Prepare the move to qutebrowser

--- bin/konix_web_history.sh
+++ bin/konix_web_history.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # use the one given as parameter and fallback to the uzbl history file
-HISTORY_FILE="${1:-$UZBL_HISTORY_FILE}"
-HISTORY_=`tac "${HISTORY_FILE}"|konix_dmenu_custom.sh|cut -f3 -d' '`
+HISTORY_FILE="${1:-${XDG_DATA_HOME}/qutebrowser/history}"
+HISTORY_=`tac "${HISTORY_FILE}"|konix_dmenu_custom.sh|cut -f2 -d' '`
 if [ -n "$HISTORY_" ]
 then
     "$BROWSER" "${HISTORY_}"

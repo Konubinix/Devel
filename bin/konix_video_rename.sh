e996e5624b483640e25c974b84ff3e4d039f2f20
Samuel Loury <konubinixweb@gmail.com>
2016-06-12 21:34:42 +0200

    Use avconv to find the date. Much more stable

--- bin/konix_video_rename.sh
+++ bin/konix_video_rename.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -eu
+
 file="$1"
 echo "# handling '${file}'"
-konix_media_extract_rename.sh "${file}"
+#konix_media_extract_rename.sh "${file}"
+konix_media_avconv_rename.sh "${file}"

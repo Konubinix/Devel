ba645ec03eb3f6fd67f1571b838530bca6628cb6
Samuel Loury <konubinixweb@gmail.com>
2016-07-12 20:44:23 +0200

    Make sure to avoid going through some directories

--- bin/konix_videos_reencode.sh
+++ bin/konix_videos_reencode.sh
@@ -3,7 +3,14 @@
 set -eu
 # i should not run this in parallel since one avconv process is already taking
 # advantage of all my cpus
-find \( \
+find \
+	-name "original_videos" \
+	-prune \
+	-o \
+	-name ".git" \
+	-prune \
+	-o \
+	\( \
     -iname '*.mov' \
            -o -iname "*.mp4" \
            -o -iname "*.avi" \

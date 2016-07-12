de2daef8ff2788dbe48efb7351005f85b5e8b5dc
Samuel Loury <konubinixweb@gmail.com>
2016-07-12 20:44:23 +0200

    Make sure to avoid going through some directories

--- bin/konix_videos_rename.sh
+++ bin/konix_videos_rename.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 set -eu
-find -\( \
+find \
+	-name "original_videos" \
+	-prune \
+	-o \
+	-name ".git" \
+	-prune \
+	-o \
+	-\( \
     -iname '*.mov' \
            -o -iname "*.mp4" \
            -o -iname "*.avi" \
            -\) \
-     -exec "konix_video_rename.sh" "{}" ";"
+    -exec "konix_video_rename.sh" "{}" ";"

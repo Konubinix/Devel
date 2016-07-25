d536a6018787e77f2442c35ee378909b7fe6a172
Samuel Loury <konubinixweb@gmail.com>
2016-07-25 11:03:24 +0200

    Use the absolute path instead of the realpath

--- bin/konix_image_rename.sh
+++ bin/konix_image_rename.sh
@@ -3,8 +3,8 @@
 set -eu
 file="$1"
 NEW_NAME="`konix_exif_new_name.py "${file}"`"
-NEW_NAME_CP="$(readlink -f "${NEW_NAME}")"
-FILE_CP="$(readlink -f "${file}")"
+NEW_NAME_CP="$(konix_absolute_path.py "${NEW_NAME}")"
+FILE_CP="$(konix_absolute_path.py "${file}")"
 if [ "${NEW_NAME_CP}" != "${FILE_CP}" ]
 then
     echo "'${file}' -> '${NEW_NAME}'"

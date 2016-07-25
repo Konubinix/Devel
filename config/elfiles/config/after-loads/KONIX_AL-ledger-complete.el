d5c8fd8376fd3db29f3936ba5d34f25434a05d95
Samuel Loury <konubinixweb@gmail.com>
2016-07-25 11:03:57 +0200

    Rename the ledger after load to match a ledger file

--- config/elfiles/config/after-loads/KONIX_AL-ledger-complete.el
+++ config/elfiles/config/after-loads/KONIX_AL-ledger-complete.el
@@ -1,29 +1,3 @@
-;;; KONIX_AL-ledger.el ---                           -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2016  konubinix
-
-;; Author: konubinix <konubinixweb@gmail.com>
-;; Keywords:
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;;
-
-;;; Code:
-
 (defvar konix/ledger-accounts nil "List of all ledger accounts")
 
 (defun konix/ledger-accounts (&optional recompute)

3583722791fd448f9dbd7eee0d69997a1e40a1a1
Samuel Loury <konubinixweb@gmail.com>
2016-08-17 06:20:50 +0000

    Don't add the file:// scheme is case of relative paths.

--- bin/konix_munpack.py
+++ bin/konix_munpack.py
@@ -91,7 +91,7 @@ def munpack(message, directory, rel_path=False):
             for cid, name in CID_TO_NAME.items():
                 text = text.replace(
                     "cid:{}".format(cid),
-                    "file://{}".format(name)
+                    "{}".format(name) if rel_path else "file://{}".format(name)
                 )
             content = text.encode(charset)
         logger.debug("Writing in file {}".format(file_name))

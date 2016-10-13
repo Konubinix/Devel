24b748ace358dd9ac80127a34667e1b1ace4e6d1
Samuel Loury <konubinixweb@gmail.com>
2016-10-13 08:41:09 +0200

    Bye uzbl, long life qutebrowser

--- bin/konix_uzbl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source "$HOME/init_bin/konix_init_lib.sh"
-import_env
-
-exec uzbl "$@"
--- bin/konix_web_history.sh
+++ bin/konix_web_history.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# use the one given as parameter and fallback to the uzbl history file
 HISTORY_FILE="${1:-${XDG_DATA_HOME}/qutebrowser/history}"
 HISTORY_=`tac "${HISTORY_FILE}"|konix_dmenu_custom.sh|cut -f2 -d' '`
 if [ -n "$HISTORY_" ]

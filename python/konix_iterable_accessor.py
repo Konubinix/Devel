98cea0e0d8bb05086d9ec7b73cd07b874b3171c3
Samuel Loury <konubinixweb@gmail.com>
2016-10-13 08:32:14 +0200

    l instead of _l. Till I find a way to pretty print this

--- python/konix_iterable_accessor.py
+++ python/konix_iterable_accessor.py
@@ -19,34 +19,34 @@ class IterableCaller(object):
 
 class IterableAccessorProxy(list):
     def __init__(self, list_):
-        self._l = list_
+        self.l = list_
         try:
             self._first = list_[0]
         except IndexError:
             self._first = None
 
     def __dir__(self):
-        return dir(self._first) + ["_l"]
+        return dir(self._first) + ["l"]
 
     def __repr__(self):
-        return repr(self._l)
+        return repr(self.l)
 
     def __iter__(self):
-        return iter(self._l)
+        return iter(self.l)
 
     def __getattribute__(self, name):
-        if name in ["_l", "_first", "__dir__", "__class__"]:
+        if name in ["l", "_first", "__dir__", "__class__"]:
             return super(IterableAccessorProxy, self).__getattribute__(name)
         if hasattr(getattr(self._first, name), "__call__"):
             # callable, need to call it on each object
             return functools.wraps(getattr(self._first, name))(
                 IterableCaller(
-                    self._l,
+                    self.l,
                     name,
                 )
             )
         else: # simple attribute
             return IterableAccessorProxy([
                 getattr(object, name)
-                for object in self._l
+                for object in self.l
             ])

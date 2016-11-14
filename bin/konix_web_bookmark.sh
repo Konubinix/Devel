0fb47838e2ab7596a8141ee593e6cfabddfa9592
Samuel Loury <konubinixweb@gmail.com>
2016-11-14 17:13:35 +0100

    Don't depend anymore in uzbl_bookmark_file

--- bin/konix_web_bookmark.sh
+++ bin/konix_web_bookmark.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-BOOKMARK=`konix_dmenu_custom.sh < "${UZBL_BOOKMARKS_FILE}"|cut -f1 -d' '`
+set -ue
+
+BOOKMARK=`konix_dmenu_custom.sh < "${BOOKMARKS_FILE}"|cut -f1 -d' '`
 # if the scheme indicates so, the BOOKMARK must be evaled
 if echo "$BOOKMARK" | grep -q -e "^eval://"
 then

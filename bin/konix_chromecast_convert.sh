270d95ece0acdc11f4b90878f90312634f24a294
Samuel Loury <konubinixweb@gmail.com>
2017-02-19 14:11:44 +0100

    Add a few scripts to ease sending videos to chromecast

---
+++ bin/konix_chromecast_convert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eux
+
+INPUT="${1}"
+OUTPUT="${INPUT}.webm"
+shift
+
+avconv -i "${INPUT}" \
+	   -c:v libvpx -c:a vorbis -ac 2 -f webm -b:v 2000k -c:s webvtt -strict -2 \
+	   "${@}"
+	   "${OUTPUT}"
---
+++ bin/konix_chromecast_stream.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+stream2chromecast.py "$@"

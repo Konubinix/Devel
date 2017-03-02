0f465a9581480195e0e0a5a6ca08233278e4e9eb
Samuel Loury <konubinixweb@gmail.com>
2017-03-02 19:08:48 +0100

    Implement a few operators

--- python/konix_iterable_accessor.py
+++ python/konix_iterable_accessor.py
@@ -5,17 +5,33 @@ from padme import proxy
 import functools
 from pprint import pformat
 
+
 class IterableCaller(object):
     def __init__(self, list_, name):
         self.list_ = list_
         self.name = name
-
 
     def __call__(self, *args, **kwargs):
         return IterableAccessorProxy([
             getattr(object, self.name)(*args, **kwargs)
             for object in self.list_
         ])
+
+
+def powerzip(it1, it2):
+    for e1, e2 in zip(it1, it2):
+        res = []
+        if isinstance(e1, tuple) or isinstance(e1, list):
+            for elem in e1:
+                res.append(elem)
+        else:
+            res.append(e1)
+        if isinstance(e2, tuple) or isinstance(e2, list):
+            for elem in e2:
+                res.append(elem)
+        else:
+            res.append(e2)
+        yield res
 
 
 class IterableAccessorProxy(list):
@@ -40,8 +56,26 @@ class IterableAccessorProxy(list):
     def __iter__(self):
         return iter(self.l)
 
+    def __sub__(self, other):
+        return IterableAccessorProxy([b - a for a, b in zip(self.l, other.l)])
+
+    def __gt__(self, other):
+        return IterableAccessorProxy([a > b for a, b in zip(self.l, other.l)])
+
+    def __lt__(self, other):
+        return IterableAccessorProxy([a < b for a, b in zip(self.l, other.l)])
+
+    def __add__(self, other):
+        return IterableAccessorProxy([a + b for a, b in zip(self.l, other.l)])
+
+    def __and__(self, other):
+        return IterableAccessorProxy(list(powerzip(self.l, other.l)))
+
     def __getattribute__(self, name):
-        if name in ["l", "__repr__", "df", "_first", "__dir__", "__class__"]:
+        if name in [
+                "l", "__repr__", "df", "_first", "__dir__", "__class__",
+                "__sub__", "__add__", "__gt__", "__lt__", "__and__",
+        ]:
             return super(IterableAccessorProxy, self).__getattribute__(name)
         if hasattr(getattr(self._first, name), "__call__"):
             # callable, need to call it on each object

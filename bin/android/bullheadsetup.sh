0b9d5510adcd857aa0e46ec7504e5c50b34a48c8
Samuel Loury <konubinixweb@gmail.com>
2017-03-12 23:24:11 +0100

    Allow to build several architectures for bullhead

--- bin/android/bullheadsetup.sh
+++ bin/android/bullheadsetup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 set -eux
-sudo aptitude install debootstrap
+#sudo aptitude install debootstrap
+arch=${1:-arm64}
 
-sudo debootstrap --arch=armhf --variant=minbase --foreign stable debian http://httpredir.debian.org/debian
-sudo cp $(which android_setup_perso.sh) "debian/sbin/"
-sudo cp $(which setup_debian_second_stage.sh) "debian/sbin/"
-sudo tar cf debian.tar debian
-adb push debian.tar /sdcard/
-adb shell su -c 'rm -rf /data/debian* && mv /sdcard/debian.tar /data && cd /data && tar xf debian.tar && rm debian.tar'
-adb shell su -c 'mount -oremount,exec,dev /data'
+# testing won't work because of resolv.conf problems
+sudo debootstrap --arch=${arch} --variant=minbase --foreign stable debian-${arch} http://httpredir.debian.org/debian
+sudo cp $(which android_setup_perso.sh) "debian-${arch}/sbin/"
+sudo cp $(which setup_debian_second_stage.sh) "debian-${arch}/sbin/"
+sudo tar cf debian-${arch}.tar debian-${arch}
+adb push debian-${arch}.tar /sdcard/
+adb shell su -c 'rm -rf /data/debian* && cp /sdcard/debian-${arch}.tar /data/debian-${arch}.tar && cd /data && tar xf debian-${arch}.tar && rm debian-${arch}.tar && mv debian-${arch} debian'
 echo "run su -c deb.sh, then setup_debian_second_stage.sh, then reboot the phone"
 adb shell

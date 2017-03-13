49881653f58b574cda4e5277080005543d1a9304
Samuel Loury <konubinixweb@gmail.com>
2017-03-13 22:16:09 +0100

    Create a stub kbd_mode to prevent android rebooting

--- bin/android/bullheadsetup.sh
+++ bin/android/bullheadsetup.sh
@@ -8,6 +8,11 @@ arch=${1:-arm64}
 sudo debootstrap --arch=${arch} --variant=minbase --foreign stable debian-${arch} http://httpredir.debian.org/debian
 sudo cp $(which android_setup_perso.sh) "debian-${arch}/sbin/"
 sudo cp $(which setup_debian_second_stage.sh) "debian-${arch}/sbin/"
+# for some obscure reason, kbd_mode make android reboot
+cat <<EOF > "debian-${arch}/bin/kbd_mode"
+#!/bin/bash
+exit 1
+EOF
 sudo tar cf debian-${arch}.tar debian-${arch}
      adb push debian-${arch}.tar /sdcard/
 adb shell su -c 'rm -rf /data/debian* && cp /sdcard/debian-${arch}.tar /data/debian-${arch}.tar && cd /data && tar xf debian-${arch}.tar && rm debian-${arch}.tar && mv debian-${arch} debian'

8acb56438a5725f880b966009e3cd00d4bd4c198
Samuel Loury <konubinixweb@gmail.com>
2017-05-12 21:41:07 +0200

    Fix includes

--- python/ipython_helpers_datetime.py
+++ python/ipython_helpers_datetime.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from datetime import datetime as dt, timedelta as td
+from datetime import datetime as dt, timedelta as td, timezone as tz
 
 # cf http://stackoverflow.com/questions/4563272/how-to-convert-a-python-utc-datetime-to-a-local-datetime-using-only-python-stand/13287083#13287083
 def datetime_naive_insert_local_timezone(datetime_):
-    datetime_.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
+    return datetime_.replace(tzinfo=tz.utc).astimezone(tz=None)

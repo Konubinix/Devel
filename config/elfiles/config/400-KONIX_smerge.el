0ee3703d43dead799f6ed264192da91624081e69
Samuel Loury <konubinixweb@gmail.com>
2017-05-16 08:05:25 +0200

    Add a simple customization of smerge mode

---
+++ config/elfiles/config/400-KONIX_smerge.el
@@ -0,0 +1,30 @@
+;;; 400-KONIX_smerge.el ---                          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq-default smerge-command-prefix (kbd "C-c v"))
+
+(provide '400-KONIX_smerge)
+;;; 400-KONIX_smerge.el ends here

69615792c3aada7dfb473ce0a54b7363130636a9
Samuel Loury <konubinixweb@gmail.com>
2017-05-17 08:43:53 +0200

    Add backends and config to get access to keyring from netrc and assword

---
+++ python/keyring_assword.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import json
+import os
+import subprocess
+import six
+from contextlib import contextmanager
+
+import keyring.backend
+
+
+@contextmanager
+def updated_env(**kwargs):
+    u"""Temporarily update the environment. To be used in a with statement"""
+    oldenv = dict(os.environ)
+    for k, v in six.iteritems(kwargs):
+        if v is None and k in os.environ:
+            del os.environ[k]
+        else:
+            os.environ[k] = v
+    yield
+    os.environ.clear()
+    os.environ.update(oldenv)
+
+
+class AsswordKeyring(keyring.backend.KeyringBackend):
+    priority = 1
+
+    def set_password(self, servicename, username, password):
+        raise NotImplementedError
+
+    def get_password(self, servicename, username):
+        key = "{}@{}".format(username, servicename)
+        with updated_env(ASSWORD_DUMP_PASSWORDS="1"):
+            return json.loads(
+                subprocess.check_output(
+                    [
+                        "assword",
+                        "dump",
+                        key,
+                    ]
+                ).decode("utf-8").strip()
+            )[key]["password"]
+
+    def delete_password(self, servicename, username, password):
+        raise NotImplementedError

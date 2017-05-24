1b39f8a3ddbd3a9e89b5a27c11e764325826323f
Samuel Loury <konubinixweb@gmail.com>
2017-05-24 08:36:54 +0200

    Add a file to find out whether I am here or not

--- bin/konix_away_watch.sh
+++ bin/konix_away_watch.sh
@@ -4,10 +4,12 @@ xscreensaver-command -watch | while read line
 do
     if echo "${line}" grep "^LOCK "
     then
+        rm -f "${HOME}/.here"
         emacsclient -e "(run-hooks 'konix/away-hooks)"
     fi
     if echo "${line}" grep "^UNBLANK "
     then
+        echo "1" > "${HOME}/.here"
         emacsclient -e "(run-hooks 'konix/back-hooks)"
     fi
 done

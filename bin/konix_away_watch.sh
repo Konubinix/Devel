102345c7beb2e5ec22253c0a1d365aee8a80bdc2
Samuel Loury <konubinixweb@gmail.com>
2017-05-24 08:46:04 +0200

    End the away watch script -> first functional version

--- bin/konix_away_watch.sh
+++ bin/konix_away_watch.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
 xscreensaver-command -watch | while read line
 do
-    if echo "${line}" grep "^LOCK "
+    if echo "${line}" | grep "^LOCK "
     then
         rm -f "${HOME}/.here"
-        emacsclient -e "(run-hooks 'konix/away-hooks)"
+        emacsclient -e "(run-hooks 'konix/away-hooks)" -a "false"
     fi
-    if echo "${line}" grep "^UNBLANK "
+    if echo "${line}" | grep "^UNBLANK "
     then
         echo "1" > "${HOME}/.here"
-        emacsclient -e "(run-hooks 'konix/back-hooks)"
+        emacsclient -e "(run-hooks 'konix/back-hooks)" -a "false"
     fi
 done

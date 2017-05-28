ee5fb239130ab01fe5911781ea337e77017f64f7
Samuel Loury <konubinixweb@gmail.com>
2017-05-28 21:52:18 +0200

    Use the correct quotes

--- bin/android/debinstall.sh
+++ bin/android/debinstall.sh
@@ -4,7 +4,7 @@ set -eu
 
 target="${1}"
 
-adb shell su -c "mkdir -p /data/bin"
+adb shell 'su -c "mkdir -p /data/bin"'
 adb push "${KONIX_DEVEL_DIR}/bin/android/${target}debsetup.sh" /sdcard/
 #adb push "${KONIX_DEVEL_DIR}/bin/android/${target}sdcard1_umount.sh" /sdcard/
 
@@ -13,4 +13,4 @@ adb push "${KONIX_DEVEL_DIR}/bin/android/launch_ssh.sh" /sdcard/
 adb push "${KONIX_DEVEL_DIR}/bin/android/install.sh" /sdcard/
 adb push "${KONIX_DEVEL_DIR}/bin/android/deb.sh" /sdcard/
 
-adb shell su -c "bash /sdcard/install.sh ${target}"
+adb shell "su -c 'bash /sdcard/install.sh ${target}'"

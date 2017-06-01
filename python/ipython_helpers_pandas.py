e7a6d277e6f04e4484aaaa7fd3c1df1012538c94
Samuel Loury <konubinixweb@gmail.com>
2017-06-01 09:15:29 +0200

    A helper to scrap apache logs

--- python/ipython_helpers_pandas.py
+++ python/ipython_helpers_pandas.py
@@ -40,5 +40,52 @@ def pandas_open_in_browser(df):
     os.unlink(htmlfile.name)
 pd.DataFrame.open_in_browser = pandas_open_in_browser
 
+
 def pd_stripna(input_dataframe):
     return input_dataframe.dropna(axis=0, how="all").dropna(axis=1, how="all")
+
+
+def pd_read_apache_logs(path):
+    import pytz
+    from datetime import datetime
+    def parse_str(x):
+        """
+        Returns the string delimited by two characters.
+
+        Example:
+        `>>> parse_str('[my string]')`
+        `'my string'`
+        """
+        return x[1:-1]
+
+    def parse_datetime(x):
+        '''
+        Parses datetime with timezone formatted as:
+            `[day/month/year:hour:minute:second zone]`
+
+        Example:
+            `>>> parse_datetime('13/Nov/2015:11:45:42 +0000')`
+            `datetime.datetime(2015, 11, 3, 11, 45, 4, tzinfo=<UTC>)`
+
+        Due to problems parsing the timezone (`%z`) with `datetime.strptime`, the
+        timezone will be obtained using the `pytz` library.
+        '''
+        dt = datetime.strptime(x[1:-7], '%d/%b/%Y:%H:%M:%S')
+        dt_tz = int(x[-6:-3])*60+int(x[-3:-1])
+
+        return dt.replace(tzinfo=pytz.FixedOffset(dt_tz))
+
+    return pd.read_csv(
+        path,
+        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        engine='python',
+        na_values='-',
+        header=None,
+        usecols=[0, 3, 4, 5, 6, 7, 8],
+        names=['ip', 'time', 'request', 'status', 'size', 'referer', 'user_agent'],
+        converters={'time': parse_datetime,
+                    'request': parse_str,
+                    'status': int,
+                    'size': int,
+                    'referer': parse_str,
+                    'user_agent': parse_str})

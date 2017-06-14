ea3ff5d9bba0e93a5ad35cffc5120854b7c34a96
Samuel Loury <konubinixweb@gmail.com>
2017-06-14 09:36:10 +0200

    Allow the attachment fixer to be given a manual search

--- bin/konix_notmuch_mail_fix_attachment.py
+++ bin/konix_notmuch_mail_fix_attachment.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import sys
 import notmuch
 
 db = notmuch.Database(mode=notmuch.Database.MODE.READ_WRITE)
-q=notmuch.Query(db, "tag:new and not tag:attachment")
+q=notmuch.Query(db, " ".join(sys.argv[1:]) if sys.argv[1:] else "tag:new and not tag:attachment")
 for message in q.search_messages():
     filenames=set([part.get_filename() for part in message.get_message_parts()])
     filenames.remove(None)
     if filenames != set():
-        print "Adding the tag attachment to %s" % message
-        message.add_tag("attachment")
-        message.add_tag("fixed_attachment")
+        if "attachment" not in message.get_tags():
+            print "Adding the tag attachment to %s" % message
+            message.add_tag("attachment")
+            message.add_tag("fixed_attachment")

60bb427a57f2b18ae87fad4e03f5f873b50f5998
Samuel Loury <konubinixweb@gmail.com>
2017-06-28 08:07:12 +0200

    Try to reduce the mutex zone to ease fetching mails and rss

--- bin/_konix_mail_new.sh
+++ bin/_konix_mail_new.sh
@@ -3,34 +3,9 @@
 # Some mua like AppleMail like to add attachment inline. Notmuch doesn't
 # consider them as attachment. This call will fix that.
 konix_notmuch_mail_fix_attachment.py
-
-echo "############"
-date
-echo "############"
-LOG_FILE="$(mktemp)"
-MAIL_TRAY_DAEMON_CTRL="${TMPDIR}/mail_tray_daemon_control"
-echo "?" > "$MAIL_TRAY_DAEMON_CTRL"
-echo "b" > "$MAIL_TRAY_DAEMON_CTRL"
-trap "echo > ${TMPDIR}/konix_mail_tray_stamp; echo B > '$MAIL_TRAY_DAEMON_CTRL' ; rm '$LOG_FILE'" 0
-
-# make notmuch db consistent (earlier removed mail files etc)
-notmuch new --verbose || exit 1
-
-#sync with imap server
-nice -n 10 timeout 1200  offlineimap -c "${KONIX_OFFLINEIMAPRC}" "$@" 2>&1 | tee "$LOG_FILE" || exit 1
-#get messages from pop server
-# getmail
 #finally reflect externally changed maildir flags in notmuch tags
 notmuch new || exit 1
 
-#get messages from the spool, but after notmuch new because I don't want to be
-#informed the cron jobs that did $0 ended...
-# commented the things below because I don't like the cron messages that pollute
-#my maildirs
-# mkdir -p "${HOME}/Mail/mail.spool_${LOGNAME}"
-# mb2md -s "/var/spool/mail/${LOGNAME}" -d "${HOME}/Mail/mail.spool_${LOGNAME}"
-
-# this is useless since 0.5 -> notmuchsync -d -r --all --sync-deleted-tag
 konix_mail_init_tags.sh || exit 1
 
 # perform general tag manipulation
@@ -57,4 +32,3 @@ then
     cat "$LOG_FILE" >&2
     exit 1
 fi
-konix_sendmail_flush.sh

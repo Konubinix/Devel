38bfe39dbd12faf1bc8226e8f1fa40aeade2063c
Samuel Loury <konubinixweb@gmail.com>
2017-06-29 10:43:35 +0200

    Mutex the offlineimap part though

--- bin/konix_mail_sync.sh
+++ bin/konix_mail_sync.sh
@@ -9,7 +9,7 @@ echo "?" > "$MAIL_TRAY_DAEMON_CTRL"
 echo "b" > "$MAIL_TRAY_DAEMON_CTRL"
 trap "echo > ${TMPDIR}/konix_mail_tray_stamp; echo B > '$MAIL_TRAY_DAEMON_CTRL' ; rm '$LOG_FILE'" 0
 
-nice -n 10 timeout 1200  offlineimap -c "${KONIX_OFFLINEIMAPRC}" "$@" 2>&1 | tee "$LOG_FILE" || exit 1
+konix_lock_run.sh -n -N offlineimap timeout 1200  offlineimap -c "${KONIX_OFFLINEIMAPRC}" "$@" 2>&1 | tee "$LOG_FILE" || exit 1
 konix_sendmail_flush.sh
 
 konix_mail_new.sh "$@"

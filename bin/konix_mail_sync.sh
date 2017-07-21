f14d9abf0d9a7e76968b61996f1f87007ad48b38
Samuel Loury <konubinixweb@gmail.com>
2017-07-21 08:15:10 +0200

    Move the update at the end

--- bin/konix_mail_sync.sh
+++ bin/konix_mail_sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "############"
 date
@@ -10,6 +10,7 @@ echo "b" > "$MAIL_TRAY_DAEMON_CTRL"
 trap "echo > ${TMPDIR}/konix_mail_tray_stamp; echo B > '$MAIL_TRAY_DAEMON_CTRL' ; rm '$LOG_FILE'" 0
 
 konix_lock_run.sh -n -N offlineimap timeout 1200  offlineimap -c "${KONIX_OFFLINEIMAPRC}" "$@" 2>&1 | tee "$LOG_FILE" || exit 1
+
 konix_sendmail_flush.sh
 
 konix_mail_new.sh "$@"
@@ -20,3 +21,5 @@ then
     cat "$LOG_FILE" >&2
     exit 1
 fi
+
+konix_mail_tray_daemon_update.py -d || exit 1

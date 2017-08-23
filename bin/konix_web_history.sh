b5b1c1eaf364f49507b03df4e6a6c94e178fd3ba
Samuel Loury <konubinixweb@gmail.com>
2017-08-23 11:29:37 +0200

    Update qutebrowser

--- bin/konix_web_history.sh
+++ bin/konix_web_history.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HISTORY_FILE="${1:-${XDG_DATA_HOME}/qutebrowser/history}"
-HISTORY_=`tac "${HISTORY_FILE}"|konix_dmenu_custom.sh|cut -f2 -d' '`
+HISTORY_FILE="${1:-${XDG_DATA_HOME}/qutebrowser/history.sqlite}"
+HISTORY_=`sqlite3 "${HISTORY_FILE}" "select last_atime, url from CompletionHistory order by last_atime desc;"|konix_dmenu_custom.sh|cut -f2 -d'|'`
 if [ -n "$HISTORY_" ]
 then
     "$BROWSER" "${HISTORY_}"

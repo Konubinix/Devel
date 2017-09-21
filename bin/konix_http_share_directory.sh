b7ceab112fa537d8fa143e10f75b96e145738a90
Samuel Loury <konubinixweb@gmail.com>
2017-09-21 07:59:20 +0200

    Use the new syntax of twisted

--- bin/konix_http_share_directory.sh
+++ bin/konix_http_share_directory.sh
@@ -9,4 +9,4 @@ IP="0.0.0.0"
 source "${KONIX_LIB_DIR}/konix_share_defaults.sh"
 
 #exec fsserve -v -t http "${DIR}" -a 0.0.0.0 -p 9642
-exec twistd3 -n web --path "${DIR}" --port "${PORT}"
+exec twistd3 -n web --path "${DIR}" --port "tcp:${PORT}"

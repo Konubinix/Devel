5d4f32199f354a23d3ee02769b5b75fa37c80972
Samuel Loury <konubinixweb@gmail.com>
2017-10-17 08:07:34 +0200

    Remove the extension in the subtitle filename

--- bin/konix_avconv_subtitle_extract.sh
+++ bin/konix_avconv_subtitle_extract.sh
@@ -4,5 +4,5 @@ INPUT="${1}"
 shift
 avconv -i "${INPUT}" 2>&1 |grep Subtitle
 echo "Use -map <stream_number> to choose another subtitle"
-OUTPUT="${INPUT}.webvtt"
+OUTPUT="${INPUT%*.*}.webvtt"
 avconv -i "${INPUT}" -vn -an -f webvtt -loglevel error  "${@}" "${OUTPUT}"

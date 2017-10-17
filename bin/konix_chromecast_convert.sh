8916531aebc8ae20f954690a50e4d926b8216716
Samuel Loury <konubinixweb@gmail.com>
2017-10-17 08:08:06 +0200

    Handle the subtitles in the conversion arguments

--- bin/konix_chromecast_convert.sh
+++ bin/konix_chromecast_convert.sh
@@ -1,10 +1,32 @@
 #!/bin/bash -eux
 
-INPUT="${1}"
-OUTPUT="${INPUT}.webm"
-shift
+SUB=""
+ARGS=""
+while getopts "i:sy" opt; do
+	case $opt in
+		h)
+			usage
+			exit 0
+			;;
+		i)
+			INPUT="$OPTARG"
+			;;
+		s)
+			SUB=1
+			;;
+		y)
+			ARGS="${ARGS} -y"
+			;;
+	esac
+done
+shift $((OPTIND-1))
+OUTPUT="${INPUT%*.*}.webm"
+if [ -n "${SUB}" ]
+then
+	ARGS="${ARGS} -vf subtitles=${INPUT%*.*}.webvtt"
+fi
 
 avconv -i "${INPUT}" \
 	   -c:v libvpx -c:a vorbis -ac 2 -f webm -b:v 2000k -c:s webvtt -strict -2 \
-	   "${@}" \
+	   ${ARGS} \
 	   "${OUTPUT}"

e6174bc3ceb5b81b01bd4d809feb0dcb22668fda
Samuel Loury <konubinixweb@gmail.com>
2017-11-03 21:34:18 +0100

    Add a custom pretty printer

--- bin/konix_json_pretty_print.py
+++ bin/konix_json_pretty_print.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 import json, pprint, sys
 
-pprint.pprint(json.loads(sys.stdin.read()))
+content = json.loads(open(sys.argv[1], "rb").read().decode("utf-8"))
+content = json.dumps(content, indent=4, sort_keys=True).replace(' \n', '\n') + '\n'
+open(sys.argv[1], "wb").write(content.encode("utf-8"))

f3f5c3209c6987d4d0aff8e048ef9e68e60773a8
Samuel Loury <konubinixweb@gmail.com>
2018-05-01 15:25:22 +0200

    Move the images commands to kn

--- bin/konix_images_rename.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eu
-
-find -name .git -prune -o -\( -name '*.JPG' -o -name '*.jpg' -\) -exec "konix_image_rename.sh" "{}" ";"
--- bin/konix_images_rotate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eu
-
-find -name .git -prune -o -\( -name '*.JPG' -o -name '*.jpg' -\) -print | parallel konix_image_reorient.sh '{}' ';'

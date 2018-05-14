758aee82a79fcbd98d8ee315115a986af82f44f5
Samuel Loury <konubinixweb@gmail.com>
2018-05-14 09:20:28 +0200

    Add some functions to urldecode and urlencode

--- lib/lib_bash.sh
+++ lib/lib_bash.sh
@@ -501,3 +501,27 @@ set_redis_cache ( ) {
     local cache_key="$(redis_cache_key "$@")"
     konix_redis-cli.sh --raw set "${cache_key}" "$(cat)" > /dev/null
 }
+
+urlencode() {
+    # urlencode <string>
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LC_COLLATE=$old_lc_collate
+}
+
+urldecode() {
+    # urldecode <string>
+
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\\x}"
+}

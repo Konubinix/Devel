c0db74a26b106018d2441d9bbe19c2b9aee719cc
Samuel Loury <konubinixweb@gmail.com>
2018-05-29 08:50:57 +0200

    Wip on slack

--- python/kn_commands/slack.py
+++ python/kn_commands/slack.py
@@ -1,23 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 from __future__ import print_function, absolute_import
 
-
+import time
 import os
 import re
 import datetime
 import builtins
+import json
+import pprint
+import asyncio
+import base64
+from requests.exceptions import HTTPError
 
+import asyncio_redis
+import websockets
 import click
 import slacker
 import parsedatetime
+from redis_bot import lib as botlib
 
-from click_project.decorators import group, table_fields, table_format, \
-    argument, param_config, option
+from click_project.decorators import (
+    group,
+    table_fields,
+    table_format,
+    argument,
+    param_config,
+    option,
+    flag,
+)
 from click_project.log import get_logger
-from click_project.lib import get_authenticator, TablePrinter,\
-    ParameterType, makedirs
+from click_project.lib import (
+    get_authenticator,
+    TablePrinter,
+    ParameterType,
+    makedirs
+)
 from click_project.completion import startswith
 from click_project.config import config
 from click_project.core import cache_disk
@@ -47,6 +66,7 @@ class Message():
         data["text"] = data["text"].replace("{", "{{").replace("}", "}}")
         data["text"] = re.sub("<@([^>|]+)([|][^>]+)?>", r"{\1}", data["text"])
         data["date"] = datetime.datetime.fromtimestamp(float(data["ts"]))
+        data["ts_str"] = "ts: " + data["ts"]
         data["text"] = data["text"].format(
             **{
                 id: user["name"]
@@ -120,7 +140,13 @@ class Conversation():
     def name(self):
         return self.data["name"]
 
-    def history(self, oldest=None, latest=None):
+    def invite(self, user):
+        self.endpoint.invite(self.id, user.id)
+
+    def set_topic(self, topic):
+        self.endpoint.set_topic(self.id, topic)
+
+    def history(self, oldest=None, latest=None, user=None):
         def get_history_unpage(latest, oldest):
             res = self.endpoint.history(
                 self.data["id"],
@@ -135,7 +161,9 @@ class Conversation():
             return messages
         return builtins.sorted([
             Message(message)
-            for message in get_history_unpage(oldest=oldest, latest=latest)])
+            for message in get_history_unpage(oldest=oldest, latest=latest)
+            if not user or message.get("user") == user.id
+        ])
 
     def __repr__(self):
         return "<{} - {}>".format(self.data["id"], self.name)
@@ -249,13 +277,94 @@ class User():
     def __getitem__(self, item):
         return self.data[item]
 
+    def __hash__(self):
+        return hash(self.id)
+
     def __repr__(self):
         return "<{}>".format(self.data["name"])
+
+
+async_get = asyncio.get_event_loop().run_until_complete
+
+
+class RTM():
+    def __init__(self, data):
+        self.data = data
+        url = data["url"]
+        self.ws = async_get(websockets.connect(url))
+        assert async_get(self.read())["type"] == "hello"
+        self.id = 0
+
+    async def read(self):
+        msg = await self.ws.recv()
+        result = json.loads(msg)
+        return result
+
+    async def write(self, message):
+        message = message.copy()
+        message["id"] = self.id
+        await self.ws.send(json.dumps(message))
+        self.id = self.id + 1
+
+    async def listen(
+            self,
+            conversations=None,
+            types=None,
+            user=None,
+    ):
+        while True:
+            chunk = await self.read()
+            if (
+                    (
+                        not conversations
+                        or chunk.get("channel") in [
+                            conversation.id
+                            for conversation in conversations
+                        ]
+                    )
+                    and
+                    (
+                        not types
+                        or chunk["type"] in types
+                    )
+                    and
+                    (
+                        not user
+                        or chunk.get("user") == user.id
+                    )
+            ):
+                yield chunk
 
 
 class SlackConfig():
     def __init__(self):
         self._client = None
+        self._rtm = None
+        self._users = None
+
+    @property
+    def me(self):
+        return self.rtm.data["self"]
+
+    @property
+    def rtm(self):
+        if self._rtm is None:
+            tryit = True
+            while tryit:
+                try:
+                    self._rtm = RTM(self.client.rtm.connect().body)
+                except HTTPError as e:
+                    if e.response.status_code == 429:
+                        wait_time = int(e.response.headers["Retry-After"]) * 1.2
+                        LOGGER.info(
+                            "Got a 429 response while getting the RTM."
+                            " Waiting for {}s".format(wait_time))
+                        time.sleep(wait_time)
+                    else:
+                        raise
+                else:
+                    tryit = False
+        return self._rtm
 
     @property
     def client(self):
@@ -271,10 +380,17 @@ class SlackConfig():
                 user["id"]: User(user)
                 for user in self.client.users.list().body["members"]
             }
-        return _users()
+        if self._users is None:
+            self._users = _users()
+        return self._users
 
-    def get_user(self, name):
-        return [u for u in self.users.values() if u.data["name"] == name][0]
+    def get_user(self, name_or_id):
+        return [
+            u
+            for u in self.users.values()
+            if u.data["name"] == name_or_id
+            or u.data["id"] == name_or_id
+        ][0]
 
     def get_conversation(self, id_or_name):
         return [
@@ -427,6 +543,7 @@ def conversations(fields, format, type):
         "username",
         "text",
         "type",
+        "ts_str",
     ],
     default=(
         'datetime',
@@ -438,7 +555,9 @@ def conversations(fields, format, type):
 @argument("conversation", type=ConversationType())
 @option("--oldest")
 @option("--latest")
-def history(fields, format, conversation, oldest, latest):
+@option("--user", type=UserType())
+def history(fields, format, conversation, oldest, latest,
+            user):
     if oldest:
         cal = parsedatetime.Calendar()
         oldest = cal.parseDT(oldest)[0].strftime("%s")
@@ -448,7 +567,7 @@ def history(fields, format, conversation, oldest, latest):
     with TablePrinter(fields, format) as tp:
         tp.echo_records(
             message.data for message in conversation.history(
-                oldest=oldest, latest=latest)
+                oldest=oldest, latest=latest, user=user)
         )
 
 
@@ -482,6 +601,112 @@ def record_log(output_directory):
             f.write("{}".format(now.strftime("%s")))
 
 
+@slack.group()
+def rtm():
+    ""
+
+
+@rtm.command()
+@argument("conversations", type=ConversationType(), nargs=-1)
+@option("--type", "types", multiple=True)
+@option("--user", type=UserType())
+def listen(conversations, types, user):
+    _listen = config.slack.rtm.listen
+
+    async def do():
+        async for message in _listen(
+                conversations=conversations,
+                types=types,
+                user=user,
+        ):
+            pprint.pprint(message)
+    async_get(do())
+
+
+def mess_encode(mess):
+    return botlib.mess_encode(mess)
+
+
+def mess_decode(mess):
+    return botlib.mess_decode(mess)
+
+
+@rtm.command()
+@option("--conversation", "conversations", type=ConversationType(), multiple=True)
+@option("--type", "types", multiple=True)
+@option("--redis-host", default="localhost")
+@option("--redis-port", type=int, default=6379)
+@option("--channel-to", default="bot:comm:to")
+@option("--channel-control", default="bot:comm:control")
+@option("--channel-from", default="bot:comm:from")
+def redis_bot(conversations, types, channel_control, channel_to,
+              channel_from, redis_host, redis_port):
+    _listen = config.slack.rtm.listen
+
+    redis_connection = botlib.ChanToRedis(
+        redis_host, redis_port,
+        channel_from, channel_to, channel_control
+    )
+    el = asyncio.get_event_loop()
+
+    async def slack_listen():
+        async for message in _listen(
+                conversations=conversations,
+                types=types
+        ):
+            def fixup_message(message):
+                if "channel" in message:
+                    message["mucroom"] = config.slack.get_conversation(
+                        message["channel"]
+                    ).name
+                if "user" in message:
+                    message["username"] = config.slack.get_user(
+                        message["user"]
+                    ).name
+                message["body"] = message.get("text", "")
+            try:
+                fixup_message(message)
+            except:
+                import sys
+                import ipdb
+                ipdb.post_mortem(sys.exc_info()[2])
+
+            LOGGER.debug("Got message: {}".format(message))
+            mess = mess_encode(message)
+            await redis_connection.send(mess)
+
+    async def redis_listen():
+        async for message in redis_connection.listen():
+            if "channel" not in message and "mucroom" in message:
+                message["channel"] = config.slack.get_conversation(message["mucroom"]).id
+            if "text" not in message and "body" in message:
+                message["text"] = message["body"]
+            if "type" not in message:
+                message["type"] = "message"
+            LOGGER.debug("Write message: {}".format(message))
+            await config.slack.rtm.write(message)
+
+    asyncio.ensure_future(slack_listen())
+    asyncio.ensure_future(redis_listen())
+    el.run_forever()
+
+
+@rtm.command()
+@option("--conversation", type=ConversationType())
+@argument("message")
+def send(conversation, message):
+    ""
+    async_get(
+        config.slack.rtm.write(
+            {
+                "type": "message",
+                "channel": conversation.id,
+                "text": message,
+            }
+        )
+    )
+
+
 @slack.command()
 def ipython():
     c = config.slack.client
@@ -490,3 +715,41 @@ def ipython():
     dict_ = globals()
     dict_.update(locals())
     IPython.start_ipython(argv=[], user_ns=dict_)
+
+
+@slack.command()
+@argument("name")
+def create_group(name):
+    resp = config.slack.groups.create(name).body
+    LOGGER.info("Group created with id {}".format(resp["id"]))
+
+
+@slack.command()
+@option("--user", "users", type=UserType(), multiple=True)
+@option("--all-but-user", "all_but_users", type=UserType(), multiple=True)
+@argument("conversation", type=ConversationType())
+def invite_to_conversation(users, all_but_users, conversation):
+    assert (
+        (users and not all_but_users)
+        or (all_but_users and not users)
+    )
+    users_to_invite = []
+    if users:
+        users_to_invite = list(users)
+    if all_but_users:
+        users_to_invite = set(config.slack.users.values()) - set(all_but_users)
+    for user in users_to_invite:
+        conversation.invite(user)
+
+
+@slack.command()
+@argument("conversation", type=ConversationType())
+@argument("topic")
+def set_conversation_topic(conversation, topic):
+    conversation.set_topic(topic)
+
+
+@slack.command()
+@argument("user", type=UserType(), nargs=-1)
+def create_mpim(user):
+    config.slack.client.mpim.open([u.id for u in user])

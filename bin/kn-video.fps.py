5a1d331b5c68e7a7cae958a9660d691acf33474f
Samuel Loury <konubinixweb@gmail.com>
2018-05-30 17:08:59 +0200

    typo

--- bin/kn-video.fps.py
+++ bin/kn-video.fps.py
@@ -6,7 +6,7 @@ import sys
 from moviepy.editor import VideoFileClip
 
 if __name__ == "__main__":
-    if len(sys.argv > 1) and sys.argv[1] == "--help":
+    if len(sys.argv) > 1 and sys.argv[1] == "--help":
         print("{} :TODO".format(sys.argv[0]))
         sys.exit(0)
     clip = VideoFileClip(sys.argv[1])

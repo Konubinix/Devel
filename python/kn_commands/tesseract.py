55b093c81af4fecf2a38bcc72c0a9ced161c2bf4
Samuel Loury <konubinixweb@gmail.com>
2018-08-30 14:58:59 +0200

    Add a flag to remove the input after the conversion

--- python/kn_commands/tesseract.py
+++ python/kn_commands/tesseract.py
@@ -4,19 +4,24 @@
 import os
 from shlex import split
 
-from click_project.decorators import command, argument, option
-from click_project.lib import call, temporary_file
+from click_project.decorators import command, argument, option, flag
+from click_project.lib import call, temporary_file, rm
 
 
 @command()
 @argument("input", help="Input file to convert")
 @option("--language", default="fra", help="Language of the input file")
-def tesseract(input, language):
+@flag("--remove-after",
+      default=True,
+      help="Remove the original file when everything went well")
+def tesseract(input, language, remove_after):
     """Extract the text from a pdf and create a searchable pdf"""
     ocr_base = os.path.splitext(input)[0] + "_ocr"
     with temporary_file(suffix=".tiff") as tiff:
         call(split(f"convert -density 300 '{input}' -depth 8 '{tiff.name}'"))
         call(split(f"tesseract '{tiff.name}' '{ocr_base}' -l '{language}' pdf"))
+    if remove_after:
+        rm(input)
 
 
 if __name__ == "__main__":

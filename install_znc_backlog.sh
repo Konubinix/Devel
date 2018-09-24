02b20a220a7c5899a0493cc05e2042efb6305262
Samuel Loury <konubinixweb@gmail.com>
2018-09-24 15:53:35 +0200

    Make sure to remove the tmp dir

--- install_znc_backlog.sh
+++ install_znc_backlog.sh
@@ -3,9 +3,9 @@
 sudo apt install znc-dev
 
 TMP_DIR="${HOME}/tmp/znc-backlog"
+trap "rm -rf '${TMP_DIR}'" 0
 git clone https://github.com/FruitieX/znc-backlog "${TMP_DIR}"
 pushd "${TMP_DIR}"
 make
 cp backlog.so "${KONIX_PERSO_DIR}/znc/modules/backlog.so"
 popd
-rm -rf "${TMP_DIR}"

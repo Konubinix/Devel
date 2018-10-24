9632e81a4733b246a91ea294f569b2522e88702c
Samuel Loury <konubinixweb@gmail.com>
2018-10-24 22:25:12 +0200

    Don't let a failing git annex drop prevent the real rm

--- bin/konix_git_annex_remove.sh
+++ bin/konix_git_annex_remove.sh
@@ -5,6 +5,7 @@ git annex add "$@"
 echo '##### setting the metadata to delete'
 git annex metadata --force -s ack=delete "$@"
 echo ' ####### git annex dropping the file'
+set +e
 git annex drop "$@"
 echo ' ####### removing the file'
 rm -rf "$@"

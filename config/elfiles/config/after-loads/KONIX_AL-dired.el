a19bd337a15381f0f9c8d6c0f58e11ea5e82259f
Samuel Loury <konubinixweb@gmail.com>
2018-12-27 09:32:36 +0000

    Make a few requires silently fail instead of crashing the whole setup

--- config/elfiles/config/after-loads/KONIX_AL-dired.el
+++ config/elfiles/config/after-loads/KONIX_AL-dired.el
@@ -27,7 +27,7 @@
 (require 'dired-x)
 (require 'find-dired)
 (require 'wuxch-dired-copy-paste)
-(require 'dired-sort)
+(require 'dired-sort nil t)
 (require 'diredful)
 
 (defun konix/dired-git-annex-find-restored ()

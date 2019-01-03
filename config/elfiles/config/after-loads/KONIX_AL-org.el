ac152ca9987af3c11da39b1a5270f7f2c3a5f48f
Samuel Loury <konubinixweb@gmail.com>
2019-01-03 21:45:34 +0100

    Show only the effort, since I barely took a look at it for the time being
    We'll see for implementing the pomodoro technique later

--- config/elfiles/config/after-loads/KONIX_AL-org.el
+++ config/elfiles/config/after-loads/KONIX_AL-org.el
@@ -1925,8 +1925,8 @@ items"
 (setq-default org-clock-report-include-clocking-task t)
 (setq-default org-columns-default-format "%CATEGORY %TODO %90ITEM %1PRIORITY
 %10Effort{:} %10CLOCKSUM %10CLOCKSUM_T %ORDERED %allocate %BLOCKER")
-(setq-default org-agenda-overriding-columns-format "%CATEGORY %TODO %90ITEM
- %1PRIORITY %2DONE_POMODORO(dp){+} %2POMODORO{+} %10Effort{:} %10CLOCKSUM_T
+(setq-default org-agenda-overriding-columns-format "%10Effort{:} %90ITEM
+ %1PRIORITY %10CLOCKSUM_T
 %10CLOCKSUM %ORDERED %allocate %BLOCKER")
 (setq-default org-cycle-separator-lines -1)
 (setq-default org-default-notes-file (concat org-directory "/notes.org"))

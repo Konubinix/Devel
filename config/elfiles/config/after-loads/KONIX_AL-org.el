6566ca33bf5242bc7eb63fda72f184c472513cf2
Samuel Loury <konubinixweb@gmail.com>
2019-02-13 21:47:54 +0100

    Unescape the link to make the fonction idempotent

--- config/elfiles/config/after-loads/KONIX_AL-org.el
+++ config/elfiles/config/after-loads/KONIX_AL-org.el
@@ -2107,8 +2107,8 @@ of the clocksum."
       (when (org-in-regexp org-bracket-link-regexp 1)
         (add-to-list 'org-stored-links
                      (list
-                      (org-match-string-no-properties 1)
-                      (org-match-string-no-properties 3)
+                      (org-link-unescape (org-match-string-no-properties 1))
+                      (org-link-unescape (org-match-string-no-properties 3))
                       )
                      )
         (message "Stored link : %s" (org-match-string-no-properties 3))

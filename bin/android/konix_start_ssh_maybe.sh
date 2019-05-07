829e3e6a783bce851e1d5a8d07055aa89b780df6
Samuel Loury <konubinixweb@gmail.com>
2019-05-07 06:09:55 +0000

    Make sure the log get to the appropriate location

--- bin/android/konix_start_ssh_maybe.sh
+++ bin/android/konix_start_ssh_maybe.sh
@@ -3,7 +3,7 @@
 if ! ps aux|grep sshd|grep -v grep|grep -q sshd
 then
 	echo "Starting sshd"
-	/usr/sbin/sshd -p 1235
+	/usr/sbin/sshd -p 1235 -E /var/log/sshd.log
 else
 	echo "sshd already started"
 fi

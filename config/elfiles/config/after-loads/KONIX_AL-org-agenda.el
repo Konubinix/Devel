13d2ad0a72ea7146cbe93bf38d1d503d613363d5
Samuel Loury <konubinixweb@gmail.com>
2019-07-23 22:44:09 +0200

    Add a custom made org export to ics
    For some reason, implementations using the org-element tend to
    miss some parts of the org tree.

--- config/elfiles/config/after-loads/KONIX_AL-org-agenda.el
+++ config/elfiles/config/after-loads/KONIX_AL-org-agenda.el
@@ -1649,5 +1649,251 @@ STOP is the end of the agenda."
 ;; (advice-remove 'org-agenda-clock-out #'konix/org-agenda-unhighlight)
 ;; (remove-hook 'org-agenda-finalize-hook #'konix/org-agenda-highlight-same-contexts-as-clocked-in)
 
+(defun konix/org-agenda-get-start-time ()
+  (let (
+        (time-of-day (org-get-at-bol 'time-of-day))
+        (date (org-get-at-bol 'date))
+        )
+    (unless (listp date)
+      (setq date (calendar-gregorian-from-absolute date))
+      )
+    (encode-time
+     0
+     (% time-of-day 100)
+     (/ time-of-day 100)
+     (second date)
+     (first date)
+     (third date)
+     )
+    )
+  )
+
+(defun konix/org-agenda-get-end-time nil
+  (let* (
+         (start-time (konix/org-agenda-get-start-time))
+         (duration (org-get-at-bol 'duration))
+         (start-seconds (time-to-seconds start-time))
+         (end-seconds (+ start-seconds (* duration 60)))
+         (end-time (seconds-to-time end-seconds))
+         )
+    end-time
+    )
+  )
+
+(defun konix/org-agenda-export-description nil
+  (replace-regexp-in-string "
+" "\\\\n"
+(konix/org-with-point-on-heading
+ (let (
+       (org-export-with-toc nil)
+       (org-export-babel-evaluate nil)
+       )
+   (save-excursion
+     (org-back-to-heading)
+     (org-end-of-meta-data)
+     (save-restriction
+       (narrow-to-region (point) (org-entry-end-position))
+       (org-ascii-export-as-ascii nil nil t t)
+       )
+     )
+   (with-current-buffer "*Org ASCII Export*"
+     (buffer-substring-no-properties (point-min) (point-max))
+     )
+   ;; (org-md-export-as-markdown nil t)
+   )
+ )
+)
+  )
+
+
+(defun konix/org-agenda-to-ics/format-item (type)
+  (let (
+        (res "")
+        (date (org-get-at-bol 'date))
+        (summary (konix/org-with-point-on-heading
+                  (konix/org-trim-active-timestamp
+                   (konix/org-trim-link
+                    (org-get-heading t t t t)
+                    )
+                   )
+                  ))
+        (duration (org-get-at-bol 'duration))
+        (stamp (format-time-string "%Y%m%dT%H%M%SZ"))
+        (id (konix/org-get-id))
+        (categories (mapconcat 'identity (konix/org-get-tags) ","))
+        (description (konix/org-agenda-export-description))
+        (location
+         (konix/org-with-point-on-heading
+          (or (org-entry-get (point) "LOCATION") "NA")
+          )
+         )
+        (warn-time (konix/org-with-point-on-heading
+                    (string-to-number (or (org-entry-get (point) "APPT_WARNTIME") "30"))
+                    ))
+        (deadline
+         (konix/org-with-point-on-heading (org-get-deadline-time (point))))
+        )
+    (when (string-prefix-p "deadline" (org-get-at-bol 'type))
+      (setq summary (concat "DL: " summary))
+      )
+    (when (not (null date))
+      (unless (listp date)
+        (setq date (calendar-gregorian-from-absolute date))
+        )
+      )
+    (setq res (concat res "BEGIN:V" type))
+    (when deadline
+      (setq res (concat res "
+DUE:" (format-time-string "%Y%m%dT%H%M%S" deadline))))
+    (setq res (concat res
+                      (format "
+SUMMARY:%s
+CATEGORIES:%s
+DESCRIPTION:%s
+LOCATION:%s
+DTSTAMP:%s
+BEGIN:VALARM
+ACTION:DISPLAY
+DESCRIPTION:%s
+TRIGGER:-PT%sM
+END:VALARM"
+                              summary
+                              categories
+                              description
+                              location
+                              stamp
+                              summary
+                              warn-time
+                              )
+                      )
+          )
+    (setq res
+          (concat res
+                  (if (null (org-get-at-bol 'time-of-day))
+                      (if (null date)
+                          (format "
+UID:%s_%s
+"
+                                  id
+                                  type
+                                  )
+                        (format "
+DTSTART;VALUE=DATE:%02d%02d%02d
+UID:%s_%02d%02d%02d_%s
+"
+                                (third date)
+                                (first date)
+                                (second date)
+                                id
+                                (third date)
+                                (first date)
+                                (second date)
+                                type
+                                )
+                        )
+                    (let (
+                          (start-time (konix/org-agenda-get-start-time))
+                          (end-time (konix/org-agenda-get-end-time))
+                          )
+                      (format "
+DTSTART:%s
+DTEND:%s
+UID:%s_%s_%s
+"
+                              (format-time-string "%Y%m%dT%H%M00" start-time)
+                              (format-time-string "%Y%m%dT%H%M00" end-time)
+                              id
+                              (format-time-string "%Y%m%dT%H%M00" start-time)
+                              type
+                              )
+                      )
+                    )
+                  )
+          )
+    (setq res (concat res "END:V" type "
+"))
+    )
+  )
+
+(defun konix/org-agenda-to-ics ()
+  (goto-char (point-min))
+  (save-excursion
+    (let (
+          (res "BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//konubinix//Emacs with Org mode//EN
+CALSCALE:GREGORIAN
+X-WR-CALDESC:
+X-WR-CALNAME:OrgMode
+X-WR-TIMEZONE:CEST
+")
+          date
+          day
+          time-of-day
+          duration
+          summary
+          stamp
+          id
+          categories
+          description
+          warn-time
+          )
+      (while (konix/org-agenda-next-entry-1)
+        (when (not
+               (-any
+                (lambda (tag)
+                  (member tag konix/org-icalendar-exclude-tags)
+                  )
+                (org-get-at-bol 'tags)
+                )
+               )
+          (when (or
+                 (string-prefix-p "deadline" (org-get-at-bol 'type))
+                 (string-prefix-p "tag" (org-get-at-bol 'type))
+                 )
+            (setq res (concat res (konix/org-agenda-to-ics/format-item "TODO")))
+            )
+          (when (or
+                 (string-prefix-p "deadline" (org-get-at-bol 'type))
+                 (string-prefix-p "timestamp" (org-get-at-bol 'type))
+                 )
+            (setq res (concat res (konix/org-agenda-to-ics/format-item "EVENT")))
+            )
+          )
+        )
+      (setq res (concat res "END:VCALENDAR
+          ")
+            )
+      res
+      )
+    )
+  )
+
+(defun konix/org-agenda-format-item/add-location (orig-fun extra txt &rest args)
+  (let (
+        res
+        (location (org-entry-get (point) "LOCATION"))
+        )
+    (when (and location (not (string= (string-trim location) "")))
+      ;; insert the location before the tags part
+      (let (
+            (re-with-tags "^\\(.+[^ ]\\) +:\\([^ ]+\\):$")
+            )
+        (setq txt
+              (if (string-match-p re-with-tags txt)
+                  (replace-regexp-in-string
+                   re-with-tags
+                   (format "\\1 (in %s) \\2" location)
+                   txt
+                   )
+                (format "%s (in %s)" txt location)
+                )
+              )
+        )
+      )
+    (apply orig-fun extra txt args)
+    )
+  )
+
 (provide 'KONIX_AL-org-agenda)
 ;;; KONIX_AL-org-agenda.el ends here

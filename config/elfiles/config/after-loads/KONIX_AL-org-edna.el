1b8040b06cff973c3e18111b4910432aca4e9a3b
Samuel Loury <konubinixweb@gmail.com>
2019-08-08 09:03:22 +0200

    Add an action to help use repetitive but unsure events

--- config/elfiles/config/after-loads/KONIX_AL-org-edna.el
+++ config/elfiles/config/after-loads/KONIX_AL-org-edna.el
@@ -37,5 +37,24 @@ TAGS is a single tag name."
   (org-set-property "LAST_REPEAT" (konix/org-time-stamp-now))
   )
 
+(defun org-edna-action/scheduled-lower-of! (_last_entry &rest dates)
+  "Action to take the closer occurrence of several DATES.
+
+Say you have a repetitive appointment that is always late, like a
+meeting with your boss every 6 months.  You know that the meeting
+is scheduled to take place at the beginning of January and the
+beginning of June.  Often, the January meeting takes place in
+February or March and the June one on July or August.  You can
+set scheduled-lower-of!(\"01-01\" \"06-01\") so that it will be
+scheduled in January if it is triggered in July."
+  (org-schedule nil (first
+                     (sort
+                      (mapc (lambda (date) (org-read-date nil nil date)) dates)
+                      'string<
+                      )
+                     )
+                )
+  )
+
 (provide 'KONIX_AL-org-edna)
 ;;; KONIX_AL-org-edna.el ends here

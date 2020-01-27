87dbb6dd4959ce5ad967df5ca1e7d30c6fde4221
Samuel Loury <konubinixweb@gmail.com>
2020-01-27 17:15:48 +0100

    Use the clk pulse command to manipulate the volume

---
+++ python/kn_commands/pulse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from click_project.decorators import group, argument
+
+
+@group()
+def pulse():
+    """Manipulate the sound"""
+
+
+@pulse.command()
+@argument("value", type=float, default=0.1, help="The value to use")
+def up(value):
+    """Increase the volume"""
+    import pulsectl
+    with pulsectl.Pulse('volume-increaser') as pulse:
+        for sink in pulse.sink_list():
+            pulse.volume_change_all_chans(sink, value)
+
+
+@pulse.command()
+@argument("value", type=float, default=0.1, help="The value to use")
+def down(value):
+    """Decrease the volume"""
+    import pulsectl
+    with pulsectl.Pulse('volume-increaser') as pulse:
+        for sink in pulse.sink_list():
+            pulse.volume_change_all_chans(sink, -value)
+
+
+@pulse.command()
+def show():
+    """Show the volume"""
+    import pulsectl
+    with pulsectl.Pulse('volume-increaser') as pulse:
+        for sink in pulse.sink_list():
+            print(f"{sink.description}:\n\t{sink.volume.values[0]:.2f}, {sink.volume.values[1]:.2f}")

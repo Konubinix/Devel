ac644034d2a7778d56aec30b32b9a86b720ce308
Samuel Loury <konubinixweb@gmail.com>
2020-02-25 10:11:21 +0100

    Start adding key chords

---
+++ config/elfiles/config/after-loads/KONIX_AL-key-chord.el
@@ -0,0 +1,32 @@
+;;; KONIX_AL-key-chord.el ---                        -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(key-chord-define-global "uu" 'undo)
+(key-chord-define-global "fh" 'find-file)
+
+
+(provide 'KONIX_AL-key-chord)
+;;; KONIX_AL-key-chord.el ends here

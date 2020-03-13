0eb07906865b03f00c54428330f2b2517b9473ec
Samuel Loury <konubinixweb@gmail.com>
2020-03-13 15:04:06 +0100

    Make the unused file have the correct extension

--- bin/konix_git-annex_unused_status.sh
+++ bin/konix_git-annex_unused_status.sh
@@ -40,45 +40,58 @@ done
 
 shift $((OPTIND-1))
 
-cut -f2 -d ' ' .git/annex/unused | while read sha
-do
-	ack="$(git annex metadata -g ack --key "${sha}")"
-	if [ "${ack}" == "delete" ]
-	then
-		git annex drop --force --key "${sha}"
-		continue
-	fi
-	# git log return an expression to be evaluated 3 times before giving the correct string, hence the two nested printf
-	lastname="$(printf "$(printf "$(git log -1 --format='%e %s' --name-status -S "${sha}" | tail -1|sed 's|^[A-Z]\+[ \t]\+||'|sed 's|^"\\"||'|sed 's|\\""$||')")"|sed 's|^"||'|sed 's|"$||')"
-	lastchanged="$(git annex metadata -g lastchanged --key "${sha}")"
-	echo "############### ${lastchanged}: ${lastname} : ${sha}"
-	if [ -n "${LOCATION}" ]
-	then
-		git log --format=oneline --name-status -S "${sha}" 2>&1|cat
-		echo
-	fi
-	if [ -n "${RESTORE}" ]
-	then
-	   git annex get --key "${sha}"
-	   annexobjectlocation="$(git annex contentlocation "${sha}")"
-	   dst="${lastname}"
-	   if [ "${dst}" == "" ]
-	   then
-		   dst="noname"
-	   fi
-	   while [ -e "$dst" ] || [ -L "$dst" ]
-	   do
-		   dst="$(mktemp -u "${dst}.XXXX")"
-	   done
-	   DIR="$(dirname "${dst}")"
-	   if [ "${DIR}" != "" ]
-	   then
-		   mkdir -p "$DIR"
-	   fi
-	   ln -v -r -s "${annexobjectlocation}" "${dst}"
-	   git annex metadata --set ack=restored --key="${sha}"
-	fi
-done | {
+sed -r 's/^.+(SHA256E-s([0-9]+)-.+)$/\2 \1/' .git/annex/unused \
+    | sort -rn \
+    | cut -f2 -d ' ' | {
+    coproc git annex metadata --quiet --batch --json
+    while read sha
+    do
+	    state="$(git annex metadata -g state --key "${sha}")"
+	    if [ "${state}" == "delete" ]
+	    then
+		    git annex drop --force --key "${sha}"
+		    continue
+	    fi
+	    # git log return an expression to be evaluated 3 times before giving the correct string, hence the two nested printf
+	    lastname="$(printf "$(printf "$(git log -1 --format='%e %s' --name-status -S "${sha}" | tail -1|sed 's|^[A-Z]\+[ \t]\+||'|sed 's|^"\\"||'|sed 's|\\""$||')")"|sed 's|^"||'|sed 's|"$||')"
+	    lastchanged="$(git annex metadata -g lastchanged --key "${sha}")"
+        echo
+	    echo "############### ${lastchanged}: ${lastname} : ${sha}"
+	    if [ -n "${LOCATION}" ]
+	    then
+		    git log --format=oneline --name-status -S "${sha}" 2>&1|cat
+		    echo
+	    fi
+	    if [ -n "${RESTORE}" ]
+	    then
+	        git annex get --key "${sha}"
+	        annexobjectlocation="$(git annex contentlocation "${sha}")"
+            base="$(basename "${annexobjectlocation}")"
+            ext="${base#*.}"
+	        dst="${lastname}"
+	        if [ "${dst}" == "" ]
+	        then
+		        dst="noname.${ext}"
+	        fi
+	        while [ -e "$dst" ] || [ -L "$dst" ]
+	        do
+                dir="$(dirname "${dst}")"
+                base="$(basename "${dst}")"
+                basenoext="${base%%.*}"
+                ext="${base#*.}"
+		        dst="$(mktemp -u "${dir}/${basenoext}_XXXX.${ext}")"
+	        done
+	        DIR="$(dirname "${dst}")"
+	        if [ "${DIR}" != "" ]
+	        then
+		        mkdir -p "$DIR"
+	        fi
+	        ln -v -r -s "${annexobjectlocation}" "${dst}"
+            echo "{\"key\": \"${sha}\", \"fields\": {\"state\": [\"restored\"]}}" >&"${COPROC[1]}"
+	    fi
+    done
+    exec {COPROC[1]}>&-
+} | {
 	if [ -n "${SORT}" ]
 	then
 		echo "Sorting. Might take a long time" 2>&1

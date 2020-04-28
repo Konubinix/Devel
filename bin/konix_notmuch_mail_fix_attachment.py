f90311f117cc51eb1c06c77744e263ffa69d5992
Samuel Loury <konubinixweb@gmail.com>
2020-04-28 15:01:11 +0200

    Don't bother with empty messages

--- bin/konix_notmuch_mail_fix_attachment.py
+++ bin/konix_notmuch_mail_fix_attachment.py
@@ -8,7 +8,10 @@ db = notmuch.Database(mode=notmuch.Database.MODE.READ_WRITE)
 q=notmuch.Query(db, " ".join(sys.argv[1:]) if sys.argv[1:] else "tag:new and not tag:attachment")
 for message in q.search_messages():
     filenames=set([part.get_filename() for part in message.get_message_parts()])
-    filenames.remove(None)
+    try:
+        filenames.remove(None)
+    except KeyError:
+        pass
     if filenames != set():
         if "attachment" not in message.get_tags():
             print "Adding the tag attachment to %s" % message

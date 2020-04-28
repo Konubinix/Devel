c194fca879a58e89625b5344f5a8b02be70345ff
Samuel Loury <konubinixweb@gmail.com>
2020-04-28 14:57:26 +0200

    Add the hcl mode

---
+++ config/elfiles/config/after-loads/KONIX_AL-hcl-mode.el
@@ -0,0 +1,36 @@
+;;; KONIX_AL-appt.el ---
+
+;; Copyright (C) 2012  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun konix/hcl-mode-hook nil
+  (hs-minor-mode)
+  )
+
+(add-hook 'hcl-mode-hook
+          'konix/hcl-mode-hook)
+
+
+(provide 'KONIX_AL-appt)
+;;; KONIX_AL-appt.el ends here

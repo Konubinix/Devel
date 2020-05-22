d96e53123cbded3e7b3abf71f43bf694ffa073b7
Samuel Loury <konubinixweb@gmail.com>
2020-05-22 22:15:39 +0200

    Always dump the added cid

--- bin/clk-ipfs.add.sh
+++ bin/clk-ipfs.add.sh
@@ -13,6 +13,7 @@ A:file:str:The file to add
 N:The metadata to add
 O:--rmin:int:Minimal number of allocations:-1
 O:--rmax:int:Maximal number of allocations:-1
+O:--allocs:str:The allocs
 EOF
 }
 
@@ -23,12 +24,25 @@ then
 fi
 
 cid="$(ipfs add --quieter --progress --pin=false "$(readlink -f "${CLK___FILE}")")"
+append=1
 if grep -q "^$cid" "${IPFS_INDEX}"
 then
-    echo "${cid} Already added"
-    exit 1
+    echo "${cid} Already added" >&2
+    append=0
 fi
 
-ipfs-cluster-ctl pin add --rmin "${CLK___RMIN}" --rmax "${CLK___RMAX}" "${cid}"
+args=""
+if [ "${CLK___ALLOCS}" != "" ]
+then
+    args+=" --allocs ${CLK___ALLOCS}"
+else
+    clause
+fi
 
-echo "$cid ${CLK___ARGS}" >> "${IPFS_INDEX}"
+
+ipfs-cluster-ctl pin add ${args} --rmin "${CLK___RMIN}" --rmax "${CLK___RMAX}" "${cid}" >&2
+if [ "${append}" == "1" ]
+then
+    echo "$cid ${CLK___ARGS}" >> "${IPFS_INDEX}"
+fi
+echo "${cid}"

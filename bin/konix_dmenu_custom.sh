643bcdb9fe7379199a28bae070eb2086387958c3
Samuel Loury <konubinixweb@gmail.com>
2020-05-30 09:41:27 +0200

    Change the look of dmenu

--- bin/konix_dmenu_custom.sh
+++ bin/konix_dmenu_custom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-konix_dmenu.sh -l "${DMENU_LINES:-10}" -i -nb '#303030' -nf 'khaki' -sb '#ccffaa' -sf '#303030'
+konix_dmenu.sh -b -l "${DMENU_LINES:-60}" -i -nb '#303030' -nf 'khaki' -sb '#ccffaa' -sf '#303030'

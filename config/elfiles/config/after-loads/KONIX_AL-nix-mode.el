c52872313de6433e3a7f4f6d2fb4b9d92bfd8b0e
Samuel Loury <konubinixweb@gmail.com>
2020-06-12 11:40:17 +0200

    Start playing with nix

---
+++ config/elfiles/config/after-loads/KONIX_AL-nix-mode.el
@@ -0,0 +1,36 @@
+;;; KONIX_AL-nix-mode.el ---                         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun konix/nix-mode-hook ()
+  (hs-minor-mode 1)
+  )
+
+(add-hook 'nix-mode-hook
+          'konix/nix-mode-hook)
+
+
+(provide 'KONIX_AL-nix-mode)
+;;; KONIX_AL-nix-mode.el ends here

eaeb295c25d06870191c9ed661d96093b5cfeab7
Samuel Loury <konubinixweb@gmail.com>
2020-07-30 10:45:35 +0200

    Use localhost to let the system decide whether it is ipv4 or ipv6

--- bin/konix_mpd_status.py
+++ bin/konix_mpd_status.py
@@ -8,7 +8,7 @@ import mpd
 if __name__ == "__main__":
     c = mpd.MPDClient()
     c.connect(
-        os.environ.get("MPD_HOST", "0.0.0.0"),
+        os.environ.get("MPD_HOST", "localhost"),
         os.environ.get("MPD_PORT", "6660")
     )
     print(c.status()["state"])

965dccd9eee69f7a15bfbc81ea0456963d8db21c
Samuel Loury <konubinixweb@gmail.com>
2020-09-10 22:09:03 +0200

    Remove obsolete uzbl stuff after github warned me it had some vulnerability

--- share/uzbl/scripts/highlight_select_element.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "js page string 'var konix_edited_elem = document.activeElement;var konix_edited_elem_old_background_color = konix_edited_elem.style.backgroundColor;konix_edited_elem.style.backgroundColor=\"lightBlue\";'" > "$UZBL_FIFO"

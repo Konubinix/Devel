9541bf65e3cb57819711a139a6527dae165f27a2
Samuel Loury <sam@konixwork>
2020-09-28 22:32:03 +0200

    Add a bunch of options to tackle several kind of use cases

--- python/patchwork.py
+++ python/patchwork.py
@@ -15,12 +15,15 @@ LOGGER = logging.getLogger(__name__)
 class Patchwork():
     def __init__(self, width=1920, height=1080, thumbwidth=700,
                  thumbheight=700, dist_ratio=3./4., background_color=300,
-                 rotation=15):
+                 rotation=15, shakefactorwidth=8, shakefactorheight=8, border=10):
         self.width = width
         self.height = height
+        self.border = border
         self.thumbwidth = thumbwidth
         self.thumbheight = thumbheight
         self.dist_ratio = dist_ratio
+        self.shakefactorwidth = shakefactorwidth
+        self.shakefactorheight = shakefactorheight
         self.background_color = background_color
         self.rotation = rotation
         self.coordinates = itertools.product(
@@ -30,14 +33,15 @@ class Patchwork():
                   int(self.thumbheight * self.dist_ratio)),
         )
 
-    def compute_thumbnails(self, paths):
+    def compute_thumbnails(self, paths, type=None):
         for path in paths:
-            type = mimetypes.guess_type(path)[0].split("/")[0]
+            type = type or mimetypes.guess_type(path)[0].split("/")[0]
             if type == "image":
-                i = ImageOps.expand(Image.open(path).convert("RGBA"), 10,
-                                    (255,
-                                     255,
-                                     255))
+                i = ImageOps.expand(
+                    Image.open(path).convert("RGBA"), self.border,
+                    (255,
+                     255,
+                     255))
                 i.thumbnail((self.thumbwidth, self.thumbheight))
                 yield i
             elif type == "video":
@@ -46,7 +50,8 @@ class Patchwork():
                 tt = np.arange(0, clip.duration, 1.0 / fps)
                 for t in tt:
                     array = clip.get_frame(t)
-                    i = ImageOps.expand(Image.fromarray(array).convert("RGBA"), 10,
+                    i = ImageOps.expand(
+                        Image.fromarray(array).convert("RGBA"), self.border,
                                         (255,
                                          255,
                                          255))
@@ -59,8 +64,8 @@ class Patchwork():
                         type
                     ))
 
-    def from_files(self, imagepaths):
-        thumbnails = self.compute_thumbnails(imagepaths)
+    def from_files(self, imagepaths, type=None):
+        thumbnails = self.compute_thumbnails(imagepaths, type=type)
         return self.from_images(thumbnails)
 
     def from_images(self, images):
@@ -73,8 +78,8 @@ class Patchwork():
         )
         random.shuffle(values)
         for (x, y), i in values:
-            dx = random.randint(-self.thumbwidth // 8, self.thumbwidth // 8)
-            dy = random.randint(-self.thumbheight // 8, self.thumbheight // 8)
+            dx = random.randint(-self.thumbwidth // self.shakefactorwidth, self.thumbwidth // self.shakefactorwidth)
+            dy = random.randint(-self.thumbheight // self.shakefactorheight, self.thumbheight // self.shakefactorheight)
             nx = x + dx
             ny = y + dy
             i = i.copy()

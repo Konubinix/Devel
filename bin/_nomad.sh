05d66eadf30fd4ecb2b14589da12452d219c79ae
Samuel Loury <sam@konixwork>
2020-09-28 22:32:18 +0200

    Add a few helper in nomad scripts

---
+++ bin/_nomad.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+
+nomad_list_eligible () {
+    nomad node status -json|jq -r '.[] | select(.SchedulingEligibility == "eligible").Name'
+}
+
+nomad_list_ineligible () {
+    nomad node status -json|jq -r '.[] | select(.SchedulingEligibility == "ineligible").Name'
+}
+
+nomad_name_to_id () {
+    local name="$1"
+    nomad node status -json|jq -r ".[] | select(.Name == \"${name}\").ID"
+}

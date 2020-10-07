8ac21777a5a3e986eaec5cfd6f9150e13239e746
Samuel Loury <konubinixweb@gmail.com>
2020-10-07 18:12:16 +0200

    Use the default port

--- bin/konix_mpd_status.py
+++ bin/konix_mpd_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     c = mpd.MPDClient()
     c.connect(
         os.environ.get("MPD_HOST", "localhost"),
-        os.environ.get("MPD_PORT", "6660")
+        os.environ.get("MPD_PORT", "6600")
     )
     print(c.status()["state"])

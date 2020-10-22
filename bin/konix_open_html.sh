7693b30cf1345fcd3961b6255c5958b59e525f41
Samuel Loury <konubinixweb@gmail.com>
2020-10-22 14:19:22 +0200

    Wait a little longer before removing the file
    To allow non blocking programs reading the html file.

--- bin/konix_open_html.sh
+++ bin/konix_open_html.sh
@@ -5,7 +5,7 @@ FILE="${1}"
 if ! echo "${FILE}"|grep -q '\.html.\?$'
 then
     TMP="$(mktemp --suffix='.html' "${TMPDIR}/konix_open_html.XXXXX")"
-    rm "${TMP}"
+    echo "rm '${TMP}'" | at now + 2 minutes
     ln -sv "${FILE}" "${TMP}"
     FILE="${TMP}"
 fi

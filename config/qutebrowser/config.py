13a7d7cc1a260f1f1a253c09c016d4eb6568d3a8
Samuel Loury <konubinixweb@gmail.com>
2020-11-05 21:21:45 +0100

    Add a few customizations of the completion

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -19,6 +19,8 @@ config.set('content.javascript.enabled', True, 'chrome://*/*')
 config.set('content.javascript.enabled', True, 'qute://*/*')
 
 config.set("editor.command", ["ec", "{file}"])
+config.set("completion.cmd_history_max_items", -1)
+config.set("completion.use_best_match", True)
 
 # Bindings for normal mode
 config.bind('Uc', 'set network user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"')

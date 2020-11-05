b8a5d937817021940bf7c954e67c86b1ea2cbac2
Samuel Loury <konubinixweb@gmail.com>
2020-11-05 21:20:26 +0100

    Don't try to move http addresses

--- bin/konix_open_html.sh
+++ bin/konix_open_html.sh
@@ -2,10 +2,10 @@
 
 FILE="${1}"
 
-if ! echo "${FILE}"|grep -q '\.html.\?$'
+if ! echo "${FILE}"|grep -q '^http.://' && ! echo "${FILE}"|grep -q '\.html.\?$'
 then
     TMP="$(mktemp --suffix='.html' "${TMPDIR}/konix_open_html.XXXXX")"
-    echo "rm '${TMP}'" | at now + 2 minutes
+	echo "rm '${TMP}'" | at now + 2 minutes
     ln -sv "${FILE}" "${TMP}"
     FILE="${TMP}"
 fi

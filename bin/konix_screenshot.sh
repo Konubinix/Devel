a2fc74b60d05e06d7d4899cb3153b33b44b67d47
Samuel Loury <konubinixweb@gmail.com>
2020-11-20 11:47:15 +0100

    Make sure the cid is correctly prefixed /ipfs/

--- bin/konix_screenshot.sh
+++ bin/konix_screenshot.sh
@@ -6,6 +6,6 @@ TMP="$(mktemp -d)"
 trap "rm -rf '${TMP}'" 0
 
 import "${TMP}/out.png"
-CID="$(ipfs add --pin=false --quiet "${TMP}/out.png")"
+CID="/ipfs/$(ipfs add --pin=false --quiet "${TMP}/out.png")"
 sql -c "insert into screenshots (cid, date, state) values ('${CID}', 'now', 'todo')" >&2
-echo -n "/ipfs/${CID}"
+echo -n "${CID}"

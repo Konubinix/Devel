62f8a3dd1d9a476d24bbdbf9ce4000cc96d15444
Samuel Loury <konubinixweb@gmail.com>
2020-11-20 11:47:02 +0100

    Make sure the destination of the link does not exist yet

--- bin/konix_open_html.sh
+++ bin/konix_open_html.sh
@@ -6,6 +6,7 @@ if ! echo "${FILE}"|grep -q '^http.://' && ! echo "${FILE}"|grep -q '\.html.\?$'
 then
     TMP="$(mktemp --suffix='.html' "${TMPDIR}/konix_open_html.XXXXX")"
 	echo "rm '${TMP}'" | at now + 2 minutes
+	rm "${TMP}"
     ln -sv "${FILE}" "${TMP}"
     FILE="${TMP}"
 fi

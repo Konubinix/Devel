abb529425d821b29f63eb44e4081e3eecf5dc169
Samuel Loury <konubinixweb@gmail.com>
2021-01-04 11:57:11 +0100

    Use ffmpeg instead of avconv

--- bin/konix_avconv_creation_date.sh
+++ bin/konix_avconv_creation_date.sh
@@ -2,23 +2,23 @@
 
 file="$1"
 
-creation_date="$(avconv -i "${file}" 2>&1 \
+creation_date="$(ffmpeg -i "${file}" 2>&1 \
 	| grep -i creation_time \
 	| head -1 \
 	| sed -r 's|^.+[Cc][Rr][Ee][Aa][Tt][Ii][Oo][Nn]_[Tt][Ii][Mm][Ee]   : ([0-9][0-9][0-9][0-9]-[0-9]+-[0-9]+)[ T]([0-9]+:[0-9]+:[0-9]+).*$|\1T\2|')"
-if avconv -i "${file}" 2>&1 |grep -q -e "handler_name.*:.*Video Handler"
+if ffmpeg -i "${file}" 2>&1 |grep -q -e "handler_name.*:.*Video Handler"
 then
     # xp120 => local date
 	result="${creation_date}"
-elif avconv -i "${file}" 2>&1 |grep -q -e "com.apple.quicktime.make: Apple"
+elif ffmpeg -i "${file}" 2>&1 |grep -q -e "com.apple.quicktime.make: Apple"
 then
 	# utc date
 	result="${creation_date}+0000"
-elif avconv -i "${file}" 2>&1 |grep -q -i 'iCatch Alias Data Handler' # xp120
+elif ffmpeg -i "${file}" 2>&1 |grep -q -i 'iCatch Alias Data Handler' # xp120
 then
 	# local date
 	result="${creation_date}"
-elif avconv -i "${file}" 2>&1 |grep -q -i 'creation_time.\+Z$'
+elif ffmpeg -i "${file}" 2>&1 |grep -q -i 'creation_time.\+Z$'
 then
 	# utc date
 	result="${creation_date}+0000"

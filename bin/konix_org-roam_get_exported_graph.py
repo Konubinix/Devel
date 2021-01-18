a4c6aa68d8a1d7672d25cc3de5ce915a2768d0a1
Samuel Loury <konubinixweb@gmail.com>
2021-01-18 11:41:23 +0100

    Don't hardcode the site anymore

--- bin/konix_org-roam_get_exported_graph.py
+++ bin/konix_org-roam_get_exported_graph.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import os
 from pathlib import Path
 import json
+
 from sqlalchemy import create_engine
 from click_project.lib import cd, check_output
 import sys
@@ -13,7 +15,7 @@ def get_db(db_location, path):
         return create_engine(f'sqlite:///{path}.db')
 
 
-def main(db_location, exported_path):
+def main(db_location, exported_path, kind):
     roam = get_db(db_location, "org-roam")
     exported = [
         f'"{file}"'
@@ -24,7 +26,7 @@ def main(db_location, exported_path):
     nodes = []
 
     def name(node):
-        return node.replace('"', '').replace("/home/sam/perso/perso/roam/", "")[:-len(".org")]
+        return node.replace('"', '').replace(f"{os.environ['KONIX_PERSO_DIR']}/roam/", "")[:-len(".org")]
 
     def add_node(node):
         if node not in handled:
@@ -32,7 +34,7 @@ def main(db_location, exported_path):
             nodes.append(
                 {
                     "id": name(node),
-                    "url": f"https://konubinix.eu/braindump/posts/{name(node)}/",
+                    "url": f"/{kind}/posts/{name(node)}/",
                     "group": 1
                 }
             )
@@ -63,4 +65,5 @@ if __name__ == "__main__":
     main(
         Path(sys.argv[1]),
         Path(sys.argv[2]),
+        sys.argv[3],
     )

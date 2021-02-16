d0a2b73ba0dbf3127b83767a608332faf0077e73
Samuel Loury <konubinixweb@gmail.com>
2021-02-16 09:55:30 +0100

    Don't fetch all the messages as it takes forever

--- lib/konix_offlineimap.py
+++ lib/konix_offlineimap.py
@@ -4,8 +4,9 @@
 import os
 
 def get_folders():
-  with_trash = bool(int(os.environ.get("KONIX_OFFLINEIMAP_FOLDERS_WITH_TRASH", "0")))
-  folders = ['[Gmail]/All Mail','[Gmail]/Tous les messages',]
-  if with_trash:
-    folders += ['[Gmail]/Corbeille','[Gmail]/Bin','[Gmail]/Trash',]
-  return folders
+    with_trash = bool(int(os.environ.get("KONIX_OFFLINEIMAP_FOLDERS_WITH_TRASH", "0")))
+    # folders = ['[Gmail]/All Mail','[Gmail]/Tous les messages',]
+    folders = ['INBOX', 'Drafts', 'Sent']
+    if with_trash:
+        folders += ['[Gmail]/Corbeille','[Gmail]/Bin','[Gmail]/Trash',]
+    return folders

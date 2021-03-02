fc4ee0a5c52a7302f97fb142e7c8a75c4ded6f18
Samuel Loury <konubinixweb@gmail.com>
2021-03-02 09:13:46 +0100

    Make the expiration time more aggressive
    To have faster agendas.

--- elfiles/config/after-loads/KONIX_AL-org.el
+++ elfiles/config/after-loads/KONIX_AL-org.el
@@ -763,7 +763,7 @@ to be organized.
 
 (defun konix/org-agenda-keep-if-expired (&optional days subtree)
   (org-back-to-heading t)
-  (setq days (or days (* 30 4)))
+  (setq days (or days 30))
   (let (
         (end (org-entry-end-position))
         )

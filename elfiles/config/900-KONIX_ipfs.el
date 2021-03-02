95c48799f5ce959311aaa8a1eb7818d508f5c0f8
Samuel Loury <konubinixweb@gmail.com>
2021-03-02 09:15:11 +0100

    Advice org-display-inline-images to show ipfs images as well

--- elfiles/config/900-KONIX_ipfs.el
+++ elfiles/config/900-KONIX_ipfs.el
@@ -31,6 +31,32 @@
 (advice-add 'find-file :around #'konix/find-file/ipfs-ignore-query-parameter)
 (advice-add 'org-link-open-as-file :around #'konix/find-file/ipfs-ignore-query-parameter)
 
+(defun konix/org-display-inline-images/ipfs (&optional include-linked refresh beg end)
+  (let ((end (or end (point-max))))
+    (org-with-point-at (or beg (point-min))
+      (while (re-search-forward "^[ -]*\\(\\(file:\\(//\\)?\\)?\\(/ipfs/[a-zA-Z0-9]+\\)\\?[a-zA-Z0-9=_.]+\\(png\\|jpg\\)\\)" end t)
+        (when (file-exists-p (match-string 4))
+          (let* (
+                 (image `(image :type png :file ,(konix/org-display-inline-images/scale-down (match-string 4)) :scale 1 :width nil))
+                 (ov (make-overlay
+                      (match-beginning 1)
+                      (match-end 1)))
+                 )
+            (overlay-put ov 'display image)
+            (overlay-put ov 'face 'default)
+            (overlay-put ov 'org-image-overlay t)
+            (overlay-put ov 'modification-hooks (list 'org-display-inline-remove-overlay))
+            (when (<= 26 emacs-major-version)
+              (cl-assert (boundp 'image-map))
+              (overlay-put ov 'keymap image-map))
+            (push ov org-inline-image-overlays))
+          )
+        )
+      )
+    )
+  )
+(advice-add 'org-display-inline-images :after
+            #'konix/org-display-inline-images/ipfs)
 
 (provide '900-KONIX_ipfs)
 ;;; 900-KONIX_ipfs.el ends here

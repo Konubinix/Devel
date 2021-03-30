3677fb2e487a1e135bf43151faa8e6ce032e9198
Samuel Loury <konubinixweb@gmail.com>
2021-03-30 22:01:12 +0200

    Keep exported kinds, for I use those to export my graph

--- bin/konix_org-roam_update_links.sh
+++ bin/konix_org-roam_update_links.sh
@@ -14,5 +14,6 @@ OUTPUT_DIR="$2"
 DB="$3"
 KIND="$4"
 
+konix_org-roam_exported_kind.sh "${ROAM_DIR}" "${KIND}" | sort > "${OUTPUT_DIR}/exported_${KIND}.txt"
 konix_org-roam_exported.sh "${ROAM_DIR}" | sort > "${OUTPUT_DIR}/exported.txt"
 konix_org-roam_get_exported_links.py "${DB}" "${ROAM_DIR}" "${OUTPUT_DIR}/exported.txt" | sort > "${OUTPUT_DIR}/links.txt"

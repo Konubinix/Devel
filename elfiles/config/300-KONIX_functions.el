7a555f3533fee863871f135deef153d4aefa7662
Samuel Loury <konubinixweb@gmail.com>
2021-04-14 10:53:59 +0200

    Take the code from Sacha Chua to capture the transcript of a youtube video

--- elfiles/config/300-KONIX_functions.el
+++ elfiles/config/300-KONIX_functions.el
@@ -231,7 +231,7 @@ White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
           )
     (unless (equal res 0)
       (pop-to-buffer buffer)
-     )
+      )
     res
     )
   )
@@ -1350,3 +1350,38 @@ Version 2016-09-02"
 (defun konix/line-and-buffer-at-point nil
   (cons (konix/line-number-at-pos-widen) (current-buffer))
   )
+
+
+(defun konix/msecs-to-timestamp (msecs)
+  "Convert MSECS to string in the format HH:MM:SS.MS.
+
+  Copied the code from https://sachachua.com/blog/2021/04/org-mode-insert-youtube-video-with-separate-captions/"
+  (concat (format-seconds "%02h:%02m:%02s" (/ msecs 1000))
+          "." (format "%03d" (mod msecs 1000))))
+
+(defun konix/org-insert-youtube-video-transcript (url)
+  "Copied the code from https://sachachua.com/blog/2021/04/org-mode-insert-youtube-video-with-separate-captions/"
+  (interactive "MURL: ")
+  (require 'dom)
+  (let* ((id (if (string-match "v=\\([^&]+\\)" url) (match-string 1 url) url))
+         (temp-file (make-temp-name "org-youtube-"))
+         (temp-file-name (concat temp-file ".en.srv1"))
+         data)
+    (when (and (call-process "youtube-dl" nil nil nil
+                             "--write-sub" "--write-auto-sub"  "--no-warnings" "--sub-lang" "en" "--skip-download" "--sub-format" "srv1"
+                             "-o" temp-file
+                             (format "https://youtube.com/watch?v=%s" id))
+               (file-exists-p temp-file-name))
+      (insert
+       (mapconcat (lambda (o)
+                    (format "| [[https://youtube.com/watch?v=%s&t=%ss][%s]] | %s |\n"
+                            id
+                            (dom-attr o 'start)
+                            (konix/msecs-to-timestamp (* 1000 (string-to-number (dom-attr o 'start))))
+                            (->> (dom-text o)
+                                 (replace-regexp-in-string "[ \n]+" " ")
+                                 (replace-regexp-in-string "&#39;" "'")
+                                 (replace-regexp-in-string "&quot;" "\""))))
+                  (dom-by-tag (xml-parse-file temp-file-name) 'text)
+                  ""))
+      (delete-file temp-file-name))))

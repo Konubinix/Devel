6b1125057c3151fc7f3b24d694f6f9dbe19b9c7a
Samuel Loury <konubinixweb@gmail.com>
2021-04-14 11:00:25 +0200

    Make ORB work a little more like I want

---
+++ elfiles/config/after-loads/KONIX_AL-org-roam-bibtex.el
@@ -0,0 +1,42 @@
+;;; KONIX_AL-org-roam-bibtex.el ---                  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq-default orb-slug-source 'title)
+
+(setq-default orb-templates
+              '(
+                ("r" "ref" plain #'org-roam-capture--get-point "" :file-name "${slug}" :head "#+TITLE: ${title}
+#+LANGUAGE: fr
+#+CREATED: %U
+#+DATE: %U
+#+ROAM_KEY: ${ref}
+${title}
+" :unnarrowed t)
+                )
+              )
+
+(provide 'KONIX_AL-org-roam-bibtex)
+;;; KONIX_AL-org-roam-bibtex.el ends here

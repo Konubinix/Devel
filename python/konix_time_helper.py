3985c9b872f23644a738bc74045a0d3579ac8c2d
Samuel Loury <konubinixweb@gmail.com>
2021-04-14 10:59:16 +0200

    Cleanup a bit the time helpers

--- python/konix_time_helper.py
+++ python/konix_time_helper.py
@@ -3,47 +3,54 @@
 
 import datetime, pytz, time
 
+
 def get_local_timezone():
     return pytz.timezone(time.tzname[0])
 
+
 def get_utc_timezone():
     return pytz.timezone("UTC")
+
 
 def naive_to_local(naive, tz=None):
     tz = tz or get_local_timezone()
     return tz.localize(naive, is_dst=None)
 
+
 def naive_to_utc(naive):
     tz = get_utc_timezone()
     return tz.localize(naive, is_dst=None)
+
 
 def date_to_tz(dt, tz=None):
     tz = tz or get_local_timezone()
     return dt.astimezone(tz)
 
+
 def date_to_utc(dt):
     tz = get_utc_timezone()
     return date_to_tz(dt, tz)
 
+
 def date_to_naive(dt):
     return dt.replace(tzinfo=None)
 
+
 def naive_to_local_to_utc_to_naive(naive, tz=None):
-    local_tz = tz or get_local_timezone()
-    utc_tz = get_utc_timezone()
+    tz = tz or get_local_timezone()
     return date_to_naive(
         date_to_utc(
             naive_to_local(naive, tz)
         )
     )
 
+
 def naive_to_utc_to_local_to_naive(naive, tz=None):
-    local_tz = tz or get_local_timezone()
-    utc_tz = get_utc_timezone()
+    tz = tz or get_local_timezone()
     return date_to_naive(
         date_to_tz(
             naive_to_utc(naive),
-            tz
+            tz,
         )
     )
 

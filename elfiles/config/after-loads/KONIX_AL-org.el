19b616896963d9cc296525c0a815fbe86aba9e31
Samuel Loury <konubinixweb@gmail.com>
2021-04-28 12:03:54 +0200

    Make the thumbnails smaller so that they fit my screen

--- elfiles/config/after-loads/KONIX_AL-org.el
+++ elfiles/config/after-loads/KONIX_AL-org.el
@@ -5586,7 +5586,7 @@ https://emacs.stackexchange.com/questions/10707/in-org-mode-how-to-remove-a-link
       )
     (unless (file-exists-p path)
       (message "Scaling down %s" url)
-      (shell-command (format "convert '%s' -scale 768\\> '%s'" url path))
+      (shell-command (format "convert '%s' -scale 400\\> '%s'" url path))
       )
     path
     )

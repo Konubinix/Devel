421cb859e0980ce72575690ccf6a58ac08eae4e5
Samuel Loury <konubinixweb@gmail.com>
2021-09-09 14:07:38 +0200

    Simply provide the pinentry program to the command line instead of hacking the configuration file

--- bin/konix_gpg_agent.sh
+++ bin/konix_gpg_agent.sh
@@ -32,23 +32,10 @@ then
 	    --max-cache-ttl "${KONIX_MAX_GPG_CACHE_TTL}" \
 	    --max-cache-ttl-ssh "${KONIX_MAX_GPG_CACHE_TTL_SSH}" \
         --log-file "${KONIX_GPG_AGENT_LOG_FILE}" \
+        --pinentry-program "/bin/pinentry-gtk-2" \
         --debug-level "${KONIX_GPG_AGENT_LOG_LEVEL}"
 fi
 
 reload ( ) {
 	gpg-connect-agent reloadagent /bye
 }
-
-if grep -q '^pinentry-program' "${HOME}/.gnupg/gpg-agent.conf"
-then
-	if ! grep -q '^pinentry-program /bin/pinentry-gtk-2' "${HOME}/.gnupg/gpg-agent.conf"
-	then
-		konix_display.py "Replacing current pinentry with pinentry-gtk-2, compatible with tty + X11"
-		sed -r -i 's|^pinentry-program.+|pinentry-program /bin/pinentry-gtk-2|' "${HOME}/.gnupg/gpg-agent.conf"
-		reload
-	fi
-else
-	konix_display.py "Installing pinentry-gtk-2 as pinentry, compatible with tty + X11"
-	echo "pinentry-program /usr/bin/pinentry-gtk-2" >> "${HOME}/.gnupg/gpg-agent.conf"
-	reload
-fi

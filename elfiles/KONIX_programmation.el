18bcb2fd41b90d04180d5cab0463ed1369342e44
Samuel Loury <konubinixweb@gmail.com>
2021-09-09 16:53:46 +0200

    Default to the most common language used in programming

--- elfiles/KONIX_programmation.el
+++ elfiles/KONIX_programmation.el
@@ -172,6 +172,7 @@ They can be relative or absolute
       (flycheck-mode t)
     (message "flycheck mode not installed")
     )
+  (ispell-change-dictionary "american")
   (goto-address-prog-mode 1)
   ;; highlight parentheses in prog
   (highlight-parentheses-mode 1)

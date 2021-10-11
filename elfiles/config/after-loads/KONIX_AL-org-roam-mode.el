ecc154d4a3ef4ce1d4320bef8382fd5ce73ded80
Samuel Loury <konubinixweb@gmail.com>
2021-10-11 14:40:29 +0200

    Add a few customizations of the roam buffer

---
+++ elfiles/config/after-loads/KONIX_AL-org-roam-mode.el
@@ -0,0 +1,41 @@
+;;; KONIX_AL-org-roam-mode.el ---                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun konix/org-roam-mode-hook ()
+  (visual-line-mode)
+  )
+
+(add-hook 'org-roam-mode-hook
+          'konix/org-roam-mode-hook)
+
+
+(defun konix/org-roam-buffer-render-contents (&rest rest)
+  (magit-section-show-level-2-all)
+  )
+(advice-add #'org-roam-buffer-render-contents :after #'konix/org-roam-buffer-render-contents)
+
+(provide 'KONIX_AL-org-roam-mode)
+;;; KONIX_AL-org-roam-mode.el ends here

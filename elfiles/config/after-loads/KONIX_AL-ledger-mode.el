e44d5b94cf427e4f7fd20f213f2717aa966c4b6e
Samuel Loury <konubinixweb@gmail.com>
2021-10-25 16:10:28 +0200

    Ease opening an org-mode link in the ledger files

--- elfiles/config/after-loads/KONIX_AL-ledger-mode.el
+++ elfiles/config/after-loads/KONIX_AL-ledger-mode.el
@@ -500,6 +500,7 @@ of the transaction.
 (define-key ledger-mode-map (kbd "C-c C-s") #'konix/ledger-report-account)
 (define-key ledger-mode-map (kbd "C-c C-r") #'ledger-report)
 (define-key ledger-mode-map (kbd "C-c c") #'ledger-mode-clean-buffer)
+(define-key ledger-mode-map (kbd "C-c C-o") #'org-open-at-point-global)
 
 
 (provide 'KONIX_AL-ledger)

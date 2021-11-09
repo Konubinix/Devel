bd8cc82b808d3b7651b635ef65f9dae43b14e851
Samuel Loury <konubinixweb@gmail.com>
2021-11-09 10:11:52 +0100

    Use the newer version of ipfs cid

--- bin/konix_screenshot.sh
+++ bin/konix_screenshot.sh
@@ -8,6 +8,6 @@ trap "rm -rf '${TMP}'" 0
 out="${KONIX_PERSO_DIR}/data/inbox/$(date -Is|tr : -|tr '+' -).png"
 import "${out}"
 
-CID="/ipfs/$(ipfs add --pin=false --quiet "${out}")"
+CID="/ipfs/$(ipfs add --cid-version=1 --pin=false --quiet "${out}")"
 echo -n "${CID}"
 konix_display.py "Captured"

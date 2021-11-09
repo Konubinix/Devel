dd688d2f872728db96201d7e612df1ed562a7da7
Samuel Loury <konubinixweb@gmail.com>
2021-11-09 10:11:37 +0100

    Remove a bunch of unused programs and configurations

--- bin/_konix_conky_cpu_gauges.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat /proc/cpuinfo |grep processor|sed -r 's|processor.+: ||'|while read number
-do
-    n=$((number + 1))
-    echo  "\$alignc$color3 CPU${n} \$color: \${outlinecolor black}\${cpu cpu${n}}% / \${freq 0}MHz
-\${cpugraph cpu${n}}\${outlinecolor}"
-done

3bb245859dea5198b3ecf496c9f09af659631023
Samuel Loury <konubinixweb@gmail.com>
2021-12-13 14:37:52 +0100

    Add a hint about the package to install

--- bin/konix_mpd_status.py
+++ bin/konix_mpd_status.py
@@ -3,12 +3,11 @@
 
 import os
 
+# pip insall python-mpd2
 import mpd
 
 if __name__ == "__main__":
     c = mpd.MPDClient()
-    c.connect(
-        os.environ.get("MPD_HOST", "localhost"),
-        os.environ.get("MPD_PORT", "6600")
-    )
+    c.connect(os.environ.get("MPD_HOST", "localhost"),
+              os.environ.get("MPD_PORT", "6600"))
     print(c.status()["state"])

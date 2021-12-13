8d64dcee3ec77550b5299a047b2d2b1a5c9504c4
Samuel Loury <konubinixweb@gmail.com>
2021-12-13 14:37:34 +0100

    Style

--- bin/konix_gpg_all.py
+++ bin/konix_gpg_all.py
@@ -6,19 +6,26 @@ import konix_gpg
 import sys
 import re
 import subprocess
-import socket                   # for hostname
+import socket  # for hostname
 import logging
+
 LOGGER = logging.getLogger(__name__)
 
 # ####################################################################################################
 # Parsing arguments
 # ####################################################################################################
 import argparse
+
 parser = argparse.ArgumentParser(description="""Encrypt of decrypt recursively
 current directory but this script.""")
-parser.add_argument('-d','--decrypt', nargs='?', const="a",
+parser.add_argument('-d',
+                    '--decrypt',
+                    nargs='?',
+                    const="a",
                     help="""Use decrypt (by default, encryption is done)""")
-parser.add_argument('-v','--verbose', action='store_true',
+parser.add_argument('-v',
+                    '--verbose',
+                    action='store_true',
                     help="""Verbose mode""")
 
 args = parser.parse_args()
@@ -36,13 +43,15 @@ if args.verbose:
 # **********************************************************************
 # Need gpg to use it...
 # **********************************************************************
-gpg_path="gpg2"
+gpg_path = "gpg"
 # **********************************************************************
 # Try to start a gpg agent file or use an already launched one
 # **********************************************************************
-DEFAULT_RECIPIENT=konix_gpg.get_default_key()
+DEFAULT_RECIPIENT = konix_gpg.get_default_key()
 
 dir_walker = os.walk(".")
+
+
 def remove_hidden_files(walk_res):
     hidden_file_indices = []
     index = 0
@@ -53,38 +62,47 @@ def remove_hidden_files(walk_res):
     for to_remove_file in to_remove:
         walk_res.remove(to_remove_file)
 
+
 def process_file(abs_file, decrypt):
     if abs_file.endswith(".gpg"):
-        LOGGER.info(abs_file+" looks like an encrypted file")
+        LOGGER.info(abs_file + " looks like an encrypted file")
         if decrypt:
             if "_nd" in abs_file:
                 LOGGER.info("File does not want decryption")
                 return
-            decrypted_file_name = abs_file.replace(".gpg","")
+            decrypted_file_name = abs_file.replace(".gpg", "")
             if os.path.exists(decrypted_file_name):
-                LOGGER.info("already a decrypted file for "+abs_file+", don't touch it")
+                LOGGER.info("already a decrypted file for " + abs_file +
+                            ", don't touch it")
             else:
-                LOGGER.info("decrypt "+abs_file+" to "+decrypted_file_name)
-                retcode = subprocess.call([gpg_path,"-o",decrypted_file_name,"-d",abs_file])
+                LOGGER.info("decrypt " + abs_file + " to " +
+                            decrypted_file_name)
+                retcode = subprocess.call(
+                    [gpg_path, "-o", decrypted_file_name, "-d", abs_file])
                 if retcode == 0:
-                    LOGGER.info("removing old file "+abs_file)
+                    LOGGER.info("removing old file " + abs_file)
                     os.remove(abs_file)
         else:
-            LOGGER.info("Nothing to do for "+abs_file)
+            LOGGER.info("Nothing to do for " + abs_file)
     else:
-        LOGGER.info(abs_file+" looks like a decrypted file")
+        LOGGER.info(abs_file + " looks like a decrypted file")
         if not decrypt:
-            encrypted_file_name = abs_file+".gpg"
+            encrypted_file_name = abs_file + ".gpg"
             if os.path.exists(encrypted_file_name):
-                LOGGER.info("already a gpg file for "+abs_file+", replace it")
+                LOGGER.info("already a gpg file for " + abs_file +
+                            ", replace it")
                 os.remove(encrypted_file_name)
-            LOGGER.info("encrypt "+abs_file+" to "+encrypted_file_name)
-            retcode = subprocess.call([gpg_path,"-o",encrypted_file_name,"-r",DEFAULT_RECIPIENT,"-e",abs_file])
+            LOGGER.info("encrypt " + abs_file + " to " + encrypted_file_name)
+            retcode = subprocess.call([
+                gpg_path, "-o", encrypted_file_name, "-r", DEFAULT_RECIPIENT,
+                "-e", abs_file
+            ])
             if retcode == 0:
-                LOGGER.info("removing old file "+abs_file)
+                LOGGER.info("removing old file " + abs_file)
                 os.remove(abs_file)
         else:
-            LOGGER.info("Nothing to do for "+abs_file)
+            LOGGER.info("Nothing to do for " + abs_file)
+
 
 for root, dirs, files in dir_walker:
     remove_hidden_files(dirs)
@@ -93,5 +111,5 @@ for root, dirs, files in dir_walker:
         if os.path.split(sys.argv[0])[1] == _file:
             LOGGER.info("Not touching to me...")
             continue
-        abs_file = os.path.join(root,_file)
+        abs_file = os.path.join(root, _file)
         process_file(abs_file, args.decrypt)

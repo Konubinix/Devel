98eeb5e59e481159a40e6952af5e676531c9adae
Samuel Loury <konubinixweb@gmail.com>
2022-02-21 22:23:05 +0100

    Make fairy show everything by default

--- bin/konix_fairy.py
+++ bin/konix_fairy.py
@@ -9,30 +9,34 @@ from konix_fairy_lib import get_files
 from optparse import OptionParser
 
 if __name__ == '__main__':
-        parser = OptionParser()
-        parser.add_option("-a", "--all",
-                          dest="all",
-                          help="Print all matches",
-                          default=False,
-                          action="store_true"
-                          )
-        parser.add_option("-n", "--no-apropos",
-                          dest="no_apropos",
-                          help="Remove the automatic .* put before and after the request",
-                          default=False,
-                          action="store_true"
-                          )
-        parser.add_option("-e", "--env-variable",
-                          dest="env_variable",
-                          help="Use this environment variable instead of PATH",
-                          default="PATH"
-                          )
-        (options, args) = parser.parse_args()
-        ALL=options.all
-        NO_APROPOS=options.no_apropos
-        # set the args for apropos matching
-        if not NO_APROPOS:
-            args = [".*"+arg+".*" for arg in args]
-        files = get_files(args, ALL, options.env_variable)
-        for fil in files:
-                print(fil)
+    parser = OptionParser()
+    parser.add_option("-a",
+                      "--all",
+                      dest="all",
+                      help="Print all matches",
+                      default=False,
+                      action="store_true")
+    parser.add_option(
+        "-n",
+        "--no-apropos",
+        dest="no_apropos",
+        help="Remove the automatic .* put before and after the request",
+        default=False,
+        action="store_true")
+    parser.add_option("-e",
+                      "--env-variable",
+                      dest="env_variable",
+                      help="Use this environment variable instead of PATH",
+                      default="PATH")
+    (options, args) = parser.parse_args()
+    ALL = options.all
+    NO_APROPOS = options.no_apropos
+    # set the args for apropos matching
+    if not NO_APROPOS:
+        if args:
+            args = [".*" + arg + ".*" for arg in args]
+        else:
+            args = [".*"]
+    files = get_files(args, ALL, options.env_variable)
+    for fil in files:
+        print(fil)

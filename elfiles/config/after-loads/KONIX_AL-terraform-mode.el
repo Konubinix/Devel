d1d003ea69b8bfbd8af24bf65e4e00f8df1b0d72
Samuel Loury <konubinixweb@gmail.com>
2022-05-02 22:43:44 +0200

    Start setting up something to play with terraform

---
+++ elfiles/config/after-loads/KONIX_AL-terraform-mode.el
@@ -0,0 +1,37 @@
+;;; KONIX_AL-terraform-mode.el ---                   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun konix/terraform-mode-hook ()
+  (lsp)
+  (terraform-format-on-save-mode)
+  )
+
+(add-hook 'terraform-mode-hook
+          #'konix/terraform-mode-hook)
+
+
+(provide 'KONIX_AL-terraform-mode)
+;;; KONIX_AL-terraform-mode.el ends here

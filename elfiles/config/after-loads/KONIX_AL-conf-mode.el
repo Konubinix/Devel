9153b4c256b1831b790e98123c7b14723328af1b
Samuel Loury <konubinixweb@gmail.com>
2022-05-02 22:43:27 +0200

    Make working with conf smoother

---
+++ elfiles/config/after-loads/KONIX_AL-conf-mode.el
@@ -0,0 +1,38 @@
+;;; KONIX_AL-conf-mode.el ---                        -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun konix/conf-mode-hook ()
+  (setq indent-tabs-mode nil
+        tab-width 4
+        )
+  )
+
+(add-hook 'conf-mode-hook
+          #'konix/conf-mode-hook)
+
+
+(provide 'KONIX_AL-conf-mode)
+;;; KONIX_AL-conf-mode.el ends here

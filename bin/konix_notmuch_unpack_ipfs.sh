948af1d8ccf6574083e59307f649628b177c7e78
Samuel Loury <konubinixweb@gmail.com>
2022-05-02 22:35:38 +0200

    Make the sole html file an index

--- bin/konix_notmuch_unpack_ipfs.sh
+++ bin/konix_notmuch_unpack_ipfs.sh
@@ -7,6 +7,11 @@ file="${dir}/msg"
 notmuch show --format=raw "${ID}" > "${file}"
 trap "rm -r $dir" EXIT
 konix_munpack.py -i "${file}" -o "${dir}"
+if [ "$(ls "${dir}"|grep .html|wc -l)" = "1" ]
+then
+    html_index="$(ls "${dir}"|grep .html)"
+    mv "${dir}/${html_index}" "${dir}/index.html"
+fi
 url="${KONIX_IPFS_GATEWAY}$(ipfa "${dir}")"
 echo -n "${url}" | xclip -i
 echo "${url}"

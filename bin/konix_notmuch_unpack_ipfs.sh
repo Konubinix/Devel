a7925ee327d63af5456273fbad351e3190a28534
Samuel Loury <konubinixweb@gmail.com>
2022-06-12 22:24:39 +0200

    Put the index in a name different tha index.html
    Or else it becomes hard to get a directory listing to find other
    attached files.

--- bin/konix_notmuch_unpack_ipfs.sh
+++ bin/konix_notmuch_unpack_ipfs.sh
@@ -7,11 +7,14 @@ file="${dir}/msg"
 notmuch show --format=raw "${ID}" > "${file}"
 trap "rm -r $dir" EXIT
 konix_munpack.py -i "${file}" -o "${dir}"
+suffix=""
 if [ "$(ls "${dir}"|grep .html|wc -l)" = "1" ]
 then
     html_index="$(ls "${dir}"|grep .html)"
-    mv "${dir}/${html_index}" "${dir}/index.html"
+    mv "${dir}/${html_index}" "${dir}/content.html"
+    suffix="/content.html"
 fi
-url="${KONIX_IPFS_GATEWAY}$(ipfa "${dir}")"
-echo -n "${url}" | xclip -i
+
+url="${KONIX_IPFS_GATEWAY}$(ipfa "${dir}"|sed -r 's/^(.+)\?.+/\1/')${suffix}"
+echo -n "${url}" | konix_xclip_in_all.sh
 echo "${url}"

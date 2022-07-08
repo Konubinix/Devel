9cdbbb5d3ba5739c2edf1fe0d10b90115708af18
Samuel Loury <konubinixweb@gmail.com>
2022-07-08 13:50:35 +0200

    Organise the command to have sinks and sources manipulations

--- clk_python/pulse.py
+++ clk_python/pulse.py
@@ -4,9 +4,13 @@
 import subprocess
 from shlex import split
 
-from clk import call, check_output
-from clk.decorators import argument, flag, group
+from clk import call, check_output, run
+from clk.config import config
+from clk.decorators import argument, flag, group, option
+from clk.log import get_logger
 from clk.types import DynamicChoice
+
+LOGGER = get_logger(__name__)
 
 
 @group()
@@ -31,14 +35,46 @@ class PlayingApplicationsTypes(DynamicChoice):
                 if "application.process.binary" in sink_input.proplist
             ]
 
-    def converter(self, value):
-        binary, sink_name, client = value.split("/")
+    def convert(self, value, param, ctx):
+        if "/" in value:
+            binary, _, client = value.split("/")
+        else:
+            binary = value
+            client = None
         import pulsectl
         with pulsectl.Pulse("type") as p:
             return [
                 sink_input for sink_input in p.sink_input_list()
                 if sink_input.proplist.get("application.process.binary") ==
-                binary and str(sink_input.client) == client
+                binary and (not client or str(sink_input.client) == client)
+            ][0]
+
+
+class SinkType(DynamicChoice):
+
+    def choices(self):
+        import pulsectl
+        with pulsectl.Pulse("type") as p:
+            return [sink.name for sink in p.sink_list()]
+
+    def converter(self, value):
+        import pulsectl
+        with pulsectl.Pulse("type") as p:
+            return [sink for sink in p.sink_list() if sink.name == value][0]
+
+
+class SourceType(DynamicChoice):
+
+    def choices(self):
+        import pulsectl
+        with pulsectl.Pulse("type") as p:
+            return [source.name for source in p.source_list()]
+
+    def converter(self, value):
+        import pulsectl
+        with pulsectl.Pulse("type") as p:
+            return [
+                source for source in p.source_list() if source.name == value
             ][0]
 
 
@@ -56,10 +92,28 @@ def find_sink_by_name_safe(name):
 
 
 @pulse.command()
+@argument(
+    "application",
+    help="What application to switch",
+    type=PlayingApplicationsTypes(),
+)
+@argument(
+    "sink",
+    help="What sink to use instead",
+    type=SinkType(),
+)
+def change_sink(application, sink):
+    """Move the application to another sink"""
+    import pulsectl
+    with pulsectl.Pulse("clk") as p:
+        p.sink_input_move(application.index, sink.index)
+
+
+@pulse.command()
 @argument("application",
           help="What application to record",
           type=PlayingApplicationsTypes())
-@argument("output", help="Where to put the record")
+@option("--output", help="Where to put the record", default="output.ogg")
 @flag("--mute", help="Record the stream while muting the source")
 def record(application, output, mute):
     """Record the application sound"""
@@ -81,7 +135,13 @@ def record(application, output, mute):
 
         def clean_sink(proxy_id):
             with pulsectl.Pulse("clk") as p:
-                p.sink_input_move(application.index, sink.index)
+                try:
+                    p.sink_input_move(application.index, sink.index)
+                except pulsectl.PulseOperationFailed:
+                    LOGGER.info("Could not restore the application sink,"
+                                " most likely the application was stopped"
+                                " in the mean time."
+                                " So, no big deal.")
                 p.module_unload(proxy_id)
 
         register(clean_sink, proxy_id=proxy_id)
@@ -89,37 +149,60 @@ def record(application, output, mute):
         proxy_sink = find_sink_by_name(proxy_name)
     with pulsectl.Pulse("clk") as p:
         proxy_id = p.sink_input_move(application.index, proxy_sink.index)
-    with open(output, 'w') as f:
-        subprocess.run(["parecord", "-d", f"{proxy_sink.monitor_source_name}"],
-                       stdout=f)
+    run([
+        "pulse",
+        "record-sink",
+        f"{proxy_sink.monitor_source_name}",
+        "--output",
+        output,
+    ], )
 
 
-@pulse.command()
+@pulse.group()
+@option("--sink", type=SinkType(), multiple=True, help="The sink to setup")
+def sink(sink):
+    """Manipulate the sinks"""
+    import pulsectl
+    with pulsectl.Pulse('volume-setter') as pulse:
+        config.sink = sink or pulse.sink_list()
+
+
+@sink.command()
+@argument("value", type=float, help="The value to use")
+def _set(sink, value):
+    """Put the volume"""
+    import pulsectl
+    with pulsectl.Pulse('volume-setter') as pulse:
+        for sink in config.sink:
+            pulse.volume_set_all_chans(sink, value)
+
+
+@sink.command()
 @argument("value", type=float, default=0.1, help="The value to use")
 def up(value):
     """Increase the volume"""
     import pulsectl
     with pulsectl.Pulse('volume-increaser') as pulse:
-        for sink in pulse.sink_list():
+        for sink in config.sink:
             pulse.volume_change_all_chans(sink, value)
 
 
-@pulse.command()
+@sink.command()
 @argument("value", type=float, default=0.1, help="The value to use")
 def down(value):
     """Decrease the volume"""
     import pulsectl
     with pulsectl.Pulse('volume-decreaser') as pulse:
-        for sink in pulse.sink_list():
+        for sink in config.sink:
             pulse.volume_change_all_chans(sink, -value)
 
 
-@pulse.command()
+@sink.command()
 def show():
     """Show the volume"""
     import pulsectl
     with pulsectl.Pulse('volume-increaser') as pulse:
-        for sink in pulse.sink_list():
+        for sink in config.sink:
             if sink.mute:
                 print(f"{sink.description} muted")
             print(
@@ -127,10 +210,29 @@ def show():
             )
 
 
-@pulse.command()
+@sink.command()
 def toggle_mute():
     """Toggle mute"""
     import pulsectl
     with pulsectl.Pulse('volume-mute-toggler') as pulse:
-        for sink in pulse.sink_list():
+        for sink in config.sink:
             pulse.mute(sink, not sink.mute)
+
+
+@pulse.group()
+@option("--source", type=SourceType(), multiple=True, help="The source to use")
+def source(source):
+    """Manipulate the sources"""
+    import pulsectl
+    with pulsectl.Pulse('volume-setter') as pulse:
+        config.source = source or pulse.source_list()
+
+
+@source.command()
+@argument("value", type=float, help="The value to use")
+def __set(value):
+    """Put the volume"""
+    import pulsectl
+    with pulsectl.Pulse('volume-setter') as pulse:
+        for source in config.source:
+            pulse.volume_set_all_chans(source, value)

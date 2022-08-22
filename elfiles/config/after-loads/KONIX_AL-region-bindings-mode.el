c88d7592ec4f5f4c7e5abaa8442b976f199c8306
Samuel Loury <konubinixweb@gmail.com>
2022-08-22 15:29:01 +0200

    Ensure the region binding mode is only enabled in transient mode
    In legacy mark mode, it is strange that it is triggers while there
    is no way to remove the mark.

---
+++ elfiles/config/after-loads/KONIX_AL-region-bindings-mode.el
@@ -0,0 +1,35 @@
+;;; KONIX_AL-region-bindings-mode.el ---
+
+;; Copyright (C) 2012  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(defun konix/region-bindings-mode/activate-only-transient ()
+  (not transient-mark-mode)
+  )
+
+(add-to-list 'region-bindings-mode-disable-predicates 'konix/region-bindings-mode/activate-only-transient)
+
+(provide 'KONIX_AL-region-bindings-mode)
+;;; KONIX_AL-region-bindings-mode.el ends here

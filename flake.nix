5ed64f0d33ace29b67d897b1d4d14c93bd6861f7
sam <sam@konixwork>
2022-08-22 15:26:07 +0200

    chore: Update deps

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   # see https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html
   # github:NixOS/nixpkgs/a3a3dda3bacf61e8a39258a0ed9c924eeca8e293: A specific revision of the nixpkgs repository.
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/21.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.05";
 
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;

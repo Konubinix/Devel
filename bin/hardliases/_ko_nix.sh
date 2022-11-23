75ac2b556bd6e0701c6e78c2079f574cd0596a1d
sam <sam@konixwork>
2022-11-23 11:25:06 +0100

    feat(hardliases): First install the nix application, then call it
    This avoids the annoying stderr stuff that appear when nix run tells there is
    nothing to be done and ensure the command is run along the other tools.

---
+++ bin/hardliases/_ko_nix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit # -e
+set -o errtrace # -E
+set -o nounset # -u
+set -o pipefail
+shopt -s inherit_errexit
+
+# ctrl-c
+trap "exit 2" SIGINT
+trap "exit 3" SIGQUIT
+
+function nix_install_binary {
+    local bin_name="$1"
+    local bin="${HOME}/.nix-profile/bin/${bin_name}"
+    local derivation_name="${2:-${bin_name}}"
+    local flake="${3:-nixpkgs}"
+
+    if ! test -e "${bin}"
+    then
+        nix profile install "${flake}#${derivation_name}"
+    fi
+    echo "${bin}"
+}

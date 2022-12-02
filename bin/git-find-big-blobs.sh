7ba340a8fc7186e786a448f69aaf1d0b7ba8c33b
sam <sam@konixwork>
2022-12-02 15:44:10 +0100

    Show the biggest blobs first

--- bin/git-find-big-blobs.sh
+++ bin/git-find-big-blobs.sh
@@ -3,5 +3,4 @@
 set -eu
 # http://naleid.com/blog/2012/01/17/finding-and-purging-big-files-from-git-history
 echo "I will analyze pack files, don't forget to run git repack" >&2
-git verify-pack -v .git/objects/pack/pack-*.idx | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+$" | sort -k 3 -n
-
+git verify-pack -v .git/objects/pack/pack-*.idx | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+$" | sort -k 3 -r -n

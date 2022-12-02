0ccc19698792352dbefd08c3326204e37a230a25
sam <sam@konixwork>
2022-12-02 15:43:58 +0100

    fix: Make sure the debugging message won't mess up with the standard output

--- bin/git-find-big-blobs.sh
+++ bin/git-find-big-blobs.sh
@@ -2,5 +2,6 @@
 
 set -eu
 # http://naleid.com/blog/2012/01/17/finding-and-purging-big-files-from-git-history
-echo "I will analyze pack files, don't forget to run git repack"
+echo "I will analyze pack files, don't forget to run git repack" >&2
 git verify-pack -v .git/objects/pack/pack-*.idx | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+$" | sort -k 3 -n
+

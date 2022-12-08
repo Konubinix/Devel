51eaf774541f0f4bd4ce6cf6692c36a9465025a3
sam <sam@konixwork>
2022-12-08 17:33:31 +0100

    feat: Use slugify

--- python/konix_file_helper.py
+++ python/konix_file_helper.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+from slugify import slugify
 
-def sanitize_filename(value):
-    import romkan
-    from youtube_dl.utils import sanitize_filename
-    return sanitize_filename(romkan.to_roma(value), restricted=True)
+
+def sanitize_filename(name):
+    return slugify(
+        name,
+        lowercase=False,
+        regex_pattern="[^-a-zA-Z0-9_.]+",
+    )

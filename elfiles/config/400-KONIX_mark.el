ab3b001b939cfb86406fcde36a9b3e7a3d54c4ef
Samuel Loury <konubinixweb@gmail.com>
2023-02-16 11:59:45 +0100

    fix: Try harder having the behavior of C-SPC to stop the transient mode while keeping compatibility with set-mark-command

--- elfiles/config/400-KONIX_mark.el
+++ elfiles/config/400-KONIX_mark.el
@@ -1,18 +1,10 @@
 (transient-mark-mode 1)
 
-(defun konix/set-mark-command ()
-  (interactive)
-  (cond
-   (current-prefix-arg
-    (call-interactively 'set-mark-command)
+(defun konix/set-mark-command/deactivate-region (&rest args)
+    (when (region-active-p)
+        ;; so that set-mark-command believe we already called it once
+        (setq last-command 'set-mark-command)
+        )
     )
-   ((region-active-p)
-    (deactivate-mark)
-    )
-   (t
-    (push-mark-command nil)
-    )
-   )
-  )
 
-(substitute-key-definition 'set-mark-command 'konix/set-mark-command global-map)
+(advice-add 'set-mark-command :before 'konix/set-mark-command/deactivate-region)

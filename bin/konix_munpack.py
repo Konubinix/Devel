ce83a392150185634c9d5d70cd4d414c0541c348
sam <sam@konixwork>
2023-02-16 11:51:41 +0100

    feat: Make konix_munpack reproducible
    That way, the mails that I unpack have consistent ipfs hashes.

--- bin/konix_munpack.py
+++ bin/konix_munpack.py
@@ -5,9 +5,9 @@ import argparse
 import email
 import logging
 import os
-import tempfile
 from collections import namedtuple
 from email.header import decode_header
+from hashlib import sha256
 
 from konix_file_helper import sanitize_filename
 from konix_mail import make_part_harmless, use_relative_links
@@ -93,59 +93,79 @@ def munpack(message, directory, rel_path=False):
             if part.get_content_subtype() == "plain"
     ]) == 1:
         plain_filename = sanitized_subject
-    for part in get_parts(message):
-        text = None
-        content = part.get_payload(decode=True)
-        bcontent = part.get_payload()
-        if content is None:
-            continue
-        charset = part.get_content_charset()
-        if charset is not None and "ascii" in charset:
-            charset = "utf-8"
-        sub_type = part.get_content_subtype()
-        if charset is None and sub_type == "html":
-            charset = "utf-8"
-        main_type = part.get_content_maintype()
-        if main_type == "text":
-            try:
-                text = content.decode(charset) if charset else None
-            except UnicodeDecodeError:
-                # taken from the code of email/message.py:get_payload:
-                # This won't happen for RFC compliant messages (messages
-                # containing only ASCII code points in the unicode input).
-                # If it does happen, turn the string into bytes in a way
-                # guaranteed not to fail.
-                text = bcontent
 
+    def compute_name(part):
+        sub_type = part.get_content_subtype()
+        main_type = part.get_content_maintype()
         prefix = main_type
         extension = sub_type
+
         name = part.get_filename()
         name = name and decode_value(name)
         if name is None and sub_type == "html" and html_filename:
             name = html_filename + ".html"
         if name is None and sub_type == "plain" and plain_filename:
             name = plain_filename + ".plain"
-        if name:
-            file_name = os.path.join(directory, name)
-            file_ = open(file_name, "bw")
-        else:
-            file_ = tempfile.NamedTemporaryFile(mode="bw",
-                                                dir=directory,
-                                                suffix=".{}".format(extension),
-                                                prefix=prefix,
-                                                delete=False)
-            file_name = os.path.relpath(file_.name,
-                                        directory) if rel_path else file_.name
+        if not name:
+            suffix = sha256(part.get_payload(decode=True)).hexdigest()[:10]
+            name = f"{prefix}-{suffix}.{extension}"
+        return name
+
+    for part in get_parts(message):
+        content = part.get_payload(decode=True)
+        if content is None:
+            continue
+        name = compute_name(part)
+        file_name = os.path.join(directory, name)
+        cid = part.get("Content-Id", None)
+        if cid:
+            cid = cid.strip("<>")
+            logger.debug("Adding the association {} -> {}".format(
+                cid, file_name))
+            CID_TO_NAME[cid] = file_name
+
+    for part in get_parts(message):
+        text = None
+        content = part.get_payload(decode=True)
+        if content is None:
+            continue
+
+        bcontent = part.get_payload()
+        sub_type = part.get_content_subtype()
+        main_type = part.get_content_maintype()
+        name = compute_name(part)
+
+        charset = part.get_content_charset()
+        if charset is not None and "ascii" in charset:
+            charset = "utf-8"
+        if charset is None and main_type == "text":
+            charset = "utf-8"
+        if main_type == "text":
+            if charset:
+                try:
+                    text = content.decode(charset)
+                except UnicodeDecodeError:
+                    # taken from the code of email/message.py:get_payload:
+                    # This won't happen for RFC compliant messages (messages
+                    # containing only ASCII code points in the unicode input).
+                    # If it does happen, turn the string into bytes in a way
+                    # guaranteed not to fail.
+                    text = bcontent
+            else:
+                text = content.decode()
+
+        file_name = os.path.join(directory, name)
+        file_ = open(file_name, "bw")
+
         if text is not None:
             for cid, name in CID_TO_NAME.items():
-                text = text.replace(
-                    "cid:{}".format(cid), "{}".format(name)
-                    if rel_path else "file://{}".format(name))
+                text = text.replace("cid:{}".format(cid), f"file://{name}")
+            text = use_relative_links(text, directory)
             content = text.encode(charset)
+
         logger.debug("Writing in file {}".format(file_name))
         if sub_type == "html":
             text = make_part_harmless(text)
-            text = use_relative_links(text, directory)
             text = """<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -162,12 +182,6 @@ def munpack(message, directory, rel_path=False):
             content = text.encode("utf-8")
         file_.write(content)
         file_.close()
-        cid = part.get("Content-Id", None)
-        if cid:
-            cid = cid.strip("<>")
-            logger.debug("Adding the association {} -> {}".format(
-                cid, file_name))
-            CID_TO_NAME[cid] = file_name
 
 
 if __name__ == "__main__":

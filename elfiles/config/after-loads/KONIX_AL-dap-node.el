a94366fc5fb1f5a5b1d2f99b4d039c093477db67
sam <sam@konixwork>
2023-03-20 14:40:02 +0100

    feat: Add the node attach DAP configuration

---
+++ elfiles/config/after-loads/KONIX_AL-dap-node.el
@@ -0,0 +1,9 @@
+(dap-register-debug-template "node attach"
+    (list :type "node"
+        :request "attach"
+        :name "Attach to node Executable"
+        :mode "local"
+        :program nil
+        :args nil
+        :env nil
+        :envFile nil))

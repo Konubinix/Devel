62b30dec0643518c91871fd98ae6230acd72239a
Samuel Loury <konubinixweb@gmail.com>
2023-03-20 14:29:32 +0100

    feat: Make go mode unconditionally use dap

--- elfiles/config/after-loads/KONIX_AL-go-mode.el
+++ elfiles/config/after-loads/KONIX_AL-go-mode.el
@@ -24,59 +24,59 @@
 
 ;;; Code:
 
-(require 'lsp-mode)
+
+(require 'dap-dlv-go)
 
 (defface konix/go-font-lock-public-function-name-face '((t :background "gray21" :inherit font-lock-function-name-face))
-  "Face for public stuff."
-  :group 'konix/go-faces)
+    "Face for public stuff."
+    :group 'konix/go-faces)
 
 (defvar konix/go-font-lock-public-function-name-face 'konix/go-font-lock-public-function-name-face
-  "Face name to use for go public functions.")
+    "Face name to use for go public functions.")
 
 
 (defun konix/go-mode-hook ()
-  (hs-minor-mode 1)
-  (when (require 'lsp nil t)
-    ;; envrc sets the exec-path before this, but if we don't wait the root loop
-    ;; to make one iteration, it is not taken into account
-    (run-at-time nil nil 'lsp)
+    (hs-minor-mode 1)
+    (when (require 'lsp nil t)
+        ;; envrc sets the exec-path before this, but if we don't wait the root loop
+        ;; to make one iteration, it is not taken into account
+        (run-at-time nil nil 'lsp)
+        )
+    (lsp-headerline-breadcrumb-mode)
+    (setq-default gofmt-command "goimports") ;; see https://pkg.go.dev/golang.org/x/tools/cmd/goimports
+    (add-hook 'before-save-hook 'gofmt-before-save nil t)
+    (add-hook 'after-save-hook 'konix/go/make-executable t t)
+    (font-lock-add-keywords
+        nil
+        `(
+             (
+                 "func *\\(?:([a-zA-Z0-9 _*]+)\\)? *\\([A-Z][a-zA-Z0-9_]+\\)"
+                 .
+                 (1 konix/go-font-lock-public-function-name-face)
+                 )
+             (
+                 "type *\\([A-Z][a-zA-Z0-9_]+\\)"
+                 .
+                 (1 konix/go-font-lock-public-function-name-face)
+                 )
+             )
+        )
     )
-  (require 'dap-dlv-go)
-  (lsp-headerline-breadcrumb-mode)
-  (setq-default gofmt-command "goimports") ;; see https://pkg.go.dev/golang.org/x/tools/cmd/goimports
-  (add-hook 'before-save-hook 'gofmt-before-save nil t)
-  (add-hook 'after-save-hook 'konix/go/make-executable t t)
-  (font-lock-add-keywords
-   nil
-   `(
-     (
-      "func *\\(?:([a-zA-Z0-9 _*]+)\\)? *\\([A-Z][a-zA-Z0-9_]+\\)"
-      .
-      (1 konix/go-font-lock-public-function-name-face)
-      )
-     (
-      "type *\\([A-Z][a-zA-Z0-9_]+\\)"
-      .
-      (1 konix/go-font-lock-public-function-name-face)
-      )
-     )
-   )
-  )
 
 (add-hook 'go-mode-hook
-          'konix/go-mode-hook)
+    'konix/go-mode-hook)
 
 
 (define-key go-mode-map (kbd "<RET>") 'newline-and-indent)
 
 (defun konix/go/make-executable ()
-  (when (save-excursion
-          (goto-char (point-min))
-          (re-search-forward "//usr/bin/env go run $0 $@ ; exit" nil t)
-          )
-    (konix/make-executable)
+    (when (save-excursion
+              (goto-char (point-min))
+              (re-search-forward "//usr/bin/env go run $0 $@ ; exit" nil t)
+              )
+        (konix/make-executable)
+        )
     )
-  )
 
 (provide 'KONIX_AL-go-mode)
 ;;; KONIX_AL-go-mode.el ends here

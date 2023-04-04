0c3c10fadadb96886f883a1806528313716c31c2
Samuel Loury <konubinixweb@gmail.com>
2023-04-04 14:07:19 +0200

    feat: Add a function that decode some quoted printable content for real

--- elfiles/config/300-KONIX_functions.el
+++ elfiles/config/300-KONIX_functions.el
@@ -36,36 +36,36 @@
 (defun konix/decorate-buffer (regexp keymap face &optional match-number)
   """On all strings matching the rexexp, add the keymap and the face"""
   (font-lock-add-keywords nil
-						  `(
-							(,regexp (if match-number match-number 0) ,face)
-							)
-						  )
+                                                  `(
+                                                        (,regexp (if match-number match-number 0) ,face)
+                                                        )
+                                                  )
   (save-excursion
-	(goto-char 0)
-	(while (re-search-forward regexp nil t)
-	  (set-text-properties
-	   (match-beginning (if match-number match-number 0))
-	   (match-end (if match-number match-number 0))
-	   `(konix-matched-string
-		 ,(match-string (if match-number match-number 0))
-		 keymap ,keymap
-		 face ,face
-		 custom_elem t
-		 )
-	   )
-	  )
-	)
+        (goto-char 0)
+        (while (re-search-forward regexp nil t)
+          (set-text-properties
+           (match-beginning (if match-number match-number 0))
+           (match-end (if match-number match-number 0))
+           `(konix-matched-string
+                 ,(match-string (if match-number match-number 0))
+                 keymap ,keymap
+                 face ,face
+                 custom_elem t
+                 )
+           )
+          )
+        )
   )
 
 (defun konix/confirm (msg)
   "Demande confirmation."
   (let (confirm)
-	(setq confirm (read-char (concat "Sur de "msg" (o/n) ? ") "n"))
-	(if (equal confirm 111) ;  111 = ascii("o")
-		t
-	  nil
-	  )
-	)
+        (setq confirm (read-char (concat "Sur de "msg" (o/n) ? ") "n"))
+        (if (equal confirm 111) ;  111 = ascii("o")
+                t
+          nil
+          )
+        )
   )
 
 (defun konix/goto-line-prog (line)
@@ -76,3 +76,25 @@ According to the documentation of 'goto-line.
 LINE is given to 'forward-line."
   (goto-char (point-min))
   (forward-line (1- line)))
+
+(defun konix/quoted-printable/decode-region ()
+  (interactive)
+  (unless (region-active-p)
+    (user-error "Select some text to decode")
+    )
+  (let (
+        (content (buffer-substring-no-properties (region-beginning)
+                                                 (region-end)))
+        result
+        )
+    (when (string-match-p "'''" content)
+      (user-error "I cannot deal with text containing ''' for now")
+      )
+    (with-temp-buffer
+      (call-process "python" nil t nil "-c" (format "import sys; import quopri; sys.stdout.write(quopri.decodestring('''%s''').decode())" content))
+      (setq result (buffer-substring-no-properties (point-min) (point-max)))
+      )
+    (delete-region (region-beginning) (region-end))
+    (insert result)
+    )
+  )

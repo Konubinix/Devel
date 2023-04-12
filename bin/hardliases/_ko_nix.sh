f9ec5c6eba3f739c581089c58a53da37e621338d
sam <sam@konixwork>
2023-04-12 15:25:08 +0200

    fix: Make the check only when the tool is not already available

--- bin/hardliases/_ko_nix.sh
+++ bin/hardliases/_ko_nix.sh
@@ -20,14 +20,13 @@ function nix_install_binary {
     local derivation_name="${2:-${bin_name}}"
     local flake="${3:-nixpkgs}"
 
-    if ! command -v nix > /dev/null
-    then
-        warn "nix not installed, won't be able to run ${bin_name}"
-        exit 1
-    fi
-
     if ! test -e "${bin}"
     then
+        if ! command -v nix > /dev/null
+        then
+            warn "nix not installed, won't be able to run ${bin_name}"
+            exit 1
+        fi
         nix flake update "${flake}"
         local path="${flake}#${derivation_name}"
         konix_display.py "Installing ${path} to use ${bin}"

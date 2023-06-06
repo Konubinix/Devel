0a2bddc75242c1c1c2c61defb24da7ef4e0572ed
sam <sam@konixwork>
2023-06-06 17:08:19 +0200

    chore: Remove unused imports

--- bin/konix_org-roam_get_exported_links.py
+++ bin/konix_org-roam_get_exported_links.py
@@ -5,8 +5,6 @@ import sqlite3
 import sys
 from pathlib import Path
 
-from clk.lib import cd, check_output
-
 
 def main(db_location, roam_dir, exported_path):
     roam = sqlite3.connect(db_location)
@@ -14,7 +12,7 @@ def main(db_location, roam_dir, exported_path):
         f'"{file}"' for file in exported_path.read_text().strip().splitlines()
     ]
     for src, dest in roam.execute(
-            "select srcnode.file, dstnode.file from links join nodes as srcnode on links.source = srcnode.id join nodes as dstnode on links.dest = dstnode.id"
+        "select srcnode.file, dstnode.file from links join nodes as srcnode on links.source = srcnode.id join nodes as dstnode on links.dest = dstnode.id"
     ):
         if src in exported and dest in exported:
             print(f"{src}|{dest}")

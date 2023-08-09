89f535cea56a46c361d85c379c2d90b77b30bb5d
Samuel Loury <konubinixweb@gmail.com>
2023-08-09 11:39:58 +0200

    fix: reset the mark when C-SPC and the region is active, like I'm used to

--- elfiles/config/400-KONIX_mark.el
+++ elfiles/config/400-KONIX_mark.el
@@ -1,10 +1,17 @@
 (transient-mark-mode 1)
 
-(defun konix/set-mark-command/deactivate-region (&rest args)
-    (when (region-active-p)
-        ;; so that set-mark-command believe we already called it once
-        (setq last-command 'set-mark-command)
+(defun konix/set-mark-command/deactivate-region (orig &rest args)
+  (if (region-active-p)
+      ;; so that set-mark-command believe we already called it once
+      (progn
+        (deactivate-mark)
+        (message "Deactivated mark instead of running set-mark-command")
+        ;; also, reseting the last command to avoid getting back to the same
+        ;; region when redoing set-mark
+        (setq this-command 'konix/set-mark-command/deactivate-regio)
         )
+    (apply orig args)
     )
+  )
 
-(advice-add 'set-mark-command :before 'konix/set-mark-command/deactivate-region)
+(advice-add 'set-mark-command :around 'konix/set-mark-command/deactivate-region)

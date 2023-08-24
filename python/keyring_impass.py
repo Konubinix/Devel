7fde891c2deb2c86ac61a11dd3f92924678d0176
sam <sam@konixwork>
2023-08-24 09:37:07 +0200

    feat: Ease working with secret already there

--- python/keyring_impass.py
+++ python/keyring_impass.py
@@ -2,13 +2,13 @@
 # -*- coding:utf-8 -*-
 
 import json
-import os
 import logging
+import os
 import subprocess
-import six
 from contextlib import contextmanager
 
 import keyring.backend
+import six
 
 
 @contextmanager
@@ -28,22 +28,19 @@ def updated_env(**kwargs):
 class ImpassKeyring(keyring.backend.KeyringBackend):
     priority = 1
 
-    def set_password(self, servicename, username, password):
+    def set_password(self, _, username, password):
         raise NotImplementedError
 
-    def get_password(self, servicename, username):
-        key = "{}@{}".format(username, servicename)
+    def get_password(self, _, username):
+        key = username
         try:
             with updated_env(IMPASS_DUMP_PASSWORDS="1"):
                 return json.loads(
-                    subprocess.check_output(
-                        [
-                            "impass",
-                            "dump",
-                            key,
-                        ]
-                    ).decode("utf-8").strip()
-                )[key]["password"]
+                    subprocess.check_output([
+                        "impass",
+                        "dump",
+                        key,
+                    ]).decode("utf-8").strip())[key]["password"]
         except KeyError:
             logging.error(f"Could not find password for {key}")
             return None

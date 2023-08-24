e965fe2046a3a17f6cc04886852e22f479c3b365
sam <sam@konixwork>
2023-08-24 09:37:20 +0200

    chore: upgrade to gtk 3

--- python/konix_gtk_zenitylib.py
+++ python/konix_gtk_zenitylib.py
@@ -1,45 +1,53 @@
 #!/usr/bin/env python3
-from gi.repository import Gtk as gtk
-from gi.repository import Gdk
-
-import sys
-import konix_notify
-
 import argparse
-parser = argparse.ArgumentParser(description="""A poor man clone of zenity --entry.""")
-parser.add_argument('-t','--text',
+import sys
+
+import konix_notify
+from gi.repository import Gdk, GLib
+from gi.repository import Gtk as gtk
+
+parser = argparse.ArgumentParser(
+    description="""A poor man clone of zenity --entry.""")
+parser.add_argument('-t',
+                    '--text',
                     help="""The text to display""",
                     type=str,
                     required=True)
-parser.add_argument('-T','--timeout',
+parser.add_argument('-T',
+                    '--timeout',
                     help="""Close after that amount of ms""",
                     type=int,
                     default=0,
                     required=False)
-parser.add_argument('-i','--initial',
-                    help="""The initial text""",
-                    type=str)
+parser.add_argument('-i', '--initial', help="""The initial text""", type=str)
 parser.add_argument('--info',
                     help="""Do not ask any input""",
                     action="store_true")
-parser.add_argument('-n','--no-clipboard',
+parser.add_argument('-n',
+                    '--no-clipboard',
                     help="""Disable clipboard handling""",
                     action="store_true")
-parser.add_argument('-a','--above-all',
+parser.add_argument('-a',
+                    '--above-all',
                     help="""Set the window above all""",
                     action="store_true")
-parser.add_argument('-b','--background-color',
+parser.add_argument('-b',
+                    '--background-color',
                     help="""Can be anything that goes into
                     http://www.pygtk.org/pygtk2reference/class-gdkcolor.html#function-gdk--color-parse""",
                     type=str)
 
 entry = None
+
+
 def responseToDialog(entry, dialog, response):
     dialog.response(response)
 
+
 def key_pressed(entry, event):
-    if event.keyval == 65307: # echap
+    if event.keyval == 65307:  # echap
         sys.exit(1)
+
 
 def get_from_clipboard(button=None, event=None):
     global entry
@@ -49,31 +57,30 @@ def get_from_clipboard(button=None, event=None):
     entry.grab_focus()
     konix_notify.main("Updated the entry with clipboard content")
 
+
 def getText(info=False,
             text="Something",
             initial="",
             clipboard=True,
             above_all=False,
             timeout=0,
-            background_color=None
-):
+            background_color=None):
     #base this on a message dialog
     text = text.replace("<", "_").replace(">", "_")
     dialog = gtk.MessageDialog(
-        None,
-        gtk.DialogFlags.MODAL | gtk.DialogFlags.DESTROY_WITH_PARENT,
+        None, gtk.DialogFlags.MODAL | gtk.DialogFlags.DESTROY_WITH_PARENT,
         gtk.MessageType.INFO if info else gtk.MessageType.QUESTION,
-        gtk.ButtonsType.NONE if info else gtk.ButtonsType.OK,
-        None)
+        gtk.ButtonsType.NONE if info else gtk.ButtonsType.OK, None)
     dialog.set_title("konix_gtk_entry")
     dialog.set_markup(text)
     if background_color:
-        dialog.modify_bg(
-            gtk.STATE_NORMAL,
-            gtk.gdk.color_parse(
-                background_color
-            )
-        )
+        css = '* { background-color: #f00; }'
+        css_provider = gtk.CssProvider()
+        css_provider.load_from_data(css.encode())
+        context = gtk.StyleContext()
+        screen = Gdk.Screen.get_default()
+        context.add_provider_for_screen(
+            screen, css_provider, gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
     if above_all:
         dialog.set_keep_above(True)
 
@@ -106,24 +113,28 @@ def getText(info=False,
         entry.grab_focus()
 
     if timeout:
+
         def _quit():
             dialog.destroy()
-        gtk.timeout_add(timeout, _quit)
+
+        GLib.timeout_add(timeout, _quit)
     dialog.run()
     text = entry.get_text()
     dialog.destroy()
     return text
 
+
 def main():
     args = parser.parse_args()
-    print(getText(info=args.info,
-                  text=args.text,
-                  initial=args.initial,
-                  clipboard=not args.no_clipboard,
-                  above_all=args.above_all,
-                  timeout=args.timeout,
-                  background_color=args.background_color
-    ))
+    print(
+        getText(info=args.info,
+                text=args.text,
+                initial=args.initial,
+                clipboard=not args.no_clipboard,
+                above_all=args.above_all,
+                timeout=args.timeout,
+                background_color=args.background_color))
+
 
 if __name__ == '__main__':
     main()

546d1d5ed45a11c09f72b4a5ef9ea16cd6de589b
sam <sam@konixwork>
2023-10-28 18:24:34 +0200

    chore: Disable a commands that annoy me more than it helps me

--- elfiles/config/after-loads/KONIX_AL-org.el
+++ elfiles/config/after-loads/KONIX_AL-org.el
@@ -5769,6 +5769,7 @@ https://emacs.stackexchange.com/questions/10707/in-org-mode-how-to-remove-a-link
 (konix/push-or-replace-assoc-in-alist 'org-speed-commands '("+" . konix/org-capture-na-in-heading))
 (konix/push-or-replace-assoc-in-alist 'org-speed-commands '("Y" . konix/org-toggle-maybe))
 (konix/push-or-replace-assoc-in-alist 'org-speed-commands '("y" . (lambda () (message "Intentionally disable y, too easily triggered to say yes"))))
+(konix/push-or-replace-assoc-in-alist 'org-speed-commands '("a" . (lambda () (message "Intentionally disable a"))))
 (konix/push-or-replace-assoc-in-alist 'org-speed-commands '("[" . (lambda () (message "Intentionally disable ["))))
 (konix/push-or-replace-assoc-in-alist 'org-speed-commands '("!" . (lambda () (message "Intentionally disable !"))))
 (konix/push-or-replace-assoc-in-alist 'org-speed-commands '("m" . konix/org-toggle-me))

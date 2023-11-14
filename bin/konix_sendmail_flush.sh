4edb0fc42994daf947ea9e50f0a78a5e59231ca6
sam <sam@konixwork>
2023-11-14 12:20:35 +0100

    fix: Try harder to find out when I cannot send an email
    Using a simpler implementation of msmtpq to find out what goes wrong.
    
    I found out that when there is no authentication, gmail returns OK
    without doing anything. I catch this by looking at the logs of
    msmtp and warn the user.

--- bin/konix_sendmail_flush.sh
+++ bin/konix_sendmail_flush.sh
@@ -1,15 +1,29 @@
 #!/bin/bash -eu
+mkdir -p "${TMPDIR}/logs/"
+LOG="${TMPDIR}/logs/msmtp.log"
+rm -f "${LOG}"
 
-if test 0 = $(msmtpq --manage list | wc -l)
+QUEUE_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/mail.queue"
+
+msg () {
+    echo "$*"
+    clk ntfy "$*"
+}
+
+if test 0 = $(ls "${QUEUE_DIR}" | wc -l)
 then
     echo "Nothing to send"
 else
     TIMETOWAIT=10
     echo "Waiting ${TIMETOWAIT}s before sending, just in case"
-    sleep ${TIMETOWAIT}
+    # sleep ${TIMETOWAIT}
     echo "Sending now!!"
-    msmtpq --manage send
-    echo OK
+    msmtpq-flush
+    if grep -q auth=off "${LOG}"
+    then
+        msg "Mail most likely not sent, because using some unknown account, check the logs at ${LOG}"
+    else
+        msg "Mail sent"
+    fi
     notmuch new
-    konix_display.py -o -t boring "Mail sent"
 fi
---
+++ flakes/msmtpq/flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "msmtpq";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.env = pkgs.buildEnv {
+          name = "msmtpq";
+          paths = with pkgs; [ packages.default ];
+        };
+        packages.default = pkgs.stdenv.mkDerivation rec {
+          name = "msmtp-queue";
+          version = "0.1.0";
+
+          dontUnpack = true;
+          dontPatch = true;
+          dontConfigure = true;
+          dontBuild = true;
+          doCheck = false;
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            cp $src/msmtpq $src/msmtpq-flush $out/bin
+            chmod +x $out/bin/*
+
+            runHook postInstall
+          '';
+          src = pkgs.fetchFromGitHub {
+            owner = "Stebalien";
+            repo = name;
+            rev = "refs/heads/master";
+            sha256 = "sha256-rvJpNApUk71wBWq2PUgePqz4AuTF7eaT/o01+appa+s=";
+          };
+        };
+      });
+}

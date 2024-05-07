2e2fce58bd2121bdc26b99333ecbbd4e9d5c3e2e
Samuel Loury <konubinixweb@gmail.com>
2024-05-07 17:57:24 +0200

    feat: Don't force scad to expect a file, so that I can use :ipfa

---
+++ elfiles/config/after-loads/KONIX_AL-ob-scad.el
@@ -0,0 +1,33 @@
+;;; KONIX_AL-ob-scad.el ---                          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  konubinix
+
+;; Author: konubinix <konubinixweb@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq org-babel-default-header-args:scad
+      '((:results . "raw")
+        (:exports . "results")))
+
+
+  (provide 'KONIX_AL-ob-scad)
+;;; KONIX_AL-ob-scad.el ends here

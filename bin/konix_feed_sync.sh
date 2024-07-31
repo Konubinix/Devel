f4aba5aee7000052706b29b4da5bd23c543eefa1
Samuel Loury <konubinixweb@gmail.com>
2024-07-31 09:56:34 +0200

    feat: Make feed_sync stricter about what is an error and what is not

--- bin/konix_feed_sync.sh
+++ bin/konix_feed_sync.sh
@@ -9,18 +9,34 @@ trap "rm -rf '${TMPDIR}'" 0
 keep_color () {
 	unbuffer "$@"
 }
-set -x
+# set -x
 pushd "${KONIX_FEED_IMAP_FLEXGET_DIR}"
 keep_color flexget execute | tee "${TMPDIR}/log"
 popd
 
+CYAN="$(tput setaf 6)"
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+OFF="$(tput sgr0)"
+BOL="$(tput cr)"
+KILL_LINE="$(tput el)"
 
-keep_color grep "ERROR\|CRITICAL\|WARNING" "${TMPDIR}/log" \
+
+cat "${TMPDIR}/log" \
+    | ansi2txt \
+    | grep "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9] \(ERROR\|CRITICAL\|WARNING\)" \
     | grep -v 'failed to import dependencies'   \
     | grep -v 'Skipped %s RSS-entries without required information'  \
-    | tee "${TMPDIR}/errors"
-konix_display.py -o "Ended getting rss feeds $(wc -l < "${TMPDIR}/errors") problems"
-
+           > "${TMPDIR}/errors"
+if test "$(wc -l < "${TMPDIR}/errors")" = "0"
+then
+    konix_display.py -o "Ended getting rss feeds. No error."
+else
+    msg="Ended getting rss feeds ${RED}$(wc -l < "${TMPDIR}/errors")${OFF} problems"
+    konix_display.py -o $(echo $msg|ansi2txt)
+    echo "${msg}"
+    echo "${RED}$(cat "${TMPDIR}/errors")${OFF}"
+fi
 # init the tags for the new mails
 echo "Initing tags"
 konix_mail_new.sh

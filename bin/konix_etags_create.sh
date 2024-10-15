eb8ff17c0de56cf4dd38d441db18b2abe54ed1e8
Samuel Loury <konubinixweb@gmail.com>
2024-10-15 10:19:42 +0200

    feat: Resurect ectags, to play with structurizr

--- bin/konix_etags_create.sh
+++ bin/konix_etags_create.sh
@@ -60,7 +60,53 @@ done
 source konix_assert_var.sh "$KONIX_CONFIG_DIR"
 source konix_assert_var.sh "$KONIX_LIB_DIR"
 
-source "${KONIX_LIB_DIR}/lib_bash.sh"
+konix_var_points_to_file_or_null_p () {
+    # ####################################################################################################
+    # This function checks if the file whose name is stored in variable whose name
+    # is in $1 exists, if not so, it replace the content of the variable by ""
+    # ####################################################################################################
+    local TAGDIR_VAR="$1"
+    eval local TAGDIR_FILE="\$$TAGDIR_VAR"
+    if [ ! -f "$TAGDIR_FILE" ]
+    then
+        echo "$TAGDIR_VAR=$TAGDIR_FILE does not point to existing file, empty it" >&2
+        eval "$TAGDIR_VAR"=""
+    fi
+}
+
+konix_file_to_lines () {
+    # ####################################################################################################
+    # Param 1 : file
+    # Param 2 : sep
+    # Param 3 : comment
+    #
+    # Reads the content of the file, split its lines and put in the RES global
+    # variable something of the form "line1"$SEP"line2"$SEP... lines beginning with
+    # comment will be avoided
+    # ####################################################################################################
+    local FILE="$1"
+    local SEP="$2"
+    local comment="$3"
+    source konix_assert_var.sh "$FILE"
+    source konix_assert_var.sh "$SEP"
+    source konix_assert.sh "-f '$FILE'"
+    RES=""
+    IFS=$'\n'
+    for LINE in $(<$FILE)
+    do
+        if [ -n "$comment" ] && [ "${LINE#${comment}}" != "${LINE}" ]
+        then
+            continue
+        fi
+        if [ -z "$RES" ]
+        then
+            RES="\"$LINE\""
+        else
+            RES="$RES$SEP\"$LINE\""
+        fi
+    done
+}
+
 
 # ####################################################################################################
 # If a tag files or tag include files does not exists, empty it
@@ -69,8 +115,7 @@ konix_var_points_to_file_or_null_p TAGDIRS_FILE
 konix_var_points_to_file_or_null_p TAGINCLUDES_FILE
 if [ -z "$TAGDIRS_FILE" -a -z "$TAGINCLUDES_FILE" ]
 then
-	echo "TAGDIRS_FILE and TAGINCLUDES_FILE are empty, adding a new TAGS_DIR
-with . in it and use it" >&2
+	echo "TAGDIRS_FILE and TAGINCLUDES_FILE are empty, adding a new TAGS_DIR with . in it and use it" >&2
 	konix_etags_add.py -d "."
 	TAGDIRS_FILE="./TAGS_DIRS"
 fi
@@ -91,13 +136,13 @@ then
 fi
 
 eval ctags $VERBOSE_CMD \
-	--options="'$KONIX_DEFAULT_CTAGS_CONFIG'" \
-	$( [ -f "${KONIX_CUSTOM_CTAGS_CONFIG}" ] && echo "--options='$KONIX_CUSTOM_CTAGS_CONFIG'" ; ) \
-	-e \
-	"$APPEND_CMD" \
-	-f "'$TAGS_FILE'" \
-	$TAGDIRS_CMD \
-	$TAGINCLUDES_CMD || die "Failed to generate tags"
+	 --options="'$KONIX_DEFAULT_CTAGS_CONFIG'" \
+	 $( [ -f "${KONIX_CUSTOM_CTAGS_CONFIG}" ] && echo "--options='$KONIX_CUSTOM_CTAGS_CONFIG'" ; ) \
+	 -e \
+	 "$APPEND_CMD" \
+	 -f "'$TAGS_FILE'" \
+	 $TAGDIRS_CMD \
+	 $TAGINCLUDES_CMD || die "Failed to generate tags"
 echo updating tags with kinds
 konix_etags_add_kinds.sh 'TAGS'
 if [ "$?" == "0" ]

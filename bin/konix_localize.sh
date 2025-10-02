6531554db14003242cd6333239a2f3899b69488d
Samuel Loury <konubinixweb@gmail.com>
2025-10-02 10:43:28 +0200

    chore: Add a few tool to deal with dates from the command line

---
+++ bin/konix_localize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit # -e
+set -o errtrace # -E
+set -o nounset # -u
+set -o pipefail
+shopt -s inherit_errexit
+
+# ctrl-c
+trap "exit 2" SIGINT
+trap "exit 3" SIGQUIT
+
+while read line
+do
+    echo "$line" | dateconv -S --zone Europe/Paris "$@"
+done

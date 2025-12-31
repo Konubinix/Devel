9a285857d0065539e1abba2c1ecd213ed07ebc27
Samuel Loury <konubinixweb@gmail.com>
2025-12-31 09:27:14 +0100

    fix: Don't ask for a model that does not have completion

--- elfiles/config/after-loads/KONIX_AL-copilot.el
+++ elfiles/config/after-loads/KONIX_AL-copilot.el
@@ -28,7 +28,11 @@
 (keymap-set copilot-completion-map "C-<tab>" 'copilot-accept-completion-by-word)
 (keymap-set copilot-completion-map "C-n" 'copilot-next-completion)
 (keymap-set copilot-completion-map "C-p" 'copilot-previous-completion)
-(setq-default copilot-lsp-settings '(:github (:copilot (:selectedCompletionModel "claude-sonnet-4"))))
+;; https://github.com/copilot-emacs/copilot.el/issues/382
+;; (pp (copilot--request 'copilot/models '(:dummy "dummy")))
+;; (setq-default copilot-lsp-settings '(:github (:copilot (:selectedCompletionModel "gemini-2.5-pro"))))
+;; but actually, it gets User selected model gemini-2.5-pro is not in the list of generic models: gpt-41-copilot, falling back to default model.
+;; it seems to be because gemini-2.5-pro does not provide the scope completion
 
 (provide 'KONIX_AL-copilot)
 ;;; KONIX_AL-copilot.el ends here

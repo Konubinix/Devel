553748ab09ceb272b6fc3a489992f788d69c911c
Samuel Loury <konubinixweb@gmail.com>
2026-02-17 16:42:42 +0100

    chore: Remove unused comint customization that conflicted with some bindings I wanted to have with agent-shell

--- elfiles/config/after-loads/KONIX_AL-comint.el
+++ elfiles/config/after-loads/KONIX_AL-comint.el
@@ -53,71 +53,53 @@
 (defun konix/comint/send-command-redirect (command handler &optional display)
   "The shell must support echo"
   (let (
-                (end_of_output nil)
-                (temp_file (make-temp-file "konix_shell_redirection_"))
-                callback_output_finished
-                (result nil)
-                (current-process (get-buffer-process (current-buffer)))
-                )
-        (unwind-protect
-                (progn
-                  (setq callback_output_finished
-                                (lambda(elt)
-                                  (if (string-match-p "^FINISHED" elt)
-                                          (setq end_of_output t)
-                                        )
-                                  )
-                                )
-                  (comint-skip-input)
-                  (comint-send-string
-                   current-process
-                   (concat command
-                                   (if display
-                                           "| tee "
-                                         "> "
-                                         )
-                                   temp_file)
-                   )
-                  (comint-send-input)
-                  (add-hook 'comint-output-filter-functions
-                                        callback_output_finished)
-                  (comint-send-string current-process "echo FINISHED")
-                  (comint-send-input)
-                  ;; Wait for the FINISHED output
-                  (while (not end_of_output)
-                        (accept-process-output nil 1)
-                        )
-                  (remove-hook 'comint-output-filter-functions
-                                           callback_output_finished)
-                  (with-temp-buffer
-                        (insert-file-contents temp_file)
-                        (beginning-of-buffer)
-                        (setq result (funcall handler))
-                        )
-                  )
-          (delete-file temp_file)
-          )
-        result
+        (end_of_output nil)
+        (temp_file (make-temp-file "konix_shell_redirection_"))
+        callback_output_finished
+        (result nil)
+        (current-process (get-buffer-process (current-buffer)))
         )
+    (unwind-protect
+        (progn
+          (setq callback_output_finished
+                (lambda(elt)
+                  (if (string-match-p "^FINISHED" elt)
+                      (setq end_of_output t)
+                    )
+                  )
+                )
+          (comint-skip-input)
+          (comint-send-string
+           current-process
+           (concat command
+                   (if display
+                       "| tee "
+                     "> "
+                     )
+                   temp_file)
+           )
+          (comint-send-input)
+          (add-hook 'comint-output-filter-functions
+                    callback_output_finished)
+          (comint-send-string current-process "echo FINISHED")
+          (comint-send-input)
+          ;; Wait for the FINISHED output
+          (while (not end_of_output)
+            (accept-process-output nil 1)
+            )
+          (remove-hook 'comint-output-filter-functions
+                       callback_output_finished)
+          (with-temp-buffer
+            (insert-file-contents temp_file)
+            (beginning-of-buffer)
+            (setq result (funcall handler))
+            )
+          )
+      (delete-file temp_file)
+      )
+    result
+    )
   )
-
-(defun konix/comint-dynamic-complete ()
-  (interactive)
-  )
-
-(defun konix/comint-dynamic-complete-no-error (&rest args)
-  (interactive)
-  (and (ignore-errors (konix/comint-dynamic-complete))
-           t)
-  )
-
-(defun konix/comint-mode-hook()
-  (keymap-local-set "C-w" 'comint-kill-region)
-  (keymap-local-set "C-c C-w" 'konix/comint-kill-last-output)
-  (keymap-local-set "C-c C-M-w" 'konix/comint-hide-or-delete-before-last-output)
-  (keymap-local-set "<tab>" 'konix/comint-dynamic-complete)
-  )
-(add-hook 'comint-mode-hook 'konix/comint-mode-hook)
 
 (setq-default comint-process-echoes t)
 

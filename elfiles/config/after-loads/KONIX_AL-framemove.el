4bde1df4ff8aa941069c2a38c0cb1c2f63bea4e2
Samuel Loury <konubinixweb@gmail.com>
2026-04-13 16:14:07 +0200

    feat: Make framemove cycle to avoid the annoying error when I reach the border

--- elfiles/config/after-loads/KONIX_AL-framemove.el
+++ elfiles/config/after-loads/KONIX_AL-framemove.el
@@ -26,5 +26,29 @@
 
 (setq-default framemove-hook-into-windmove t)
 
+(defvar konix/framemove-cycle t
+  "When non-nil, cycle to the frame on the opposite side.")
+
+(defun konix/fm-next-frame-cycle (orig-fun dir)
+  "Advice around `fm-next-frame' to cycle frames when no frame in DIR."
+  (condition-case nil
+      (funcall orig-fun dir)
+    (error
+     (if konix/framemove-cycle
+         (let* ((thisframe (selected-frame))
+                (opposite (fm-opposite dir))
+                (candidates
+                 (sort
+                  (cl-remove-if-not
+                   (lambda (f) (fm-frame-is-to-dir-of f opposite thisframe))
+                   (visible-frame-list))
+                  (lambda (f1 f2) (fm-frame-is-to-dir-of f1 opposite f2)))))
+           (if candidates
+               (select-frame-set-input-focus (car candidates))
+             (error "No other frame")))
+       (error "No frame in that direction")))))
+
+(advice-add 'fm-next-frame :around #'konix/fm-next-frame-cycle)
+
 (provide 'KONIX_AL-framemove)
 ;;; KONIX_AL-framemove.el ends here

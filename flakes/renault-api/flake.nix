99aa67eac5c46437dd0fd4c7bbd83c7b269618f3
Samuel Loury <konubinixweb@gmail.com>
2026-04-13 16:17:36 +0200

    chore: Upgrade deps

--- flakes/renault-api/flake.nix
+++ flakes/renault-api/flake.nix
@@ -10,8 +10,10 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { system = system; };
       in {
-        packages.default = pkgs.python3.withPackages (ps:
-          with ps;
-          [ renault-api ] ++ renault-api.optional-dependencies.cli);
+        packages.default = pkgs.python3.pkgs.toPythonApplication
+          (pkgs.python3.pkgs.renault-api.overridePythonAttrs (old: {
+            dependencies = (old.dependencies or [ ])
+              ++ old.optional-dependencies.cli;
+          }));
       });
 }

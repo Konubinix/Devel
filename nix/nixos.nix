38eb7b5a61cbde1cb523d3e0f44891de8cd6ac02
Samuel Loury <konubinixweb@gmail.com>
2026-04-29 11:39:41 +0200

    chore: Make the xauthority file fixed so that I can easily make a non X session X aware
    I also reorganised the file and feel lazy enough to put that in the commit as well.

--- nix/nixos.nix
+++ nix/nixos.nix
@@ -57,25 +57,44 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  services.xserver.enable = true;
-  # Equivalent to: xset r rate 170 150
-  # autoRepeatDelay is in ms (same as xset delay)
-  # autoRepeatInterval is in ms between repeats (1000 / xset rate = 1000 / 150 ≈ 7)
-  services.xserver.autoRepeatDelay = 170;
-  services.xserver.autoRepeatInterval = 7;
+  services.xserver = {
+    enable = true;
 
-  # awesome must be declared here so sddm registers the session
-  services.xserver.windowManager.awesome.enable = true;
+    # Equivalent to: xset r rate 170 150
+    # autoRepeatDelay is in ms (same as xset delay)
+    # autoRepeatInterval is in ms between repeats (1000 / xset rate = 1000 / 150 ≈ 7)
+    autoRepeatDelay = 170;
+    autoRepeatInterval = 7;
+
+    # awesome must be declared here so sddm registers the session
+    windowManager.awesome.enable = true;
+
+    # SDDM v0.20+ removed UserAuthFile= and hardcodes the auth cookie to
+    # /tmp/xauth_XXXXXX (random). That breaks any workflow that needs a stable
+    # XAUTHORITY (ssh-in, scripts) and gets nuked when systemd-tmpfiles cleans
+    # /tmp after 10 days of no logout. Copy to $HOME/.Xauthority (the legacy
+    # default, so FHS wrappers still find it) and re-export.
+    # Refs:
+    #   https://github.com/sddm/sddm/issues/944
+    #   https://gitlab.archlinux.org/archlinux/packaging/packages/sddm/-/work_items/3
+    #   https://github.com/NixOS/nixpkgs/issues/239054
+    displayManager.sessionCommands = ''
+      if [ -n "$XAUTHORITY" ] && [ "$XAUTHORITY" != "$HOME/.Xauthority" ]; then
+        install -m 600 "$XAUTHORITY" "$HOME/.Xauthority"
+        export XAUTHORITY="$HOME/.Xauthority"
+        systemctl --user import-environment XAUTHORITY
+      fi
+    '';
+
+    xkb = {
+      layout = "fr";
+      variant = "bepo";
+    };
+  };
 
   services.displayManager = {
     sddm.enable = true;
     defaultSession = "none+awesome";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "fr";
-    variant = "bepo";
   };
 
   # Configure console keymap
@@ -111,18 +130,20 @@
     pulse.enable = true;
   };
 
-  # FUSE
-  programs.fuse.userAllowOther = true;
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   # Enable atd for scheduled jobs (at, atq, atrm)
   services.atd.enable = true;
 
-  # Agents are managed outside NixOS
-  programs.ssh.startAgent = false;
-  programs.gnupg.agent.enable = false;
+  programs = {
+    # FUSE
+    fuse.userAllowOther = true;
+
+    # Agents are managed outside NixOS
+    ssh.startAgent = false;
+    gnupg.agent.enable = false;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions

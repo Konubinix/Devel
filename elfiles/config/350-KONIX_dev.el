d87276ab933386fb6818a8368f819fa3d5632a9c
sam <sam@konix>
2026-05-27 10:37:41 +0200

    chore: Start playing with jj

--- elfiles/config/350-KONIX_dev.el
+++ elfiles/config/350-KONIX_dev.el
@@ -5,16 +5,13 @@
   (find-file (concat elfiles "/config"))
   )
 
-(defun konix/gitk ()
-  "Lance gitk --all."
+(defun konix/gk ()
+  "Launch a commit graph viewer: gg for jujutsu repos, gitk otherwise."
   (interactive)
-  (let (
-		(append (if current-prefix-arg
-					""
-				  "--all"
-				  ))
-		)
-	(start-process "konix_gitk.sh" nil "konix_gitk.sh" append)
-	)
-  (message "git k launched")
-  )
+  (if (locate-dominating-file default-directory ".jj")
+      (progn
+        (start-process "gg" nil "gg")
+        (message "gg launched"))
+    (let ((append (if current-prefix-arg "" "--all")))
+      (start-process "konix_gitk.sh" nil "konix_gitk.sh" append)
+      (message "gitk launched"))))

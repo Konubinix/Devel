5943a0bb62625760357fb15268430914a4ef4ef8
Samuel Loury <konubinixweb@gmail.com>
2026-06-10 09:56:13 +0200

    chore: Make the regexp closer to the pattern we want to match to leave room for /ipfs/zz.. examples

--- bin/konix_extract_ipfs_cids.sh
+++ bin/konix_extract_ipfs_cids.sh
@@ -3,5 +3,5 @@
 sed -r 's%/p/ba%\n/ipfs/ba%g; s%(/ipfs/|ipfs:/*|https://ipfs.konubinix.eu/[^/]+/)%\n/ipfs/%g' \
 	| grep -v konixnostore \
 	| sed -r 's|^.*(/ipfs/[a-zA-Z0-9]+).*$|\1|' \
-	| grep '/ipfs/[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' \
+	| grep '/ipfs/[bQ][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' \
 	| sort | uniq
